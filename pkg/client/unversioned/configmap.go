pmorie/kubernetes

ConfigMap resource and its REST implementation

pkg/client/unversioned/configmap.go
@@ -1,0 +1,123 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package unversioned
+
+import (
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	"k8s.io/kubernetes/pkg/watch"
+)
+
+const (
+	ConfigMapResourceName string = "configMaps"
+)
+
+type ConfigMapsNamespacer interface {
+	ConfigMaps(namespace string) ConfigMapsInterface
+}
+
+type ConfigMapsInterface interface {
+	Get(string) (*extensions.ConfigMap, error)
+	List(opts api.ListOptions) (*extensions.ConfigMapList, error)
+	Create(*extensions.ConfigMap) (*extensions.ConfigMap, error)
+	Delete(string) error
+	Update(*extensions.ConfigMap) (*extensions.ConfigMap, error)
+	Watch(api.ListOptions) (watch.Interface, error)
+}
+
+type ConfigMaps struct {
+	client    *Client
+	namespace string
+}
+
+// ConfigMaps should implement ConfigMapsInterface
+var _ ConfigMapsInterface = &ConfigMaps{}
+
+func newConfigMaps(c *Client, ns string) *ConfigMaps {
+	return &ConfigMaps{
+		client:    c,
+		namespace: ns,
+	}
+}
+
+func (c *ConfigMaps) Get(name string) (*extensions.ConfigMap, error) {
+	result := &extensions.ConfigMap{}
+	err := c.client.Get().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Name(name).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) List(opts api.ListOptions) (*extensions.ConfigMapList, error) {
+	result := &extensions.ConfigMapList{}
+	err := c.client.Get().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		VersionedParams(&opts, api.Scheme).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) Create(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	result := &extensions.ConfigMap{}
+	err := c.client.Post().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Body(cfg).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) Delete(name string) error {
+	return c.client.Delete().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Name(name).
+		Do().
+		Error()
+}
+
+func (c *ConfigMaps) Update(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	result := &extensions.ConfigMap{}
+
+	err := c.client.Put().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Name(cfg.Name).
+		Body(cfg).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) Watch(opts api.ListOptions) (watch.Interface, error) {
+	return c.client.Get().
+		Prefix("watch").
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		VersionedParams(&opts, api.Scheme).
+		Watch()
+}

pkg/client/unversioned/testclient/fake_configmaps.go
@@ -1,0 +1,79 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package testclient
+
+import (
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	"k8s.io/kubernetes/pkg/watch"
+)
+
+const (
+	configMapResourceName string = "configMaps"
+)
+
+// FakeConfigMaps implements ConfigMapInterface. Meant to be embedded into a struct to get a default
+// implementation. This makes faking out just the method you want to test easier.
+type FakeConfigMaps struct {
+	Fake      *Fake
+	Namespace string
+}
+
+func (c *FakeConfigMaps) Get(name string) (*extensions.ConfigMap, error) {
+	obj, err := c.Fake.Invokes(NewGetAction(configMapResourceName, c.Namespace, name), &extensions.ConfigMap{})
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMap), err
+}
+
+func (c *FakeConfigMaps) List(opts api.ListOptions) (*extensions.ConfigMapList, error) {
+	obj, err := c.Fake.Invokes(NewListAction(configMapResourceName, c.Namespace, opts), &extensions.ConfigMapList{})
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMapList), err
+}
+
+func (c *FakeConfigMaps) Create(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	obj, err := c.Fake.Invokes(NewCreateAction(configMapResourceName, c.Namespace, cfg), cfg)
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMap), err
+}
+
+func (c *FakeConfigMaps) Update(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	obj, err := c.Fake.Invokes(NewUpdateAction(configMapResourceName, c.Namespace, cfg), cfg)
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMap), err
+}
+
+func (c *FakeConfigMaps) Delete(name string) error {
+	_, err := c.Fake.Invokes(NewDeleteAction(configMapResourceName, c.Namespace, name), &extensions.ConfigMap{})
+	return err
+}
+
+func (c *FakeConfigMaps) Watch(opts api.ListOptions) (watch.Interface, error) {
+	return c.Fake.InvokesWatch(NewWatchAction(configMapResourceName, c.Namespace, opts))
+}

pkg/client/unversioned/testclient/testclient.go
@@ -215,6 +215,10 @@ func (c *Fake) Actions() []Action {
 	return fa
 }
 
+func (c *Fake) ConfigMaps(namespace string) client.ConfigMapsInterface {
+	return &FakeConfigMaps{Fake: c, Namespace: namespace}
+}
+
 func (c *Fake) LimitRanges(namespace string) client.LimitRangeInterface {
 	return &FakeLimitRanges{Fake: c, Namespace: namespace}
 }

cymtrick/kubernetes-error-fuzzer

pkg/client/unversioned/configmap.go
@@ -1,0 +1,123 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package unversioned
+
+import (
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	"k8s.io/kubernetes/pkg/watch"
+)
+
+const (
+	ConfigMapResourceName string = "configmaps"
+)
+
+type ConfigMapsNamespacer interface {
+	ConfigMaps(namespace string) ConfigMapsInterface
+}
+
+type ConfigMapsInterface interface {
+	Get(string) (*extensions.ConfigMap, error)
+	List(opts api.ListOptions) (*extensions.ConfigMapList, error)
+	Create(*extensions.ConfigMap) (*extensions.ConfigMap, error)
+	Delete(string) error
+	Update(*extensions.ConfigMap) (*extensions.ConfigMap, error)
+	Watch(api.ListOptions) (watch.Interface, error)
+}
+
+type ConfigMaps struct {
+	client    *ExtensionsClient
+	namespace string
+}
+
+// ConfigMaps should implement ConfigMapsInterface
+var _ ConfigMapsInterface = &ConfigMaps{}
+
+func newConfigMaps(c *ExtensionsClient, ns string) *ConfigMaps {
+	return &ConfigMaps{
+		client:    c,
+		namespace: ns,
+	}
+}
+
+func (c *ConfigMaps) Get(name string) (*extensions.ConfigMap, error) {
+	result := &extensions.ConfigMap{}
+	err := c.client.Get().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Name(name).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) List(opts api.ListOptions) (*extensions.ConfigMapList, error) {
+	result := &extensions.ConfigMapList{}
+	err := c.client.Get().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		VersionedParams(&opts, api.Scheme).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) Create(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	result := &extensions.ConfigMap{}
+	err := c.client.Post().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Body(cfg).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) Delete(name string) error {
+	return c.client.Delete().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Name(name).
+		Do().
+		Error()
+}
+
+func (c *ConfigMaps) Update(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	result := &extensions.ConfigMap{}
+
+	err := c.client.Put().
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		Name(cfg.Name).
+		Body(cfg).
+		Do().
+		Into(result)
+
+	return result, err
+}
+
+func (c *ConfigMaps) Watch(opts api.ListOptions) (watch.Interface, error) {
+	return c.client.Get().
+		Prefix("watch").
+		Namespace(c.namespace).
+		Resource(ConfigMapResourceName).
+		VersionedParams(&opts, api.Scheme).
+		Watch()
+}

pkg/client/unversioned/testclient/fake_configmaps.go
@@ -1,0 +1,79 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package testclient
+
+import (
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	"k8s.io/kubernetes/pkg/watch"
+)
+
+const (
+	configMapResourceName string = "configMaps"
+)
+
+// FakeConfigMaps implements ConfigMapInterface. Meant to be embedded into a struct to get a default
+// implementation. This makes faking out just the method you want to test easier.
+type FakeConfigMaps struct {
+	Fake      *FakeExperimental
+	Namespace string
+}
+
+func (c *FakeConfigMaps) Get(name string) (*extensions.ConfigMap, error) {
+	obj, err := c.Fake.Invokes(NewGetAction(configMapResourceName, c.Namespace, name), &extensions.ConfigMap{})
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMap), err
+}
+
+func (c *FakeConfigMaps) List(opts api.ListOptions) (*extensions.ConfigMapList, error) {
+	obj, err := c.Fake.Invokes(NewListAction(configMapResourceName, c.Namespace, opts), &extensions.ConfigMapList{})
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMapList), err
+}
+
+func (c *FakeConfigMaps) Create(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	obj, err := c.Fake.Invokes(NewCreateAction(configMapResourceName, c.Namespace, cfg), cfg)
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMap), err
+}
+
+func (c *FakeConfigMaps) Update(cfg *extensions.ConfigMap) (*extensions.ConfigMap, error) {
+	obj, err := c.Fake.Invokes(NewUpdateAction(configMapResourceName, c.Namespace, cfg), cfg)
+	if obj == nil {
+		return nil, err
+	}
+
+	return obj.(*extensions.ConfigMap), err
+}
+
+func (c *FakeConfigMaps) Delete(name string) error {
+	_, err := c.Fake.Invokes(NewDeleteAction(configMapResourceName, c.Namespace, name), &extensions.ConfigMap{})
+	return err
+}
+
+func (c *FakeConfigMaps) Watch(opts api.ListOptions) (watch.Interface, error) {
+	return c.Fake.InvokesWatch(NewWatchAction(configMapResourceName, c.Namespace, opts))
+}

pkg/client/unversioned/testclient/testclient.go
@@ -353,6 +353,10 @@ func (c *FakeExperimental) Ingress(namespace string) client.IngressInterface {
 	return &FakeIngress{Fake: c, Namespace: namespace}
 }
 
+func (c *FakeExperimental) ConfigMaps(namespace string) client.ConfigMapsInterface {
+	return &FakeConfigMaps{Fake: c, Namespace: namespace}
+}
+
 func (c *FakeExperimental) ThirdPartyResources(namespace string) client.ThirdPartyResourceInterface {
 	return &FakeThirdPartyResources{Fake: c, Namespace: namespace}
 }