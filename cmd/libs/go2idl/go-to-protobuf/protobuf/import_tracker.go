kubernetes/kubernetes

go-to-protobuf: generate protobuf IDL and marshalers for Go structs

cmd/libs/go2idl/go-to-protobuf/import_tracker.go
@@ -1,0 +1,117 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"k8s.io/kubernetes/cmd/libs/go2idl/types"
+)
+
+// ImportTracker handles Protobuf package imports
+//
+// TODO: pay attention to the package name (instead of renaming every package).
+// TODO: Figure out the best way to make names for packages that collide.
+type ImportTracker struct {
+	pathToName map[string]string
+	// forbidden names are in here. (e.g. "go" is a directory in which
+	// there is code, but "go" is not a legal name for a package, so we put
+	// it here to prevent us from naming any package "go")
+	nameToPath map[string]string
+	local      types.Name
+}
+
+func NewImportTracker(local types.Name, types ...*types.Type) *ImportTracker {
+	tracker := &ImportTracker{
+		local:      local,
+		pathToName: map[string]string{},
+		nameToPath: map[string]string{
+		// Add other forbidden keywords that also happen to be
+		// package names here.
+		},
+	}
+	tracker.AddTypes(types...)
+	return tracker
+}
+
+// AddNullable ensures that support for the nullable Gogo-protobuf extension is added.
+func (tracker *ImportTracker) AddNullable() {
+	tracker.AddType(&types.Type{
+		Kind: typesKindProtobuf,
+		Name: types.Name{
+			Name:    "nullable",
+			Package: "gogoproto",
+			Path:    "github.com/gogo/protobuf/gogoproto/gogo.proto",
+		},
+	})
+}
+
+func (tracker *ImportTracker) AddTypes(types ...*types.Type) {
+	for _, t := range types {
+		tracker.AddType(t)
+	}
+}
+func (tracker *ImportTracker) AddType(t *types.Type) {
+	if tracker.local.Package == t.Name.Package {
+		return
+	}
+	// Golang type
+	if t.Kind != typesKindProtobuf {
+		// ignore built in package
+		if t.Kind == types.Builtin {
+			return
+		}
+		if _, ok := tracker.nameToPath[t.Name.Package]; !ok {
+			tracker.nameToPath[t.Name.Package] = ""
+		}
+		return
+	}
+	// ignore default proto package
+	if len(t.Name.Package) == 0 {
+		return
+	}
+	path := t.Name.Path
+	if len(path) == 0 {
+		panic(fmt.Sprintf("imported proto package %s must also have a path", t.Name.Package))
+	}
+	if _, ok := tracker.pathToName[path]; ok {
+		return
+	}
+	tracker.nameToPath[t.Name.Package] = path
+	tracker.pathToName[path] = t.Name.Package
+}
+
+func (tracker *ImportTracker) ImportLines() []string {
+	for k, v := range tracker.nameToPath {
+		if len(v) == 0 {
+			panic(fmt.Sprintf("tracking import Go package %s from %s, but no matching proto path set", k, tracker.local.Package))
+		}
+	}
+	out := []string{}
+	for path := range tracker.pathToName {
+		out = append(out, path)
+	}
+	sort.Sort(sort.StringSlice(out))
+	return out
+}
+
+// LocalNameOf returns the name you would use to refer to the package at the
+// specified path within the body of a file.
+func (tracker *ImportTracker) LocalNameOf(path string) string {
+	return tracker.pathToName[path]
+}

cmd/libs/go2idl/go-to-protobuf/package.go
@@ -1,0 +1,169 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/kubernetes/cmd/libs/go2idl/generator"
+	"k8s.io/kubernetes/cmd/libs/go2idl/types"
+)
+
+func newProtobufPackage(packagePath, packageName string, generateAll bool, omitFieldTypes map[types.Name]struct{}) *protobufPackage {
+	pkg := &protobufPackage{
+		// The protobuf package name (foo.bar.baz)
+		PackageName: packageName,
+		// A path segment relative to the GOPATH root (foo/bar/baz)
+		PackagePath:    packagePath,
+		GenerateAll:    generateAll,
+		OmitFieldTypes: omitFieldTypes,
+		HeaderText: []byte(
+			`
+// This file was autogenerated by the command:
+// $ ` + os.Args[0] + `
+// Do not edit it manually!
+
+`),
+		PackageDocumentation: []byte(fmt.Sprintf(
+			`// Package %s is an autogenerated protobuf IDL.
+`, packageName)),
+	}
+	return pkg
+}
+
+// protobufPackage contains the protobuf implentation of Package.
+type protobufPackage struct {
+	// Short name of package, used in the "package xxxx" line.
+	PackageName string
+	// Import path of the package, and the location on disk of the package.
+	PackagePath string
+	// If true, generate protobuf serializations for all public types.
+	// If false, only generate protobuf serializations for structs that
+	// request serialization.
+	GenerateAll bool
+
+	// Emitted at the top of every file.
+	HeaderText []byte
+
+	// Emitted only for a "doc.go" file; appended to the HeaderText for
+	// that file.
+	PackageDocumentation []byte
+
+	// A list of types to filter to; if not specified all types will be included.
+	FilterTypes map[types.Name]struct{}
+
+	// A list of field types that will be excluded from the output struct
+	OmitFieldTypes map[types.Name]struct{}
+
+	// A list of names that this package exports
+	LocalNames map[string]struct{}
+
+	// An import tracker for this package
+	Imports *ImportTracker
+}
+
+func (p *protobufPackage) Clean(outputBase string) error {
+	for _, s := range []string{p.ImportPath(), p.OutputPath()} {
+		if err := os.Remove(filepath.Join(outputBase, s)); err != nil && !os.IsNotExist(err) {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *protobufPackage) ProtoTypeName() types.Name {
+	return types.Name{
+		Name:    p.Path(),       // the go path "foo/bar/baz"
+		Package: p.Name(),       // the protobuf package "foo.bar.baz"
+		Path:    p.ImportPath(), // the path of the import to get the proto
+	}
+}
+
+func (p *protobufPackage) Name() string { return p.PackageName }
+func (p *protobufPackage) Path() string { return p.PackagePath }
+
+func (p *protobufPackage) Filter(c *generator.Context, t *types.Type) bool {
+	switch t.Kind {
+	case types.Func, types.Chan:
+		return false
+	case types.Struct:
+		if t.Name.Name == "struct{}" {
+			return false
+		}
+	case types.Builtin:
+		return false
+	case types.Alias:
+		return false
+	case types.Slice, types.Array, types.Map:
+		return false
+	case types.Pointer:
+		return false
+	}
+	if _, ok := isFundamentalProtoType(t); ok {
+		return false
+	}
+	_, ok := p.FilterTypes[t.Name]
+	return ok
+}
+
+func (p *protobufPackage) HasGoType(name string) bool {
+	_, ok := p.LocalNames[name]
+	return ok
+}
+
+func (p *protobufPackage) Generators(c *generator.Context) []generator.Generator {
+	generators := []generator.Generator{}
+
+	p.Imports.AddNullable()
+
+	generators = append(generators, &genProtoIDL{
+		DefaultGen: generator.DefaultGen{
+			OptionalName: "generated",
+		},
+		localPackage:   types.Name{Package: p.PackageName, Path: p.PackagePath},
+		localGoPackage: types.Name{Package: p.PackagePath, Name: p.GoPackageName()},
+		imports:        p.Imports,
+		generateAll:    p.GenerateAll,
+		omitFieldTypes: p.OmitFieldTypes,
+	})
+	return generators
+}
+
+func (p *protobufPackage) Header(filename string) []byte {
+	if filename == "doc.go" {
+		return append(p.HeaderText, p.PackageDocumentation...)
+	}
+	return p.HeaderText
+}
+
+func (p *protobufPackage) GoPackageName() string {
+	return filepath.Base(p.PackagePath)
+}
+
+func (p *protobufPackage) ImportPath() string {
+	return filepath.Join(p.PackagePath, "generated.proto")
+}
+
+func (p *protobufPackage) OutputPath() string {
+	return filepath.Join(p.PackagePath, "generated.pb.go")
+}
+
+var (
+	_ = generator.Package(&protobufPackage{})
+)

cmd/libs/go2idl/go-to-protobuf/parser.go
@@ -1,0 +1,102 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"go/format"
+	"io/ioutil"
+	"os"
+
+	"k8s.io/kubernetes/third_party/golang/go/ast"
+	//"k8s.io/kubernetes/third_party/golang/go/build"
+	"k8s.io/kubernetes/third_party/golang/go/parser"
+	"k8s.io/kubernetes/third_party/golang/go/printer"
+	"k8s.io/kubernetes/third_party/golang/go/token"
+	//tc "k8s.io/kubernetes/third_party/golang/go/types"
+)
+
+func RewriteGeneratedGogoProtobufFile(name string, packageName string, typeExistsFn func(string) bool, header []byte) error {
+	fset := token.NewFileSet()
+	src, err := ioutil.ReadFile(name)
+	if err != nil {
+		return err
+	}
+	file, err := parser.ParseFile(fset, name, src, parser.DeclarationErrors|parser.ParseComments)
+	if err != nil {
+		return err
+	}
+	cmap := ast.NewCommentMap(fset, file, file.Comments)
+
+	// ensure the package name matches the expected package name
+	//file.Name.Name = packageName
+
+	// remove types that are already declared
+	decls := []ast.Decl{}
+	for _, d := range file.Decls {
+		if !dropExistingTypeDeclarations(d, typeExistsFn) {
+			decls = append(decls, d)
+		}
+	}
+	file.Decls = decls
+
+	// remove unmapped comments
+	file.Comments = cmap.Filter(file).Comments()
+
+	b := &bytes.Buffer{}
+	b.Write(header)
+	if err := printer.Fprint(b, fset, file); err != nil {
+		return err
+	}
+
+	body, err := format.Source(b.Bytes())
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write(body)
+	return err
+}
+
+func dropExistingTypeDeclarations(decl ast.Decl, existsFn func(string) bool) bool {
+	switch t := decl.(type) {
+	case *ast.GenDecl:
+		if t.Tok != token.TYPE {
+			return false
+		}
+		specs := []ast.Spec{}
+		for _, s := range t.Specs {
+			switch spec := s.(type) {
+			case *ast.TypeSpec:
+				if existsFn(spec.Name.Name) {
+					continue
+				}
+				specs = append(specs, spec)
+			}
+		}
+		if len(specs) == 0 {
+			return true
+		}
+		t.Specs = specs
+	}
+	return false
+}

cymtrick/kubernetes-error-fuzzer

cmd/libs/go2idl/go-to-protobuf/protobuf/import_tracker.go
@@ -1,0 +1,117 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package protobuf
+
+import (
+	"fmt"
+	"sort"
+
+	"k8s.io/kubernetes/cmd/libs/go2idl/types"
+)
+
+// ImportTracker handles Protobuf package imports
+//
+// TODO: pay attention to the package name (instead of renaming every package).
+// TODO: Figure out the best way to make names for packages that collide.
+type ImportTracker struct {
+	pathToName map[string]string
+	// forbidden names are in here. (e.g. "go" is a directory in which
+	// there is code, but "go" is not a legal name for a package, so we put
+	// it here to prevent us from naming any package "go")
+	nameToPath map[string]string
+	local      types.Name
+}
+
+func NewImportTracker(local types.Name, types ...*types.Type) *ImportTracker {
+	tracker := &ImportTracker{
+		local:      local,
+		pathToName: map[string]string{},
+		nameToPath: map[string]string{
+		// Add other forbidden keywords that also happen to be
+		// package names here.
+		},
+	}
+	tracker.AddTypes(types...)
+	return tracker
+}
+
+// AddNullable ensures that support for the nullable Gogo-protobuf extension is added.
+func (tracker *ImportTracker) AddNullable() {
+	tracker.AddType(&types.Type{
+		Kind: typesKindProtobuf,
+		Name: types.Name{
+			Name:    "nullable",
+			Package: "gogoproto",
+			Path:    "github.com/gogo/protobuf/gogoproto/gogo.proto",
+		},
+	})
+}
+
+func (tracker *ImportTracker) AddTypes(types ...*types.Type) {
+	for _, t := range types {
+		tracker.AddType(t)
+	}
+}
+func (tracker *ImportTracker) AddType(t *types.Type) {
+	if tracker.local.Package == t.Name.Package {
+		return
+	}
+	// Golang type
+	if t.Kind != typesKindProtobuf {
+		// ignore built in package
+		if t.Kind == types.Builtin {
+			return
+		}
+		if _, ok := tracker.nameToPath[t.Name.Package]; !ok {
+			tracker.nameToPath[t.Name.Package] = ""
+		}
+		return
+	}
+	// ignore default proto package
+	if len(t.Name.Package) == 0 {
+		return
+	}
+	path := t.Name.Path
+	if len(path) == 0 {
+		panic(fmt.Sprintf("imported proto package %s must also have a path", t.Name.Package))
+	}
+	if _, ok := tracker.pathToName[path]; ok {
+		return
+	}
+	tracker.nameToPath[t.Name.Package] = path
+	tracker.pathToName[path] = t.Name.Package
+}
+
+func (tracker *ImportTracker) ImportLines() []string {
+	for k, v := range tracker.nameToPath {
+		if len(v) == 0 {
+			panic(fmt.Sprintf("tracking import Go package %s from %s, but no matching proto path set", k, tracker.local.Package))
+		}
+	}
+	out := []string{}
+	for path := range tracker.pathToName {
+		out = append(out, path)
+	}
+	sort.Sort(sort.StringSlice(out))
+	return out
+}
+
+// LocalNameOf returns the name you would use to refer to the package at the
+// specified path within the body of a file.
+func (tracker *ImportTracker) LocalNameOf(path string) string {
+	return tracker.pathToName[path]
+}

cmd/libs/go2idl/go-to-protobuf/protobuf/package.go
@@ -1,0 +1,169 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package protobuf
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/kubernetes/cmd/libs/go2idl/generator"
+	"k8s.io/kubernetes/cmd/libs/go2idl/types"
+)
+
+func newProtobufPackage(packagePath, packageName string, generateAll bool, omitFieldTypes map[types.Name]struct{}) *protobufPackage {
+	pkg := &protobufPackage{
+		// The protobuf package name (foo.bar.baz)
+		PackageName: packageName,
+		// A path segment relative to the GOPATH root (foo/bar/baz)
+		PackagePath:    packagePath,
+		GenerateAll:    generateAll,
+		OmitFieldTypes: omitFieldTypes,
+		HeaderText: []byte(
+			`
+// This file was autogenerated by the command:
+// $ ` + os.Args[0] + `
+// Do not edit it manually!
+
+`),
+		PackageDocumentation: []byte(fmt.Sprintf(
+			`// Package %s is an autogenerated protobuf IDL.
+`, packageName)),
+	}
+	return pkg
+}
+
+// protobufPackage contains the protobuf implentation of Package.
+type protobufPackage struct {
+	// Short name of package, used in the "package xxxx" line.
+	PackageName string
+	// Import path of the package, and the location on disk of the package.
+	PackagePath string
+	// If true, generate protobuf serializations for all public types.
+	// If false, only generate protobuf serializations for structs that
+	// request serialization.
+	GenerateAll bool
+
+	// Emitted at the top of every file.
+	HeaderText []byte
+
+	// Emitted only for a "doc.go" file; appended to the HeaderText for
+	// that file.
+	PackageDocumentation []byte
+
+	// A list of types to filter to; if not specified all types will be included.
+	FilterTypes map[types.Name]struct{}
+
+	// A list of field types that will be excluded from the output struct
+	OmitFieldTypes map[types.Name]struct{}
+
+	// A list of names that this package exports
+	LocalNames map[string]struct{}
+
+	// An import tracker for this package
+	Imports *ImportTracker
+}
+
+func (p *protobufPackage) Clean(outputBase string) error {
+	for _, s := range []string{p.ImportPath(), p.OutputPath()} {
+		if err := os.Remove(filepath.Join(outputBase, s)); err != nil && !os.IsNotExist(err) {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *protobufPackage) ProtoTypeName() types.Name {
+	return types.Name{
+		Name:    p.Path(),       // the go path "foo/bar/baz"
+		Package: p.Name(),       // the protobuf package "foo.bar.baz"
+		Path:    p.ImportPath(), // the path of the import to get the proto
+	}
+}
+
+func (p *protobufPackage) Name() string { return p.PackageName }
+func (p *protobufPackage) Path() string { return p.PackagePath }
+
+func (p *protobufPackage) Filter(c *generator.Context, t *types.Type) bool {
+	switch t.Kind {
+	case types.Func, types.Chan:
+		return false
+	case types.Struct:
+		if t.Name.Name == "struct{}" {
+			return false
+		}
+	case types.Builtin:
+		return false
+	case types.Alias:
+		return false
+	case types.Slice, types.Array, types.Map:
+		return false
+	case types.Pointer:
+		return false
+	}
+	if _, ok := isFundamentalProtoType(t); ok {
+		return false
+	}
+	_, ok := p.FilterTypes[t.Name]
+	return ok
+}
+
+func (p *protobufPackage) HasGoType(name string) bool {
+	_, ok := p.LocalNames[name]
+	return ok
+}
+
+func (p *protobufPackage) Generators(c *generator.Context) []generator.Generator {
+	generators := []generator.Generator{}
+
+	p.Imports.AddNullable()
+
+	generators = append(generators, &genProtoIDL{
+		DefaultGen: generator.DefaultGen{
+			OptionalName: "generated",
+		},
+		localPackage:   types.Name{Package: p.PackageName, Path: p.PackagePath},
+		localGoPackage: types.Name{Package: p.PackagePath, Name: p.GoPackageName()},
+		imports:        p.Imports,
+		generateAll:    p.GenerateAll,
+		omitFieldTypes: p.OmitFieldTypes,
+	})
+	return generators
+}
+
+func (p *protobufPackage) Header(filename string) []byte {
+	if filename == "doc.go" {
+		return append(p.HeaderText, p.PackageDocumentation...)
+	}
+	return p.HeaderText
+}
+
+func (p *protobufPackage) GoPackageName() string {
+	return filepath.Base(p.PackagePath)
+}
+
+func (p *protobufPackage) ImportPath() string {
+	return filepath.Join(p.PackagePath, "generated.proto")
+}
+
+func (p *protobufPackage) OutputPath() string {
+	return filepath.Join(p.PackagePath, "generated.pb.go")
+}
+
+var (
+	_ = generator.Package(&protobufPackage{})
+)

cmd/libs/go2idl/go-to-protobuf/protobuf/parser.go
@@ -1,0 +1,99 @@
+/*
+Copyright 2015 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package protobuf
+
+import (
+	"bytes"
+	"go/format"
+	"io/ioutil"
+	"os"
+
+	"k8s.io/kubernetes/third_party/golang/go/ast"
+	"k8s.io/kubernetes/third_party/golang/go/parser"
+	"k8s.io/kubernetes/third_party/golang/go/printer"
+	"k8s.io/kubernetes/third_party/golang/go/token"
+)
+
+func RewriteGeneratedGogoProtobufFile(name string, packageName string, typeExistsFn func(string) bool, header []byte) error {
+	fset := token.NewFileSet()
+	src, err := ioutil.ReadFile(name)
+	if err != nil {
+		return err
+	}
+	file, err := parser.ParseFile(fset, name, src, parser.DeclarationErrors|parser.ParseComments)
+	if err != nil {
+		return err
+	}
+	cmap := ast.NewCommentMap(fset, file, file.Comments)
+
+	// remove types that are already declared
+	decls := []ast.Decl{}
+	for _, d := range file.Decls {
+		if !dropExistingTypeDeclarations(d, typeExistsFn) {
+			decls = append(decls, d)
+		}
+	}
+	file.Decls = decls
+
+	// remove unmapped comments
+	file.Comments = cmap.Filter(file).Comments()
+
+	b := &bytes.Buffer{}
+	b.Write(header)
+	if err := printer.Fprint(b, fset, file); err != nil {
+		return err
+	}
+
+	body, err := format.Source(b.Bytes())
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.Write(body); err != nil {
+		return err
+	}
+	return f.Close()
+}
+
+func dropExistingTypeDeclarations(decl ast.Decl, existsFn func(string) bool) bool {
+	switch t := decl.(type) {
+	case *ast.GenDecl:
+		if t.Tok != token.TYPE {
+			return false
+		}
+		specs := []ast.Spec{}
+		for _, s := range t.Specs {
+			switch spec := s.(type) {
+			case *ast.TypeSpec:
+				if existsFn(spec.Name.Name) {
+					continue
+				}
+				specs = append(specs, spec)
+			}
+		}
+		if len(specs) == 0 {
+			return true
+		}
+		t.Specs = specs
+	}
+	return false
+}