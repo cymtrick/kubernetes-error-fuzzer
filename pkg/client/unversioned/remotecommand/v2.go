up_the_irons/kubernetes

Support terminal resizing for exec/attach/run
Add support for terminal resizing for exec, attach, and run. Note that for Docker, exec sessions
inherit the environment from the primary process, so if the container was created with tty=false,
that means the exec session's TERM variable will default to "dumb". Users can override this by
setting TERM=xterm (or whatever is appropriate) to get the correct "smart" terminal behavior.

pkg/client/unversioned/remotecommand/v2.go
@@ -24,7 +24,6 @@ import (
 	"sync"
 
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/util/httpstream"
 	"k8s.io/kubernetes/pkg/util/runtime"
 )
 
@@ -33,63 +32,69 @@ import (
 // version is referred to as version 2, even though it is the first actual
 // numbered version.
 type streamProtocolV2 struct {
-	stdin  io.Reader
-	stdout io.Writer
-	stderr io.Writer
-	tty    bool
+	StreamOptions
+
+	errorStream  io.Reader
+	remoteStdin  io.ReadWriteCloser
+	remoteStdout io.Reader
+	remoteStderr io.Reader
 }
 
 var _ streamProtocolHandler = &streamProtocolV2{}
 
-func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
-	var (
-		err                                                  error
-		errorStream, remoteStdin, remoteStdout, remoteStderr httpstream.Stream
-	)
+func newStreamProtocolV2(options StreamOptions) streamProtocolHandler {
+	return &streamProtocolV2{
+		StreamOptions: options,
+	}
+}
 
+func (e *streamProtocolV2) createStreams(conn streamCreator) error {
+	var err error
 	headers := http.Header{}
 
-	// set up all the streams first
 	// set up error stream
-	errorChan := make(chan error)
 	headers.Set(api.StreamType, api.StreamTypeError)
-	errorStream, err = conn.CreateStream(headers)
+	e.errorStream, err = conn.CreateStream(headers)
 	if err != nil {
 		return err
 	}
 
 	// set up stdin stream
-	if e.stdin != nil {
+	if e.Stdin != nil {
 		headers.Set(api.StreamType, api.StreamTypeStdin)
-		remoteStdin, err = conn.CreateStream(headers)
+		e.remoteStdin, err = conn.CreateStream(headers)
 		if err != nil {
 			return err
 		}
 	}
 
 	// set up stdout stream
-	if e.stdout != nil {
+	if e.Stdout != nil {
 		headers.Set(api.StreamType, api.StreamTypeStdout)
-		remoteStdout, err = conn.CreateStream(headers)
+		e.remoteStdout, err = conn.CreateStream(headers)
 		if err != nil {
 			return err
 		}
 	}
 
 	// set up stderr stream
-	if e.stderr != nil && !e.tty {
+	if e.Stderr != nil && !e.Tty {
 		headers.Set(api.StreamType, api.StreamTypeStderr)
-		remoteStderr, err = conn.CreateStream(headers)
+		e.remoteStderr, err = conn.CreateStream(headers)
 		if err != nil {
 			return err
 		}
 	}
+	return nil
+}
 
-	// now that all the streams have been created, proceed with reading & copying
+func (e *streamProtocolV2) setupErrorStreamReading() chan error {
+	errorChan := make(chan error)
 
-	// always read from errorStream
 	go func() {
-		message, err := ioutil.ReadAll(errorStream)
+		defer runtime.HandleCrash()
+
+		message, err := ioutil.ReadAll(e.errorStream)
 		switch {
 		case err != nil && err != io.EOF:
 			errorChan <- fmt.Errorf("error reading from error stream: %s", err)
@@ -101,18 +106,23 @@ func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
 		close(errorChan)
 	}()
 
-	var wg sync.WaitGroup
-	var once sync.Once
+	return errorChan
+}
 
-	if e.stdin != nil {
+func (e *streamProtocolV2) copyStdin() {
+	if e.Stdin != nil {
+		var once sync.Once
+
 		// copy from client's stdin to container's stdin
 		go func() {
+			defer runtime.HandleCrash()
+
 			// if e.stdin is noninteractive, e.g. `echo abc | kubectl exec -i <pod> -- cat`, make sure
 			// we close remoteStdin as soon as the copy from e.stdin to remoteStdin finishes. Otherwise
 			// the executed command will remain running.
-			defer once.Do(func() { remoteStdin.Close() })
+			defer once.Do(func() { e.remoteStdin.Close() })
 
-			if _, err := io.Copy(remoteStdin, e.stdin); err != nil {
+			if _, err := io.Copy(e.remoteStdin, e.Stdin); err != nil {
 				runtime.HandleError(err)
 			}
 		}()
@@ -120,6 +130,9 @@ func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
 		// read from remoteStdin until the stream is closed. this is essential to
 		// be able to exit interactive sessions cleanly and not leak goroutines or
 		// hang the client's terminal.
+		//
+		// TODO we aren't using go-dockerclient any more; revisit this to determine if it's still
+		// required by engine-api.
 		//
 		// go-dockerclient's current hijack implementation
 		// (https://github.com/fsouza/go-dockerclient/blob/89f3d56d93788dfe85f864a44f85d9738fca0670/client.go#L564)
@@ -129,34 +142,62 @@ func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
 		// When that happens, we must Close() on our side of remoteStdin, to
 		// allow the copy in hijack to complete, and hijack to return.
 		go func() {
-			defer once.Do(func() { remoteStdin.Close() })
+			defer runtime.HandleCrash()
+			defer once.Do(func() { e.remoteStdin.Close() })
+
 			// this "copy" doesn't actually read anything - it's just here to wait for
 			// the server to close remoteStdin.
-			if _, err := io.Copy(ioutil.Discard, remoteStdin); err != nil {
+			if _, err := io.Copy(ioutil.Discard, e.remoteStdin); err != nil {
 				runtime.HandleError(err)
 			}
 		}()
 	}
+}
 
-	if e.stdout != nil {
+func (e *streamProtocolV2) copyStdout(wg *sync.WaitGroup) {
+	if e.Stdout != nil {
 		wg.Add(1)
 		go func() {
+			defer runtime.HandleCrash()
 			defer wg.Done()
-			if _, err := io.Copy(e.stdout, remoteStdout); err != nil {
+
+			if _, err := io.Copy(e.Stdout, e.remoteStdout); err != nil {
 				runtime.HandleError(err)
 			}
 		}()
 	}
+}
 
-	if e.stderr != nil && !e.tty {
+func (e *streamProtocolV2) copyStderr(wg *sync.WaitGroup) {
+	if e.Stderr != nil && !e.Tty {
 		wg.Add(1)
 		go func() {
+			defer runtime.HandleCrash()
 			defer wg.Done()
-			if _, err := io.Copy(e.stderr, remoteStderr); err != nil {
+
+			if _, err := io.Copy(e.Stderr, e.remoteStderr); err != nil {
 				runtime.HandleError(err)
 			}
 		}()
 	}
+}
+
+func (e *streamProtocolV2) stream(conn streamCreator) error {
+	// set up all the streams first
+	if err := e.createStreams(conn); err != nil {
+		return err
+	}
+
+	// now that all the streams have been created, proceed with reading & copying
+
+	// always read from errorStream
+	errorChan := e.setupErrorStreamReading()
+
+	e.copyStdin()
+
+	var wg sync.WaitGroup
+	e.copyStdout(&wg)
+	e.copyStderr(&wg)
 
 	// we're waiting for stdout/stderr to finish copying
 	wg.Wait()

pkg/client/unversioned/remotecommand/v3.go
@@ -1,0 +1,110 @@
+/*
+Copyright 2016 The Kubernetes Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package remotecommand
+
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/util/httpstream"
+	"k8s.io/kubernetes/pkg/util/runtime"
+)
+
+// streamProtocolV3 implements version 3 of the streaming protocol for attach
+// and exec. This version adds support for resizing the container's terminal.
+type streamProtocolV3 struct {
+	*streamProtocolV2
+
+	resizeStream httpstream.Stream
+}
+
+var _ streamProtocolHandler = &streamProtocolV3{}
+
+func newStreamProtocolV3(options StreamOptions) streamProtocolHandler {
+	return &streamProtocolV3{
+		streamProtocolV2: newStreamProtocolV2(options).(*streamProtocolV2),
+	}
+}
+
+func (e *streamProtocolV3) createStreams(conn streamCreator) error {
+	// set up the streams from v2
+	if err := e.streamProtocolV2.createStreams(conn); err != nil {
+		return err
+	}
+
+	// set up resize stream
+	if e.Tty {
+		headers := http.Header{}
+		headers.Set(api.StreamType, api.StreamTypeResize)
+		var err error
+		e.resizeStream, err = conn.CreateStream(headers)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (e *streamProtocolV3) handleResizes() {
+	if e.resizeStream == nil {
+		return
+	}
+
+	go func() {
+		defer runtime.HandleCrash()
+
+		encoder := json.NewEncoder(e.resizeStream)
+		for {
+			size := e.TerminalSizeQueue.Next()
+			if size == nil {
+				return
+			}
+			if err := encoder.Encode(&size); err != nil {
+				runtime.HandleError(err)
+			}
+		}
+	}()
+}
+
+func (e *streamProtocolV3) stream(conn streamCreator) error {
+	// set up all the streams first
+	if err := e.createStreams(conn); err != nil {
+		return err
+	}
+
+	// now that all the streams have been created, proceed with reading & copying
+
+	// always read from errorStream
+	errorChan := e.setupErrorStreamReading()
+
+	e.handleResizes()
+
+	e.copyStdin()
+
+	var wg sync.WaitGroup
+	e.copyStdout(&wg)
+	e.copyStderr(&wg)
+
+	// we're waiting for stdout/stderr to finish copying
+	wg.Wait()
+
+	// waits for errorStream to finish reading with an error or nil
+	return <-errorChan
+}

cymtrick/kubernetes-error-fuzzer

pkg/client/unversioned/remotecommand/v2.go
@@ -24,7 +24,6 @@ import (
 	"sync"
 
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/util/httpstream"
 	"k8s.io/kubernetes/pkg/util/runtime"
 )
 
@@ -33,63 +32,69 @@ import (
 // version is referred to as version 2, even though it is the first actual
 // numbered version.
 type streamProtocolV2 struct {
-	stdin  io.Reader
-	stdout io.Writer
-	stderr io.Writer
-	tty    bool
+	StreamOptions
+
+	errorStream  io.Reader
+	remoteStdin  io.ReadWriteCloser
+	remoteStdout io.Reader
+	remoteStderr io.Reader
 }
 
 var _ streamProtocolHandler = &streamProtocolV2{}
 
-func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
-	var (
-		err                                                  error
-		errorStream, remoteStdin, remoteStdout, remoteStderr httpstream.Stream
-	)
+func newStreamProtocolV2(options StreamOptions) streamProtocolHandler {
+	return &streamProtocolV2{
+		StreamOptions: options,
+	}
+}
 
+func (p *streamProtocolV2) createStreams(conn streamCreator) error {
+	var err error
 	headers := http.Header{}
 
-	// set up all the streams first
 	// set up error stream
-	errorChan := make(chan error)
 	headers.Set(api.StreamType, api.StreamTypeError)
-	errorStream, err = conn.CreateStream(headers)
+	p.errorStream, err = conn.CreateStream(headers)
 	if err != nil {
 		return err
 	}
 
 	// set up stdin stream
-	if e.stdin != nil {
+	if p.Stdin != nil {
 		headers.Set(api.StreamType, api.StreamTypeStdin)
-		remoteStdin, err = conn.CreateStream(headers)
+		p.remoteStdin, err = conn.CreateStream(headers)
 		if err != nil {
 			return err
 		}
 	}
 
 	// set up stdout stream
-	if e.stdout != nil {
+	if p.Stdout != nil {
 		headers.Set(api.StreamType, api.StreamTypeStdout)
-		remoteStdout, err = conn.CreateStream(headers)
+		p.remoteStdout, err = conn.CreateStream(headers)
 		if err != nil {
 			return err
 		}
 	}
 
 	// set up stderr stream
-	if e.stderr != nil && !e.tty {
+	if p.Stderr != nil && !p.Tty {
 		headers.Set(api.StreamType, api.StreamTypeStderr)
-		remoteStderr, err = conn.CreateStream(headers)
+		p.remoteStderr, err = conn.CreateStream(headers)
 		if err != nil {
 			return err
 		}
 	}
+	return nil
+}
 
-	// now that all the streams have been created, proceed with reading & copying
+func (p *streamProtocolV2) setupErrorStreamReading() chan error {
+	errorChan := make(chan error)
 
-	// always read from errorStream
 	go func() {
-		message, err := ioutil.ReadAll(errorStream)
+		defer runtime.HandleCrash()
+
+		message, err := ioutil.ReadAll(p.errorStream)
 		switch {
 		case err != nil && err != io.EOF:
 			errorChan <- fmt.Errorf("error reading from error stream: %s", err)
@@ -101,18 +106,23 @@ func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
 		close(errorChan)
 	}()
 
-	var wg sync.WaitGroup
-	var once sync.Once
+	return errorChan
+}
 
-	if e.stdin != nil {
+func (p *streamProtocolV2) copyStdin() {
+	if p.Stdin != nil {
+		var once sync.Once
+
 		// copy from client's stdin to container's stdin
 		go func() {
-			// if e.stdin is noninteractive, e.g. `echo abc | kubectl exec -i <pod> -- cat`, make sure
-			// we close remoteStdin as soon as the copy from e.stdin to remoteStdin finishes. Otherwise
-			// the executed command will remain running.
-			defer once.Do(func() { remoteStdin.Close() })
+			defer runtime.HandleCrash()
 
-			if _, err := io.Copy(remoteStdin, e.stdin); err != nil {
+			// if p.stdin is noninteractive, p.g. `echo abc | kubectl exec -i <pod> -- cat`, make sure
+			// we close remoteStdin as soon as the copy from p.stdin to remoteStdin finishes. Otherwise
+			// the executed command will remain running.
+			defer once.Do(func() { p.remoteStdin.Close() })
+
+			if _, err := io.Copy(p.remoteStdin, p.Stdin); err != nil {
 				runtime.HandleError(err)
 			}
 		}()
@@ -120,6 +130,9 @@ func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
 		// read from remoteStdin until the stream is closed. this is essential to
 		// be able to exit interactive sessions cleanly and not leak goroutines or
 		// hang the client's terminal.
+		//
+		// TODO we aren't using go-dockerclient any more; revisit this to determine if it's still
+		// required by engine-api.
 		//
 		// go-dockerclient's current hijack implementation
 		// (https://github.com/fsouza/go-dockerclient/blob/89f3d56d93788dfe85f864a44f85d9738fca0670/client.go#L564)
@@ -129,34 +142,64 @@ func (e *streamProtocolV2) stream(conn httpstream.Connection) error {
 		// When that happens, we must Close() on our side of remoteStdin, to
 		// allow the copy in hijack to complete, and hijack to return.
 		go func() {
-			defer once.Do(func() { remoteStdin.Close() })
+			defer runtime.HandleCrash()
+			defer once.Do(func() { p.remoteStdin.Close() })
+
 			// this "copy" doesn't actually read anything - it's just here to wait for
 			// the server to close remoteStdin.
-			if _, err := io.Copy(ioutil.Discard, remoteStdin); err != nil {
+			if _, err := io.Copy(ioutil.Discard, p.remoteStdin); err != nil {
 				runtime.HandleError(err)
 			}
 		}()
+	}
+}
+
+func (p *streamProtocolV2) copyStdout(wg *sync.WaitGroup) {
+	if p.Stdout == nil {
+		return
 	}
 
-	if e.stdout != nil {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if _, err := io.Copy(e.stdout, remoteStdout); err != nil {
-				runtime.HandleError(err)
-			}
-		}()
+	wg.Add(1)
+	go func() {
+		defer runtime.HandleCrash()
+		defer wg.Done()
+
+		if _, err := io.Copy(p.Stdout, p.remoteStdout); err != nil {
+			runtime.HandleError(err)
+		}
+	}()
+}
+
+func (p *streamProtocolV2) copyStderr(wg *sync.WaitGroup) {
+	if p.Stderr == nil || p.Tty {
+		return
 	}
 
-	if e.stderr != nil && !e.tty {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if _, err := io.Copy(e.stderr, remoteStderr); err != nil {
-				runtime.HandleError(err)
-			}
-		}()
+	wg.Add(1)
+	go func() {
+		defer runtime.HandleCrash()
+		defer wg.Done()
+
+		if _, err := io.Copy(p.Stderr, p.remoteStderr); err != nil {
+			runtime.HandleError(err)
+		}
+	}()
+}
+
+func (p *streamProtocolV2) stream(conn streamCreator) error {
+	if err := p.createStreams(conn); err != nil {
+		return err
 	}
+
+	// now that all the streams have been created, proceed with reading & copying
+
+	errorChan := p.setupErrorStreamReading()
+
+	p.copyStdin()
+
+	var wg sync.WaitGroup
+	p.copyStdout(&wg)
+	p.copyStderr(&wg)
 
 	// we're waiting for stdout/stderr to finish copying
 	wg.Wait()

pkg/client/unversioned/remotecommand/v3.go
@@ -1,0 +1,108 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package remotecommand
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"sync"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/util/runtime"
+)
+
+// streamProtocolV3 implements version 3 of the streaming protocol for attach
+// and exec. This version adds support for resizing the container's terminal.
+type streamProtocolV3 struct {
+	*streamProtocolV2
+
+	resizeStream io.Writer
+}
+
+var _ streamProtocolHandler = &streamProtocolV3{}
+
+func newStreamProtocolV3(options StreamOptions) streamProtocolHandler {
+	return &streamProtocolV3{
+		streamProtocolV2: newStreamProtocolV2(options).(*streamProtocolV2),
+	}
+}
+
+func (p *streamProtocolV3) createStreams(conn streamCreator) error {
+	// set up the streams from v2
+	if err := p.streamProtocolV2.createStreams(conn); err != nil {
+		return err
+	}
+
+	// set up resize stream
+	if p.Tty {
+		headers := http.Header{}
+		headers.Set(api.StreamType, api.StreamTypeResize)
+		var err error
+		p.resizeStream, err = conn.CreateStream(headers)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *streamProtocolV3) handleResizes() {
+	if p.resizeStream == nil {
+		return
+	}
+
+	go func() {
+		defer runtime.HandleCrash()
+
+		encoder := json.NewEncoder(p.resizeStream)
+		for {
+			size := p.TerminalSizeQueue.Next()
+			if size == nil {
+				return
+			}
+			if err := encoder.Encode(&size); err != nil {
+				runtime.HandleError(err)
+			}
+		}
+	}()
+}
+
+func (p *streamProtocolV3) stream(conn streamCreator) error {
+	if err := p.createStreams(conn); err != nil {
+		return err
+	}
+
+	// now that all the streams have been created, proceed with reading & copying
+
+	errorChan := p.setupErrorStreamReading()
+
+	p.handleResizes()
+
+	p.copyStdin()
+
+	var wg sync.WaitGroup
+	p.copyStdout(&wg)
+	p.copyStderr(&wg)
+
+	// we're waiting for stdout/stderr to finish copying
+	wg.Wait()
+
+	// waits for errorStream to finish reading with an error or nil
+	return <-errorChan
+}