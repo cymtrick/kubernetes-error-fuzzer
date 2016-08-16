kubernetes/kubernetes

Fix default resource limits (node capacities) for downward api volumes

pkg/controller/volume/persistentvolume/volume_host.go
@@ -80,3 +80,7 @@ func (ctrl *PersistentVolumeController) GetHostIP() (net.IP, error) {
 func (ctrl *PersistentVolumeController) GetRootContext() string {
 	return ""
 }
+
+func (ctrl *PersistentVolumeController) GetNodeCapacity() (api.ResourceList, error) {
+	return api.ResourceList{}, nil
+}

pkg/volume/plugins.go
@@ -207,6 +207,9 @@ type VolumeHost interface {
 	// mounts correctly. It will be replaced and expanded on by future
 	// SecurityContext work.
 	GetRootContext() string
+
+	// Returns node capacity
+	GetNodeCapacity() (api.ResourceList, error)
 }
 
 // VolumePluginMgr tracks registered plugins.

pkg/volume/testing/testing.go
@@ -127,6 +127,10 @@ func (f *fakeVolumeHost) GetRootContext() string {
 	return f.rootContext
 }
 
+func (f *fakeVolumeHost) GetNodeCapacity() (api.ResourceList, error) {
+	return api.ResourceList{}, nil
+}
+
 func ProbeVolumePlugins(config VolumeConfig) []VolumePlugin {
 	if _, ok := config.OtherAttributes["fake-property"]; ok {
 		return []VolumePlugin{

cymtrick/kubernetes-error-fuzzer

pkg/controller/volume/persistentvolume/volume_host.go
@@ -80,3 +80,7 @@ func (ctrl *PersistentVolumeController) GetHostIP() (net.IP, error) {
 func (ctrl *PersistentVolumeController) GetRootContext() string {
 	return ""
 }
+
+func (ctrl *PersistentVolumeController) GetNodeAllocatable() (api.ResourceList, error) {
+	return api.ResourceList{}, nil
+}

pkg/volume/plugins.go
@@ -214,6 +214,9 @@ type VolumeHost interface {
 	// mounts correctly. It will be replaced and expanded on by future
 	// SecurityContext work.
 	GetRootContext() string
+
+	// Returns node allocatable
+	GetNodeAllocatable() (api.ResourceList, error)
 }
 
 // VolumePluginMgr tracks registered plugins.

pkg/volume/testing/testing.go
@@ -127,6 +127,10 @@ func (f *fakeVolumeHost) GetRootContext() string {
 	return f.rootContext
 }
 
+func (f *fakeVolumeHost) GetNodeAllocatable() (api.ResourceList, error) {
+	return api.ResourceList{}, nil
+}
+
 func ProbeVolumePlugins(config VolumeConfig) []VolumePlugin {
 	if _, ok := config.OtherAttributes["fake-property"]; ok {
 		return []VolumePlugin{