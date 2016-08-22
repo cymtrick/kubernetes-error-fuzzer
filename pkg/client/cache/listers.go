kubernetes/kubernetes

Don't validate selector that is already validated

pkg/client/cache/listers.go
@@ -246,11 +246,10 @@ func (s *StoreToReplicationControllerLister) GetPodControllers(pod *api.Pod) (co
 
 	for _, m := range items {
 		rc = *m.(*api.ReplicationController)
-		labelSet := labels.Set(rc.Spec.Selector)
-		selector = labels.Set(rc.Spec.Selector).AsSelector()
+		selector = labels.Set(rc.Spec.Selector).ValidatedAsSelector()
 
 		// If an rc with a nil or empty selector creeps in, it should match nothing, not everything.
-		if labelSet.AsSelector().Empty() || !selector.Matches(labels.Set(pod.Labels)) {
+		if selector.Empty() || !selector.Matches(labels.Set(pod.Labels)) {
 			continue
 		}
 		controllers = append(controllers, rc)
@@ -479,7 +478,7 @@ func (s *StoreToServiceLister) GetPodServices(pod *api.Pod) (services []api.Serv
 			// services with nil selectors match nothing, not everything.
 			continue
 		}
-		selector = labels.Set(service.Spec.Selector).AsSelector()
+		selector = labels.Set(service.Spec.Selector).ValidatedAsSelector()
 		if selector.Matches(labels.Set(pod.Labels)) {
 			services = append(services, service)
 		}

pkg/client/cache/listers_test.go
@@ -143,7 +143,7 @@ func TestStoreToReplicationControllerLister(t *testing.T) {
 				},
 			},
 			list: func(lister StoreToReplicationControllerLister) ([]api.ReplicationController, error) {
-				return lister.ReplicationControllers(api.NamespaceAll).List(labels.Set{}.AsSelector())
+				return lister.ReplicationControllers(api.NamespaceAll).List(labels.Set{}.ValidatedAsSelector())
 			},
 			outRCNames: sets.NewString("hmm", "foo"),
 		},
@@ -158,7 +158,7 @@ func TestStoreToReplicationControllerLister(t *testing.T) {
 				},
 			},
 			list: func(lister StoreToReplicationControllerLister) ([]api.ReplicationController, error) {
-				return lister.ReplicationControllers("hmm").List(labels.Set{}.AsSelector())
+				return lister.ReplicationControllers("hmm").List(labels.Set{}.ValidatedAsSelector())
 			},
 			outRCNames: sets.NewString("hmm"),
 		},
@@ -715,7 +715,7 @@ func TestStoreToPodLister(t *testing.T) {
 		spl := StoreToPodLister{store}
 
 		// Verify that we can always look up by Namespace.
-		defaultPods, err := spl.Pods(api.NamespaceDefault).List(labels.Set{}.AsSelector())
+		defaultPods, err := spl.Pods(api.NamespaceDefault).List(labels.Set{}.ValidatedAsSelector())
 		if err != nil {
 			t.Errorf("Unexpected error: %v", err)
 		} else if e, a := 1, len(defaultPods); e != a {
@@ -725,7 +725,7 @@ func TestStoreToPodLister(t *testing.T) {
 		}
 
 		for _, id := range ids {
-			got, err := spl.List(labels.Set{"name": id}.AsSelector())
+			got, err := spl.List(labels.Set{"name": id}.ValidatedAsSelector())
 			if err != nil {
 				t.Errorf("Unexpected error: %v", err)
 				continue

pkg/controller/controller_utils.go
@@ -472,7 +472,7 @@ func (r RealPodControl) createPods(nodeName, namespace string, template *api.Pod
 	if len(nodeName) != 0 {
 		pod.Spec.NodeName = nodeName
 	}
-	if labels.Set(pod.Labels).AsSelector().Empty() {
+	if labels.Set(pod.Labels).ValidatedAsSelector().Empty() {
 		return fmt.Errorf("unable to create pods, no labels")
 	}
 	if newPod, err := r.KubeClient.Core().Pods(namespace).Create(pod); err != nil {

pkg/controller/endpoint/endpoints_controller.go
@@ -358,7 +358,7 @@ func (e *EndpointController) syncService(key string) {
 	}
 
 	glog.V(5).Infof("About to update endpoints for service %q", key)
-	pods, err := e.podStore.Pods(service.Namespace).List(labels.Set(service.Spec.Selector).AsSelector())
+	pods, err := e.podStore.Pods(service.Namespace).List(labels.Set(service.Spec.Selector).ValidatedAsSelector())
 	if err != nil {
 		// Since we're getting stuff from a local cache, it is
 		// basically impossible to get this error.

pkg/controller/replicaset/replica_set.go
@@ -658,7 +658,7 @@ func (rsc *ReplicaSetController) syncReplicaSet(key string) error {
 	// of the selector of the replicaset, so the possible matching pods must be
 	// part of the filteredPods.
 	fullyLabeledReplicasCount := 0
-	templateLabel := labels.Set(rs.Spec.Template.Labels).AsSelector()
+	templateLabel := labels.Set(rs.Spec.Template.Labels).ValidatedAsSelector()
 	for _, pod := range filteredPods {
 		if templateLabel.Matches(labels.Set(pod.Labels)) {
 			fullyLabeledReplicasCount++

pkg/controller/replication/replication_controller.go
@@ -291,11 +291,10 @@ func isControllerMatch(pod *api.Pod, rc *api.ReplicationController) bool {
 	if rc.Namespace != pod.Namespace {
 		return false
 	}
-	labelSet := labels.Set(rc.Spec.Selector)
-	selector := labels.Set(rc.Spec.Selector).AsSelector()
+	selector := labels.Set(rc.Spec.Selector).ValidatedAsSelector()
 
 	// If an rc with a nil or empty selector creeps in, it should match nothing, not everything.
-	if labelSet.AsSelector().Empty() || !selector.Matches(labels.Set(pod.Labels)) {
+	if selector.Empty() || !selector.Matches(labels.Set(pod.Labels)) {
 		return false
 	}
 	return true
@@ -631,7 +630,7 @@ func (rm *ReplicationManager) syncReplicationController(key string) error {
 			rm.queue.Add(key)
 			return err
 		}
-		cm := controller.NewPodControllerRefManager(rm.podControl, rc.ObjectMeta, labels.Set(rc.Spec.Selector).AsSelector(), getRCKind())
+		cm := controller.NewPodControllerRefManager(rm.podControl, rc.ObjectMeta, labels.Set(rc.Spec.Selector).ValidatedAsSelector(), getRCKind())
 		matchesAndControlled, matchesNeedsController, controlledDoesNotMatch := cm.Classify(pods)
 		for _, pod := range matchesNeedsController {
 			err := cm.AdoptPod(pod)
@@ -663,7 +662,7 @@ func (rm *ReplicationManager) syncReplicationController(key string) error {
 			return aggregate
 		}
 	} else {
-		pods, err := rm.podStore.Pods(rc.Namespace).List(labels.Set(rc.Spec.Selector).AsSelector())
+		pods, err := rm.podStore.Pods(rc.Namespace).List(labels.Set(rc.Spec.Selector).ValidatedAsSelector())
 		if err != nil {
 			glog.Errorf("Error getting pods for rc %q: %v", key, err)
 			rm.queue.Add(key)
@@ -683,7 +682,7 @@ func (rm *ReplicationManager) syncReplicationController(key string) error {
 	// a superset of the selector of the replication controller, so the possible
 	// matching pods must be part of the filteredPods.
 	fullyLabeledReplicasCount := 0
-	templateLabel := labels.Set(rc.Spec.Template.Labels).AsSelector()
+	templateLabel := labels.Set(rc.Spec.Template.Labels).ValidatedAsSelector()
 	for _, pod := range filteredPods {
 		if templateLabel.Matches(labels.Set(pod.Labels)) {
 			fullyLabeledReplicasCount++

plugin/pkg/scheduler/algorithm/listers.go
@@ -85,7 +85,7 @@ func (f FakeServiceLister) GetPodServices(pod *api.Pod) (services []api.Service,
 		if service.Namespace != pod.Namespace {
 			continue
 		}
-		selector = labels.Set(service.Spec.Selector).AsSelector()
+		selector = labels.Set(service.Spec.Selector).ValidatedAsSelector()
 		if selector.Matches(labels.Set(pod.Labels)) {
 			services = append(services, service)
 		}
@@ -134,7 +134,7 @@ func (f FakeControllerLister) GetPodControllers(pod *api.Pod) (controllers []api
 		if controller.Namespace != pod.Namespace {
 			continue
 		}
-		selector = labels.Set(controller.Spec.Selector).AsSelector()
+		selector = labels.Set(controller.Spec.Selector).ValidatedAsSelector()
 		if selector.Matches(labels.Set(pod.Labels)) {
 			controllers = append(controllers, controller)
 		}

cymtrick/kubernetes-error-fuzzer

pkg/client/cache/listers.go
@@ -269,11 +269,10 @@ func (s *StoreToReplicationControllerLister) GetPodControllers(pod *api.Pod) (co
 
 	for _, m := range items {
 		rc = *m.(*api.ReplicationController)
-		labelSet := labels.Set(rc.Spec.Selector)
-		selector = labels.Set(rc.Spec.Selector).AsSelector()
+		selector = labels.Set(rc.Spec.Selector).AsSelectorPreValidated()
 
 		// If an rc with a nil or empty selector creeps in, it should match nothing, not everything.
-		if labelSet.AsSelector().Empty() || !selector.Matches(labels.Set(pod.Labels)) {
+		if selector.Empty() || !selector.Matches(labels.Set(pod.Labels)) {
 			continue
 		}
 		controllers = append(controllers, rc)
@@ -513,7 +512,7 @@ func (s *StoreToServiceLister) GetPodServices(pod *api.Pod) (services []api.Serv
 			// services with nil selectors match nothing, not everything.
 			continue
 		}
-		selector = labels.Set(service.Spec.Selector).AsSelector()
+		selector = labels.Set(service.Spec.Selector).AsSelectorPreValidated()
 		if selector.Matches(labels.Set(pod.Labels)) {
 			services = append(services, service)
 		}

pkg/client/cache/listers_test.go
@@ -143,7 +143,7 @@ func TestStoreToReplicationControllerLister(t *testing.T) {
 				},
 			},
 			list: func(lister StoreToReplicationControllerLister) ([]api.ReplicationController, error) {
-				return lister.ReplicationControllers(api.NamespaceAll).List(labels.Set{}.AsSelector())
+				return lister.ReplicationControllers(api.NamespaceAll).List(labels.Set{}.AsSelectorPreValidated())
 			},
 			outRCNames: sets.NewString("hmm", "foo"),
 		},
@@ -158,7 +158,7 @@ func TestStoreToReplicationControllerLister(t *testing.T) {
 				},
 			},
 			list: func(lister StoreToReplicationControllerLister) ([]api.ReplicationController, error) {
-				return lister.ReplicationControllers("hmm").List(labels.Set{}.AsSelector())
+				return lister.ReplicationControllers("hmm").List(labels.Set{}.AsSelectorPreValidated())
 			},
 			outRCNames: sets.NewString("hmm"),
 		},
@@ -715,7 +715,7 @@ func TestStoreToPodLister(t *testing.T) {
 		spl := StoreToPodLister{store}
 
 		// Verify that we can always look up by Namespace.
-		defaultPods, err := spl.Pods(api.NamespaceDefault).List(labels.Set{}.AsSelector())
+		defaultPods, err := spl.Pods(api.NamespaceDefault).List(labels.Set{}.AsSelectorPreValidated())
 		if err != nil {
 			t.Errorf("Unexpected error: %v", err)
 		} else if e, a := 1, len(defaultPods); e != a {
@@ -725,7 +725,7 @@ func TestStoreToPodLister(t *testing.T) {
 		}
 
 		for _, id := range ids {
-			got, err := spl.List(labels.Set{"name": id}.AsSelector())
+			got, err := spl.List(labels.Set{"name": id}.AsSelectorPreValidated())
 			if err != nil {
 				t.Errorf("Unexpected error: %v", err)
 				continue

pkg/controller/controller_utils.go
@@ -472,7 +472,7 @@ func (r RealPodControl) createPods(nodeName, namespace string, template *api.Pod
 	if len(nodeName) != 0 {
 		pod.Spec.NodeName = nodeName
 	}
-	if labels.Set(pod.Labels).AsSelector().Empty() {
+	if labels.Set(pod.Labels).AsSelectorPreValidated().Empty() {
 		return fmt.Errorf("unable to create pods, no labels")
 	}
 	if newPod, err := r.KubeClient.Core().Pods(namespace).Create(pod); err != nil {

pkg/controller/endpoint/endpoints_controller.go
@@ -358,7 +358,7 @@ func (e *EndpointController) syncService(key string) {
 	}
 
 	glog.V(5).Infof("About to update endpoints for service %q", key)
-	pods, err := e.podStore.Pods(service.Namespace).List(labels.Set(service.Spec.Selector).AsSelector())
+	pods, err := e.podStore.Pods(service.Namespace).List(labels.Set(service.Spec.Selector).AsSelectorPreValidated())
 	if err != nil {
 		// Since we're getting stuff from a local cache, it is
 		// basically impossible to get this error.

pkg/controller/replicaset/replica_set.go
@@ -659,7 +659,7 @@ func (rsc *ReplicaSetController) syncReplicaSet(key string) error {
 	// part of the filteredPods.
 	fullyLabeledReplicasCount := 0
 	readyReplicasCount := 0
-	templateLabel := labels.Set(rs.Spec.Template.Labels).AsSelector()
+	templateLabel := labels.Set(rs.Spec.Template.Labels).AsSelectorPreValidated()
 	for _, pod := range filteredPods {
 		if templateLabel.Matches(labels.Set(pod.Labels)) {
 			fullyLabeledReplicasCount++

pkg/controller/replication/replication_controller.go
@@ -291,11 +291,10 @@ func isControllerMatch(pod *api.Pod, rc *api.ReplicationController) bool {
 	if rc.Namespace != pod.Namespace {
 		return false
 	}
-	labelSet := labels.Set(rc.Spec.Selector)
-	selector := labels.Set(rc.Spec.Selector).AsSelector()
+	selector := labels.Set(rc.Spec.Selector).AsSelectorPreValidated()
 
 	// If an rc with a nil or empty selector creeps in, it should match nothing, not everything.
-	if labelSet.AsSelector().Empty() || !selector.Matches(labels.Set(pod.Labels)) {
+	if selector.Empty() || !selector.Matches(labels.Set(pod.Labels)) {
 		return false
 	}
 	return true
@@ -662,7 +661,7 @@ func (rm *ReplicationManager) syncReplicationController(key string) error {
 			rm.queue.Add(key)
 			return err
 		}
-		cm := controller.NewPodControllerRefManager(rm.podControl, rc.ObjectMeta, labels.Set(rc.Spec.Selector).AsSelector(), getRCKind())
+		cm := controller.NewPodControllerRefManager(rm.podControl, rc.ObjectMeta, labels.Set(rc.Spec.Selector).AsSelectorPreValidated(), getRCKind())
 		matchesAndControlled, matchesNeedsController, controlledDoesNotMatch := cm.Classify(pods)
 		for _, pod := range matchesNeedsController {
 			err := cm.AdoptPod(pod)
@@ -694,7 +693,7 @@ func (rm *ReplicationManager) syncReplicationController(key string) error {
 			return aggregate
 		}
 	} else {
-		pods, err := rm.podStore.Pods(rc.Namespace).List(labels.Set(rc.Spec.Selector).AsSelector())
+		pods, err := rm.podStore.Pods(rc.Namespace).List(labels.Set(rc.Spec.Selector).AsSelectorPreValidated())
 		if err != nil {
 			glog.Errorf("Error getting pods for rc %q: %v", key, err)
 			rm.queue.Add(key)
@@ -716,7 +715,7 @@ func (rm *ReplicationManager) syncReplicationController(key string) error {
 	// matching pods must be part of the filteredPods.
 	fullyLabeledReplicasCount := 0
 	readyReplicasCount := 0
-	templateLabel := labels.Set(rc.Spec.Template.Labels).AsSelector()
+	templateLabel := labels.Set(rc.Spec.Template.Labels).AsSelectorPreValidated()
 	for _, pod := range filteredPods {
 		if templateLabel.Matches(labels.Set(pod.Labels)) {
 			fullyLabeledReplicasCount++

plugin/pkg/scheduler/algorithm/listers.go
@@ -85,7 +85,7 @@ func (f FakeServiceLister) GetPodServices(pod *api.Pod) (services []api.Service,
 		if service.Namespace != pod.Namespace {
 			continue
 		}
-		selector = labels.Set(service.Spec.Selector).AsSelector()
+		selector = labels.Set(service.Spec.Selector).AsSelectorPreValidated()
 		if selector.Matches(labels.Set(pod.Labels)) {
 			services = append(services, service)
 		}
@@ -134,7 +134,7 @@ func (f FakeControllerLister) GetPodControllers(pod *api.Pod) (controllers []api
 		if controller.Namespace != pod.Namespace {
 			continue
 		}
-		selector = labels.Set(controller.Spec.Selector).AsSelector()
+		selector = labels.Set(controller.Spec.Selector).AsSelectorPreValidated()
 		if selector.Matches(labels.Set(pod.Labels)) {
 			controllers = append(controllers, controller)
 		}