kubernetes/kubernetes

add an option to controller-manager to auto approve all CSRs

cmd/kube-controller-manager/app/controllermanager.go
@@ -443,6 +443,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 				resyncPeriod,
 				s.ClusterSigningCertFile,
 				s.ClusterSigningKeyFile,
+				s.ApproveAllCSRs,
 			)
 			if err != nil {
 				glog.Errorf("Failed to start certificate controller: %v", err)

cymtrick/kubernetes-error-fuzzer

cmd/kube-controller-manager/app/controllermanager.go
@@ -468,6 +468,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 				resyncPeriod,
 				s.ClusterSigningCertFile,
 				s.ClusterSigningKeyFile,
+				s.ApproveAllKubeletCSRsForGroup,
 			)
 			if err != nil {
 				glog.Errorf("Failed to start certificate controller: %v", err)