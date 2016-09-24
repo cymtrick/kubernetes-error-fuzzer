luxas/kubernetes

Initial version of kubeadm

pkg/kubeadm/master/discovery.go
@@ -1,0 +1,111 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package kubemaster
+
+import (
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
+	"k8s.io/kubernetes/pkg/kubeadm/tlsutil"
+)
+
+type kubeDiscovery struct {
+	Deployment *extensions.Deployment
+	Secret     *api.Secret
+}
+
+const (
+	kubeDiscoverynName      = "kube-discovery"
+	kubeDiscoverySecretName = "clusterinfo"
+)
+
+func encodeKubeDiscoverySecretData(params *kubeadmapi.BootstrapParams, caCert *x509.Certificate) map[string][]byte {
+	// TODO ListenIP is probably not the right now, although it's best we have right now
+	// if user provides a DNS name, or anything else, we should use that, may be it's really
+	// the list of all SANs (minus internal DNS names and service IP)?
+
+	var (
+		data         = map[string][]byte{}
+		endpointList = []string{}
+		tokenMap     = map[string]string{}
+	)
+
+	endpointList = append(endpointList, fmt.Sprintf("https://%s:443", params.Discovery.ListenIP))
+	tokenMap[params.Discovery.TokenID] = hex.EncodeToString(params.Discovery.Token)
+
+	data["endpoint-list.json"], _ = json.Marshal(endpointList)
+	data["token-map.json"], _ = json.Marshal(tokenMap)
+	data["ca.pem"] = tlsutil.EncodeCertificatePEM(caCert)
+
+	return data
+}
+
+func newKubeDiscoveryPodSpec(params *kubeadmapi.BootstrapParams) api.PodSpec {
+	return api.PodSpec{
+		SecurityContext: &api.PodSecurityContext{HostNetwork: true}, // TODO we should just use map it to a host port
+		Containers: []api.Container{{
+			Name:    kubeDiscoverynName,
+			Image:   params.EnvParams["discovery_image"],
+			Command: []string{"/usr/bin/kube-discovery"},
+			VolumeMounts: []api.VolumeMount{{
+				Name:      kubeDiscoverySecretName,
+				MountPath: "/tmp/secret", // TODO use a shared constant
+				ReadOnly:  true,
+			}},
+		}},
+		Volumes: []api.Volume{{
+			Name: kubeDiscoverySecretName,
+			VolumeSource: api.VolumeSource{
+				Secret: &api.SecretVolumeSource{SecretName: kubeDiscoverySecretName},
+			}},
+		},
+	}
+}
+
+func newKubeDiscovery(params *kubeadmapi.BootstrapParams, caCert *x509.Certificate) kubeDiscovery {
+	// TODO pin to master
+	return kubeDiscovery{
+		Deployment: NewDeployment(kubeDiscoverynName, 1, newKubeDiscoveryPodSpec(params)),
+		Secret: &api.Secret{
+			ObjectMeta: api.ObjectMeta{Name: kubeDiscoverySecretName},
+			Type:       api.SecretTypeOpaque,
+			Data:       encodeKubeDiscoverySecretData(params, caCert),
+		},
+	}
+}
+
+func CreateDiscoveryDeploymentAndSecret(params *kubeadmapi.BootstrapParams, client *clientset.Clientset, caCert *x509.Certificate) error {
+	kd := newKubeDiscovery(params, caCert)
+
+	if _, err := client.Extensions().Deployments(api.NamespaceSystem).Create(kd.Deployment); err != nil {
+		return fmt.Errorf("<master/discovery> failed to create %q deployment", kubeDiscoverynName)
+	}
+	if _, err := client.Secrets(api.NamespaceSystem).Create(kd.Secret); err != nil {
+		return fmt.Errorf("<master/discovery> failed to create %q secret", kubeDiscoverySecretName)
+	}
+
+	fmt.Println("<master/discovery> created essential addon: kube-discovery")
+
+	// TODO we should probably wait for the pod to become ready
+
+	return nil
+}

pkg/kubeadm/master/pki.go
@@ -1,0 +1,181 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubemaster
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"fmt"
+	"net"
+	"path"
+
+	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
+	"k8s.io/kubernetes/pkg/kubeadm/tlsutil"
+	"k8s.io/kubernetes/pkg/util/crypto"
+)
+
+/*
+func errorf(f string, err error, vargs ...string) error {
+	return fmt.Errorf("<master/pki> %s [%s]", fmt.Sprintf(f, v...), err)
+}
+*/
+
+func newCertificateAuthority() (*rsa.PrivateKey, *x509.Certificate, error) {
+	key, err := tlsutil.NewPrivateKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to create private key [%s]", err)
+	}
+
+	config := tlsutil.CertConfig{
+		CommonName: "kubernetes",
+	}
+
+	cert, err := tlsutil.NewSelfSignedCACertificate(config, key)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to create self-singed certificate [%s]", err)
+	}
+
+	return key, cert, nil
+}
+
+func newServerKeyAndCert(caCert *x509.Certificate, caKey *rsa.PrivateKey, altNames tlsutil.AltNames) (*rsa.PrivateKey, *x509.Certificate, error) {
+	key, err := tlsutil.NewPrivateKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unabel to create private key [%s]", err)
+	}
+	// TODO these are all hardcoded for now, but we need to figure out what shall we do here exactly
+	altNames.IPs = append(altNames.IPs, net.ParseIP("10.3.0.1"))
+	altNames.DNSNames = append(altNames.DNSNames,
+		"kubernetes",
+		"kubernetes.default",
+		"kubernetes.default.svc",
+		"kubernetes.default.svc.cluster.local",
+	)
+
+	config := tlsutil.CertConfig{
+		CommonName: "kube-apiserver",
+		AltNames:   altNames,
+	}
+	cert, err := tlsutil.NewSignedCertificate(config, key, caCert, caKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to sing certificate [%s]", err)
+	}
+
+	return key, cert, nil
+}
+
+func newClientKeyAndCert(caCert *x509.Certificate, caKey *rsa.PrivateKey) (*rsa.PrivateKey, *x509.Certificate, error) {
+	key, err := tlsutil.NewPrivateKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to create private key [%s]", err)
+	}
+
+	config := tlsutil.CertConfig{
+		CommonName: "kubernetes-admin",
+	}
+	cert, err := tlsutil.NewSignedCertificate(config, key, caCert, caKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to sign certificate [%s]", err)
+	}
+
+	return key, cert, nil
+}
+
+func writeKeysAndCert(pkiPath string, name string, key *rsa.PrivateKey, cert *x509.Certificate) error {
+	var (
+		publicKeyPath   = path.Join(pkiPath, fmt.Sprintf("%s-pub.pem", name))
+		privateKeyPath  = path.Join(pkiPath, fmt.Sprintf("%s-key.pem", name))
+		certificatePath = path.Join(pkiPath, fmt.Sprintf("%s.pem", name))
+	)
+
+	if key != nil {
+		if err := crypto.WriteKeyToPath(privateKeyPath, tlsutil.EncodePrivateKeyPEM(key)); err != nil {
+			return fmt.Errorf("unable to write private key file (%q) [%s]", privateKeyPath, err)
+		}
+		if pubKey, err := tlsutil.EncodePublicKeyPEM(&key.PublicKey); err == nil {
+			if err := crypto.WriteKeyToPath(publicKeyPath, pubKey); err != nil {
+				return fmt.Errorf("unable to write public key file (%q) [%s]", publicKeyPath, err)
+			}
+		} else {
+			return fmt.Errorf("unable to encode public key to PEM [%s]", err)
+		}
+	}
+
+	if cert != nil {
+		if err := crypto.WriteCertToPath(certificatePath, tlsutil.EncodeCertificatePEM(cert)); err != nil {
+			return fmt.Errorf("unable to write certificate file (%q) [%s]", err)
+		}
+	}
+
+	return nil
+}
+
+func newServiceAccountKey() (*rsa.PrivateKey, error) {
+	key, err := tlsutil.NewPrivateKey()
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func CreatePKIAssets(params *kubeadmapi.BootstrapParams) (*rsa.PrivateKey, *x509.Certificate, error) {
+	var (
+		err      error
+		altNames tlsutil.AltNames // TODO actual SANs
+	)
+
+	if params.Discovery.ListenIP != "" {
+		altNames.IPs = append(altNames.IPs, net.ParseIP(params.Discovery.ListenIP))
+	}
+
+	if params.Discovery.ApiServerDNSName != "" {
+		altNames.DNSNames = append(altNames.DNSNames, params.Discovery.ApiServerDNSName)
+	}
+
+	pkiPath := path.Join(params.EnvParams["host_pki_path"])
+
+	caKey, caCert, err := newCertificateAuthority()
+	if err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while creating CA keys and certificate - %s", err)
+	}
+
+	if err := writeKeysAndCert(pkiPath, "ca", caKey, caCert); err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while saving CA keys and certificate - %s", err)
+	}
+
+	apiKey, apiCert, err := newServerKeyAndCert(caCert, caKey, altNames)
+	if err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while creating API server keys and certificate - %s", err)
+	}
+
+	if err := writeKeysAndCert(pkiPath, "apiserver", apiKey, apiCert); err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while saving API server keys and certificate - %s", err)
+	}
+
+	saKey, err := newServiceAccountKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while creating service account signing keys [%s]", err)
+	}
+
+	if err := writeKeysAndCert(pkiPath, "sa", saKey, nil); err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while saving service account singing keys - %s", err)
+	}
+
+	// TODO print a summary of SANs used and checksums (signatures) of each of the certiicates
+	fmt.Println("<master/pki> created keys and certificates in %q", params.EnvParams["host_pki_path"])
+	return caKey, caCert, nil
+}

cymtrick/kubernetes-error-fuzzer

pkg/kubeadm/master/discovery.go
@@ -1,0 +1,111 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package kubemaster
+
+import (
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
+	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
+	certutil "k8s.io/kubernetes/pkg/util/cert"
+)
+
+type kubeDiscovery struct {
+	Deployment *extensions.Deployment
+	Secret     *api.Secret
+}
+
+const (
+	kubeDiscoverynName      = "kube-discovery"
+	kubeDiscoverySecretName = "clusterinfo"
+)
+
+func encodeKubeDiscoverySecretData(params *kubeadmapi.BootstrapParams, caCert *x509.Certificate) map[string][]byte {
+	// TODO ListenIP is probably not the right now, although it's best we have right now
+	// if user provides a DNS name, or anything else, we should use that, may be it's really
+	// the list of all SANs (minus internal DNS names and service IP)?
+
+	var (
+		data         = map[string][]byte{}
+		endpointList = []string{}
+		tokenMap     = map[string]string{}
+	)
+
+	endpointList = append(endpointList, fmt.Sprintf("https://%s:443", params.Discovery.ListenIP))
+	tokenMap[params.Discovery.TokenID] = hex.EncodeToString(params.Discovery.Token)
+
+	data["endpoint-list.json"], _ = json.Marshal(endpointList)
+	data["token-map.json"], _ = json.Marshal(tokenMap)
+	data["ca.pem"] = certutil.EncodeCertPEM(caCert)
+
+	return data
+}
+
+func newKubeDiscoveryPodSpec(params *kubeadmapi.BootstrapParams) api.PodSpec {
+	return api.PodSpec{
+		SecurityContext: &api.PodSecurityContext{HostNetwork: true}, // TODO we should just use map it to a host port
+		Containers: []api.Container{{
+			Name:    kubeDiscoverynName,
+			Image:   params.EnvParams["discovery_image"],
+			Command: []string{"/usr/bin/kube-discovery"},
+			VolumeMounts: []api.VolumeMount{{
+				Name:      kubeDiscoverySecretName,
+				MountPath: "/tmp/secret", // TODO use a shared constant
+				ReadOnly:  true,
+			}},
+		}},
+		Volumes: []api.Volume{{
+			Name: kubeDiscoverySecretName,
+			VolumeSource: api.VolumeSource{
+				Secret: &api.SecretVolumeSource{SecretName: kubeDiscoverySecretName},
+			}},
+		},
+	}
+}
+
+func newKubeDiscovery(params *kubeadmapi.BootstrapParams, caCert *x509.Certificate) kubeDiscovery {
+	// TODO pin to master
+	return kubeDiscovery{
+		Deployment: NewDeployment(kubeDiscoverynName, 1, newKubeDiscoveryPodSpec(params)),
+		Secret: &api.Secret{
+			ObjectMeta: api.ObjectMeta{Name: kubeDiscoverySecretName},
+			Type:       api.SecretTypeOpaque,
+			Data:       encodeKubeDiscoverySecretData(params, caCert),
+		},
+	}
+}
+
+func CreateDiscoveryDeploymentAndSecret(params *kubeadmapi.BootstrapParams, client *clientset.Clientset, caCert *x509.Certificate) error {
+	kd := newKubeDiscovery(params, caCert)
+
+	if _, err := client.Extensions().Deployments(api.NamespaceSystem).Create(kd.Deployment); err != nil {
+		return fmt.Errorf("<master/discovery> failed to create %q deployment", kubeDiscoverynName)
+	}
+	if _, err := client.Secrets(api.NamespaceSystem).Create(kd.Secret); err != nil {
+		return fmt.Errorf("<master/discovery> failed to create %q secret", kubeDiscoverySecretName)
+	}
+
+	fmt.Println("<master/discovery> created essential addon: kube-discovery")
+
+	// TODO we should probably wait for the pod to become ready
+
+	return nil
+}

pkg/kubeadm/master/pki.go
@@ -1,0 +1,180 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubemaster
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"fmt"
+	"net"
+	"path"
+
+	kubeadmapi "k8s.io/kubernetes/pkg/kubeadm/api"
+	certutil "k8s.io/kubernetes/pkg/util/cert"
+)
+
+/*
+func errorf(f string, err error, vargs ...string) error {
+	return fmt.Errorf("<master/pki> %s [%s]", fmt.Sprintf(f, v...), err)
+}
+*/
+
+func newCertificateAuthority() (*rsa.PrivateKey, *x509.Certificate, error) {
+	key, err := certutil.NewPrivateKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to create private key [%s]", err)
+	}
+
+	config := certutil.CertConfig{
+		CommonName: "kubernetes",
+	}
+
+	cert, err := certutil.NewSelfSignedCACert(config, key)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to create self-singed certificate [%s]", err)
+	}
+
+	return key, cert, nil
+}
+
+func newServerKeyAndCert(caCert *x509.Certificate, caKey *rsa.PrivateKey, altNames certutil.AltNames) (*rsa.PrivateKey, *x509.Certificate, error) {
+	key, err := certutil.NewPrivateKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unabel to create private key [%s]", err)
+	}
+	// TODO these are all hardcoded for now, but we need to figure out what shall we do here exactly
+	altNames.IPs = append(altNames.IPs, net.ParseIP("10.3.0.1"))
+	altNames.DNSNames = append(altNames.DNSNames,
+		"kubernetes",
+		"kubernetes.default",
+		"kubernetes.default.svc",
+		"kubernetes.default.svc.cluster.local",
+	)
+
+	config := certutil.CertConfig{
+		CommonName: "kube-apiserver",
+		AltNames:   altNames,
+	}
+	cert, err := certutil.NewSignedCert(config, key, caCert, caKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to sing certificate [%s]", err)
+	}
+
+	return key, cert, nil
+}
+
+func newClientKeyAndCert(caCert *x509.Certificate, caKey *rsa.PrivateKey) (*rsa.PrivateKey, *x509.Certificate, error) {
+	key, err := certutil.NewPrivateKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to create private key [%s]", err)
+	}
+
+	config := certutil.CertConfig{
+		CommonName: "kubernetes-admin",
+	}
+	cert, err := certutil.NewSignedCert(config, key, caCert, caKey)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to sign certificate [%s]", err)
+	}
+
+	return key, cert, nil
+}
+
+func writeKeysAndCert(pkiPath string, name string, key *rsa.PrivateKey, cert *x509.Certificate) error {
+	var (
+		publicKeyPath   = path.Join(pkiPath, fmt.Sprintf("%s-pub.pem", name))
+		privateKeyPath  = path.Join(pkiPath, fmt.Sprintf("%s-key.pem", name))
+		certificatePath = path.Join(pkiPath, fmt.Sprintf("%s.pem", name))
+	)
+
+	if key != nil {
+		if err := certutil.WriteKey(privateKeyPath, certutil.EncodePrivateKeyPEM(key)); err != nil {
+			return fmt.Errorf("unable to write private key file (%q) [%s]", privateKeyPath, err)
+		}
+		if pubKey, err := certutil.EncodePublicKeyPEM(&key.PublicKey); err == nil {
+			if err := certutil.WriteKey(publicKeyPath, pubKey); err != nil {
+				return fmt.Errorf("unable to write public key file (%q) [%s]", publicKeyPath, err)
+			}
+		} else {
+			return fmt.Errorf("unable to encode public key to PEM [%s]", err)
+		}
+	}
+
+	if cert != nil {
+		if err := certutil.WriteCert(certificatePath, certutil.EncodeCertPEM(cert)); err != nil {
+			return fmt.Errorf("unable to write certificate file (%q) [%s]", err)
+		}
+	}
+
+	return nil
+}
+
+func newServiceAccountKey() (*rsa.PrivateKey, error) {
+	key, err := certutil.NewPrivateKey()
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func CreatePKIAssets(params *kubeadmapi.BootstrapParams) (*rsa.PrivateKey, *x509.Certificate, error) {
+	var (
+		err      error
+		altNames certutil.AltNames // TODO actual SANs
+	)
+
+	if params.Discovery.ListenIP != "" {
+		altNames.IPs = append(altNames.IPs, net.ParseIP(params.Discovery.ListenIP))
+	}
+
+	if params.Discovery.ApiServerDNSName != "" {
+		altNames.DNSNames = append(altNames.DNSNames, params.Discovery.ApiServerDNSName)
+	}
+
+	pkiPath := path.Join(params.EnvParams["host_pki_path"])
+
+	caKey, caCert, err := newCertificateAuthority()
+	if err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while creating CA keys and certificate - %s", err)
+	}
+
+	if err := writeKeysAndCert(pkiPath, "ca", caKey, caCert); err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while saving CA keys and certificate - %s", err)
+	}
+
+	apiKey, apiCert, err := newServerKeyAndCert(caCert, caKey, altNames)
+	if err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while creating API server keys and certificate - %s", err)
+	}
+
+	if err := writeKeysAndCert(pkiPath, "apiserver", apiKey, apiCert); err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while saving API server keys and certificate - %s", err)
+	}
+
+	saKey, err := newServiceAccountKey()
+	if err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while creating service account signing keys [%s]", err)
+	}
+
+	if err := writeKeysAndCert(pkiPath, "sa", saKey, nil); err != nil {
+		return nil, nil, fmt.Errorf("<master/pki> failure while saving service account singing keys - %s", err)
+	}
+
+	// TODO print a summary of SANs used and checksums (signatures) of each of the certiicates
+	fmt.Println("<master/pki> created keys and certificates in %q", params.EnvParams["host_pki_path"])
+	return caKey, caCert, nil
+}