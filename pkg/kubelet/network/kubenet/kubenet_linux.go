kubernetes/kubernetes

kubenet: make it more apparent that kubenet ignores PodCIDR updates
https://github.com/kubernetes/kubernetes/issues/32900

pkg/kubelet/network/kubenet/kubenet_linux.go
@@ -240,7 +240,7 @@ func (plugin *kubenetNetworkPlugin) Event(name string, details map[string]interf
 	}
 
 	if plugin.netConfig != nil {
-		glog.V(5).Infof("Ignoring subsequent pod CIDR update to %s", podCIDR)
+		glog.Infof("Ignoring subsequent pod CIDR update to %s", podCIDR)
 		return
 	}
 

cymtrick/kubernetes-error-fuzzer

pkg/kubelet/network/kubenet/kubenet_linux.go
@@ -241,7 +241,7 @@ func (plugin *kubenetNetworkPlugin) Event(name string, details map[string]interf
 	}
 
 	if plugin.netConfig != nil {
-		glog.V(5).Infof("Ignoring subsequent pod CIDR update to %s", podCIDR)
+		glog.Warningf("Ignoring subsequent pod CIDR update to %s", podCIDR)
 		return
 	}
 