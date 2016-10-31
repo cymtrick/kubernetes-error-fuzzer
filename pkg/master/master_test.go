kubernetes/kubernetes

remove non-reuseable bits of MasterServer

pkg/master/master_test.go
@@ -102,9 +102,6 @@ func setUp(t *testing.T) (*Master, *etcdtesting.EtcdTestServer, Config, *assert.
 		t.Fatal(err)
 	}
 
-	fakeNodeClient := fake.NewSimpleClientset(registrytest.MakeNodeList([]string{"node1", "node2"}, api.NodeResources{}))
-	master.nodeClient = fakeNodeClient.Core().Nodes()
-
 	return master, server, *config, assert.New(t)
 }
 
@@ -181,23 +178,6 @@ func TestVersion(t *testing.T) {
 	}
 }
 
-// TestGetServersToValidate verifies the unexported getServersToValidate function
-func TestGetServersToValidate(t *testing.T) {
-	_, etcdserver, config, assert := setUp(t)
-	defer etcdserver.Terminate(t)
-
-	servers := getServersToValidate(config.StorageFactory)
-
-	// Expected servers to validate: scheduler, controller-manager and etcd.
-	assert.Equal(3, len(servers), "unexpected server list: %#v", servers)
-
-	for _, server := range []string{"scheduler", "controller-manager", "etcd-0"} {
-		if _, ok := servers[server]; !ok {
-			t.Errorf("server list missing: %s", server)
-		}
-	}
-}
-
 // TestFindExternalAddress verifies both pass and fail cases for the unexported
 // findExternalAddress function
 func TestFindExternalAddress(t *testing.T) {
@@ -230,34 +210,36 @@ func (*fakeEndpointReconciler) ReconcileEndpoints(serviceName string, ip net.IP,
 // TestGetNodeAddresses verifies that proper results are returned
 // when requesting node addresses.
 func TestGetNodeAddresses(t *testing.T) {
-	master, etcdserver, _, assert := setUp(t)
-	defer etcdserver.Terminate(t)
+	assert := assert.New(t)
+
+	fakeNodeClient := fake.NewSimpleClientset(registrytest.MakeNodeList([]string{"node1", "node2"}, api.NodeResources{})).Core().Nodes()
+	addressProvider := nodeAddressProvider{fakeNodeClient}
 
 	// Fail case (no addresses associated with nodes)
-	nodes, _ := master.nodeClient.List(api.ListOptions{})
-	addrs, err := master.getNodeAddresses()
+	nodes, _ := fakeNodeClient.List(api.ListOptions{})
+	addrs, err := addressProvider.addresses()
 
-	assert.Error(err, "getNodeAddresses should have caused an error as there are no addresses.")
+	assert.Error(err, "addresses should have caused an error as there are no addresses.")
 	assert.Equal([]string(nil), addrs)
 
 	// Pass case with External type IP
-	nodes, _ = master.nodeClient.List(api.ListOptions{})
+	nodes, _ = fakeNodeClient.List(api.ListOptions{})
 	for index := range nodes.Items {
 		nodes.Items[index].Status.Addresses = []api.NodeAddress{{Type: api.NodeExternalIP, Address: "127.0.0.1"}}
-		master.nodeClient.Update(&nodes.Items[index])
+		fakeNodeClient.Update(&nodes.Items[index])
 	}
-	addrs, err = master.getNodeAddresses()
-	assert.NoError(err, "getNodeAddresses should not have returned an error.")
+	addrs, err = addressProvider.addresses()
+	assert.NoError(err, "addresses should not have returned an error.")
 	assert.Equal([]string{"127.0.0.1", "127.0.0.1"}, addrs)
 
 	// Pass case with LegacyHost type IP
-	nodes, _ = master.nodeClient.List(api.ListOptions{})
+	nodes, _ = fakeNodeClient.List(api.ListOptions{})
 	for index := range nodes.Items {
 		nodes.Items[index].Status.Addresses = []api.NodeAddress{{Type: api.NodeLegacyHostIP, Address: "127.0.0.2"}}
-		master.nodeClient.Update(&nodes.Items[index])
+		fakeNodeClient.Update(&nodes.Items[index])
 	}
-	addrs, err = master.getNodeAddresses()
-	assert.NoError(err, "getNodeAddresses failback should not have returned an error.")
+	addrs, err = addressProvider.addresses()
+	assert.NoError(err, "addresses failback should not have returned an error.")
 	assert.Equal([]string{"127.0.0.2", "127.0.0.2"}, addrs)
 }
 
@@ -348,126 +330,6 @@ func TestAPIVersionOfDiscoveryEndpoints(t *testing.T) {
 	assert.NoError(decodeResponse(resp, &resourceList))
 	assert.Equal(resourceList.APIVersion, "v1")
 
-}
-
-func TestDiscoveryAtAPIS(t *testing.T) {
-	master, etcdserver, _, assert := newLimitedMaster(t)
-	defer etcdserver.Terminate(t)
-
-	server := httptest.NewServer(master.GenericAPIServer.HandlerContainer.ServeMux)
-	resp, err := http.Get(server.URL + "/apis")
-	if !assert.NoError(err) {
-		t.Errorf("unexpected error: %v", err)
-	}
-
-	assert.Equal(http.StatusOK, resp.StatusCode)
-
-	groupList := unversioned.APIGroupList{}
-	assert.NoError(decodeResponse(resp, &groupList))
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	expectGroupNames := sets.NewString(autoscaling.GroupName, batch.GroupName, apps.GroupName, extensions.GroupName)
-	expectVersions := map[string][]unversioned.GroupVersionForDiscovery{
-		autoscaling.GroupName: {
-			{
-				GroupVersion: testapi.Autoscaling.GroupVersion().String(),
-				Version:      testapi.Autoscaling.GroupVersion().Version,
-			},
-		},
-		// batch is using its pkg/apis/batch/ types here since during installation
-		// both versions get installed and testapi.go currently does not support
-		// multi-versioned clients
-		batch.GroupName: {
-			{
-				GroupVersion: batchapiv1.SchemeGroupVersion.String(),
-				Version:      batchapiv1.SchemeGroupVersion.Version,
-			},
-			{
-				GroupVersion: batchapiv2alpha1.SchemeGroupVersion.String(),
-				Version:      batchapiv2alpha1.SchemeGroupVersion.Version,
-			},
-		},
-		apps.GroupName: {
-			{
-				GroupVersion: testapi.Apps.GroupVersion().String(),
-				Version:      testapi.Apps.GroupVersion().Version,
-			},
-		},
-		extensions.GroupName: {
-			{
-				GroupVersion: testapi.Extensions.GroupVersion().String(),
-				Version:      testapi.Extensions.GroupVersion().Version,
-			},
-		},
-	}
-	expectPreferredVersion := map[string]unversioned.GroupVersionForDiscovery{
-		autoscaling.GroupName: {
-			GroupVersion: registered.GroupOrDie(autoscaling.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(autoscaling.GroupName).GroupVersion.Version,
-		},
-		batch.GroupName: {
-			GroupVersion: registered.GroupOrDie(batch.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(batch.GroupName).GroupVersion.Version,
-		},
-		apps.GroupName: {
-			GroupVersion: registered.GroupOrDie(apps.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(apps.GroupName).GroupVersion.Version,
-		},
-		extensions.GroupName: {
-			GroupVersion: registered.GroupOrDie(extensions.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(extensions.GroupName).GroupVersion.Version,
-		},
-	}
-
-	assert.Equal(4, len(groupList.Groups))
-	for _, group := range groupList.Groups {
-		if !expectGroupNames.Has(group.Name) {
-			t.Errorf("got unexpected group %s", group.Name)
-		}
-		assert.Equal(expectVersions[group.Name], group.Versions)
-		assert.Equal(expectPreferredVersion[group.Name], group.PreferredVersion)
-	}
-
-	thirdPartyGV := unversioned.GroupVersionForDiscovery{GroupVersion: "company.com/v1", Version: "v1"}
-	master.thirdPartyResourceServer.InstallThirdPartyResource(&extensions.ThirdPartyResource{
-		ObjectMeta: api.ObjectMeta{Name: "foo.company.com"},
-		Versions:   []extensions.APIVersion{{Name: "v1"}},
-	})
-
-	resp, err = http.Get(server.URL + "/apis")
-	if !assert.NoError(err) {
-		t.Errorf("unexpected error: %v", err)
-	}
-	assert.Equal(http.StatusOK, resp.StatusCode)
-	assert.NoError(decodeResponse(resp, &groupList))
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	assert.Equal(5, len(groupList.Groups))
-
-	expectGroupNames.Insert("company.com")
-	expectVersions["company.com"] = []unversioned.GroupVersionForDiscovery{thirdPartyGV}
-	expectPreferredVersion["company.com"] = thirdPartyGV
-	for _, group := range groupList.Groups {
-		if !expectGroupNames.Has(group.Name) {
-			t.Errorf("got unexpected group %s", group.Name)
-		}
-		assert.Equal(expectVersions[group.Name], group.Versions)
-		assert.Equal(expectPreferredVersion[group.Name], group.PreferredVersion)
-	}
-}
-
-func writeResponseToFile(resp *http.Response, filename string) error {
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(filename, data, 0755)
 }
 
 // TestValidOpenAPISpec verifies that the open api is added

pkg/registry/core/rest/storage_core.go
@@ -20,7 +20,12 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/golang/glog"
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/rest"
@@ -31,6 +36,7 @@ import (
 	"k8s.io/kubernetes/pkg/client/restclient"
 	"k8s.io/kubernetes/pkg/genericapiserver"
 	kubeletclient "k8s.io/kubernetes/pkg/kubelet/client"
+	"k8s.io/kubernetes/pkg/master/ports"
 	"k8s.io/kubernetes/pkg/registry/core/componentstatus"
 	configmapetcd "k8s.io/kubernetes/pkg/registry/core/configmap/etcd"
 	controlleretcd "k8s.io/kubernetes/pkg/registry/core/controller/etcd"
@@ -56,6 +62,7 @@ import (
 	ipallocator "k8s.io/kubernetes/pkg/registry/core/service/ipallocator"
 	"k8s.io/kubernetes/pkg/registry/core/service/portallocator"
 	serviceaccountetcd "k8s.io/kubernetes/pkg/registry/core/serviceaccount/etcd"
+	etcdutil "k8s.io/kubernetes/pkg/storage/etcd/util"
 	utilnet "k8s.io/kubernetes/pkg/util/net"
 )
 
@@ -72,13 +79,8 @@ type LegacyRESTStorageProvider struct {
 	ServiceClusterIPRange *net.IPNet
 	ServiceNodePortRange  utilnet.PortRange
 
-	// ComponentStatusServerFunc is a func used to locate servers to back component status
-	ComponentStatusServerFunc ComponentStatusServerFunc
-
 	LoopbackClientConfig *restclient.Config
 }
-
-type ComponentStatusServerFunc func() map[string]apiserver.Server
 
 // LegacyRESTStorage returns stateful information about particular instances of REST storage to
 // master.go for wiring controllers.
@@ -227,7 +229,7 @@ func (c LegacyRESTStorageProvider) NewLegacyRESTStorage(restOptionsGetter generi
 		"persistentVolumeClaims/status": persistentVolumeClaimStatusStorage,
 		"configMaps":                    configMapStorage,
 
-		"componentStatuses": componentstatus.NewStorage(c.ComponentStatusServerFunc),
+		"componentStatuses": componentstatus.NewStorage(componentStatusStorage{c.StorageFactory}.getServersToValidate),
 	}
 	if registered.IsEnabledVersion(unversioned.GroupVersion{Group: "autoscaling", Version: "v1"}) {
 		restStorageMap["replicationControllers/scale"] = controllerStorage.Scale
@@ -238,4 +240,50 @@ func (c LegacyRESTStorageProvider) NewLegacyRESTStorage(restOptionsGetter generi
 	apiGroupInfo.VersionedResourcesStorageMap["v1"] = restStorageMap
 
 	return restStorage, apiGroupInfo, nil
+}
+
+func (p LegacyRESTStorageProvider) GroupName() string {
+	return api.GroupName
+}
+
+type componentStatusStorage struct {
+	storageFactory genericapiserver.StorageFactory
+}
+
+func (s componentStatusStorage) getServersToValidate() map[string]apiserver.Server {
+	serversToValidate := map[string]apiserver.Server{
+		"controller-manager": {Addr: "127.0.0.1", Port: ports.ControllerManagerPort, Path: "/healthz"},
+		"scheduler":          {Addr: "127.0.0.1", Port: ports.SchedulerPort, Path: "/healthz"},
+	}
+
+	for ix, machine := range s.storageFactory.Backends() {
+		etcdUrl, err := url.Parse(machine)
+		if err != nil {
+			glog.Errorf("Failed to parse etcd url for validation: %v", err)
+			continue
+		}
+		var port int
+		var addr string
+		if strings.Contains(etcdUrl.Host, ":") {
+			var portString string
+			addr, portString, err = net.SplitHostPort(etcdUrl.Host)
+			if err != nil {
+				glog.Errorf("Failed to split host/port: %s (%v)", etcdUrl.Host, err)
+				continue
+			}
+			port, _ = strconv.Atoi(portString)
+		} else {
+			addr = etcdUrl.Host
+			port = 2379
+		}
+		// TODO: etcd health checking should be abstracted in the storage tier
+		serversToValidate[fmt.Sprintf("etcd-%d", ix)] = apiserver.Server{
+			Addr:        addr,
+			EnableHTTPS: etcdUrl.Scheme == "https",
+			Port:        port,
+			Path:        "/health",
+			Validate:    etcdutil.EtcdHealthCheck,
+		}
+	}
+	return serversToValidate
 }

pkg/registry/core/rest/storage_core_test.go
@@ -1,0 +1,52 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rest
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/storage/storagebackend"
+)
+
+func TestGetServersToValidate(t *testing.T) {
+	servers := componentStatusStorage{fakeStorageFactory{}}.getServersToValidate()
+
+	if e, a := 3, len(servers); e != a {
+		t.Errorf("expected %v, got %v", e, a)
+	}
+
+	for _, server := range []string{"scheduler", "controller-manager", "etcd-0"} {
+		if _, ok := servers[server]; !ok {
+			t.Errorf("server list missing: %s", server)
+		}
+	}
+}
+
+type fakeStorageFactory struct{}
+
+func (f fakeStorageFactory) NewConfig(groupResource unversioned.GroupResource) (*storagebackend.Config, error) {
+	return nil, nil
+}
+
+func (f fakeStorageFactory) ResourcePrefix(groupResource unversioned.GroupResource) string {
+	return ""
+}
+
+func (f fakeStorageFactory) Backends() []string {
+	return []string{"etcd-0"}
+}

cymtrick/kubernetes-error-fuzzer

pkg/master/master_test.go
@@ -102,9 +102,6 @@ func setUp(t *testing.T) (*Master, *etcdtesting.EtcdTestServer, Config, *assert.
 		t.Fatal(err)
 	}
 
-	fakeNodeClient := fake.NewSimpleClientset(registrytest.MakeNodeList([]string{"node1", "node2"}, api.NodeResources{}))
-	master.nodeClient = fakeNodeClient.Core().Nodes()
-
 	return master, server, *config, assert.New(t)
 }
 
@@ -181,23 +178,6 @@ func TestVersion(t *testing.T) {
 	}
 }
 
-// TestGetServersToValidate verifies the unexported getServersToValidate function
-func TestGetServersToValidate(t *testing.T) {
-	_, etcdserver, config, assert := setUp(t)
-	defer etcdserver.Terminate(t)
-
-	servers := getServersToValidate(config.StorageFactory)
-
-	// Expected servers to validate: scheduler, controller-manager and etcd.
-	assert.Equal(3, len(servers), "unexpected server list: %#v", servers)
-
-	for _, server := range []string{"scheduler", "controller-manager", "etcd-0"} {
-		if _, ok := servers[server]; !ok {
-			t.Errorf("server list missing: %s", server)
-		}
-	}
-}
-
 // TestFindExternalAddress verifies both pass and fail cases for the unexported
 // findExternalAddress function
 func TestFindExternalAddress(t *testing.T) {
@@ -230,34 +210,36 @@ func (*fakeEndpointReconciler) ReconcileEndpoints(serviceName string, ip net.IP,
 // TestGetNodeAddresses verifies that proper results are returned
 // when requesting node addresses.
 func TestGetNodeAddresses(t *testing.T) {
-	master, etcdserver, _, assert := setUp(t)
-	defer etcdserver.Terminate(t)
+	assert := assert.New(t)
+
+	fakeNodeClient := fake.NewSimpleClientset(registrytest.MakeNodeList([]string{"node1", "node2"}, api.NodeResources{})).Core().Nodes()
+	addressProvider := nodeAddressProvider{fakeNodeClient}
 
 	// Fail case (no addresses associated with nodes)
-	nodes, _ := master.nodeClient.List(api.ListOptions{})
-	addrs, err := master.getNodeAddresses()
+	nodes, _ := fakeNodeClient.List(api.ListOptions{})
+	addrs, err := addressProvider.externalAddresses()
 
-	assert.Error(err, "getNodeAddresses should have caused an error as there are no addresses.")
+	assert.Error(err, "addresses should have caused an error as there are no addresses.")
 	assert.Equal([]string(nil), addrs)
 
 	// Pass case with External type IP
-	nodes, _ = master.nodeClient.List(api.ListOptions{})
+	nodes, _ = fakeNodeClient.List(api.ListOptions{})
 	for index := range nodes.Items {
 		nodes.Items[index].Status.Addresses = []api.NodeAddress{{Type: api.NodeExternalIP, Address: "127.0.0.1"}}
-		master.nodeClient.Update(&nodes.Items[index])
+		fakeNodeClient.Update(&nodes.Items[index])
 	}
-	addrs, err = master.getNodeAddresses()
-	assert.NoError(err, "getNodeAddresses should not have returned an error.")
+	addrs, err = addressProvider.externalAddresses()
+	assert.NoError(err, "addresses should not have returned an error.")
 	assert.Equal([]string{"127.0.0.1", "127.0.0.1"}, addrs)
 
 	// Pass case with LegacyHost type IP
-	nodes, _ = master.nodeClient.List(api.ListOptions{})
+	nodes, _ = fakeNodeClient.List(api.ListOptions{})
 	for index := range nodes.Items {
 		nodes.Items[index].Status.Addresses = []api.NodeAddress{{Type: api.NodeLegacyHostIP, Address: "127.0.0.2"}}
-		master.nodeClient.Update(&nodes.Items[index])
+		fakeNodeClient.Update(&nodes.Items[index])
 	}
-	addrs, err = master.getNodeAddresses()
-	assert.NoError(err, "getNodeAddresses failback should not have returned an error.")
+	addrs, err = addressProvider.externalAddresses()
+	assert.NoError(err, "addresses failback should not have returned an error.")
 	assert.Equal([]string{"127.0.0.2", "127.0.0.2"}, addrs)
 }
 
@@ -348,126 +330,6 @@ func TestAPIVersionOfDiscoveryEndpoints(t *testing.T) {
 	assert.NoError(decodeResponse(resp, &resourceList))
 	assert.Equal(resourceList.APIVersion, "v1")
 
-}
-
-func TestDiscoveryAtAPIS(t *testing.T) {
-	master, etcdserver, _, assert := newLimitedMaster(t)
-	defer etcdserver.Terminate(t)
-
-	server := httptest.NewServer(master.GenericAPIServer.HandlerContainer.ServeMux)
-	resp, err := http.Get(server.URL + "/apis")
-	if !assert.NoError(err) {
-		t.Errorf("unexpected error: %v", err)
-	}
-
-	assert.Equal(http.StatusOK, resp.StatusCode)
-
-	groupList := unversioned.APIGroupList{}
-	assert.NoError(decodeResponse(resp, &groupList))
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	expectGroupNames := sets.NewString(autoscaling.GroupName, batch.GroupName, apps.GroupName, extensions.GroupName)
-	expectVersions := map[string][]unversioned.GroupVersionForDiscovery{
-		autoscaling.GroupName: {
-			{
-				GroupVersion: testapi.Autoscaling.GroupVersion().String(),
-				Version:      testapi.Autoscaling.GroupVersion().Version,
-			},
-		},
-		// batch is using its pkg/apis/batch/ types here since during installation
-		// both versions get installed and testapi.go currently does not support
-		// multi-versioned clients
-		batch.GroupName: {
-			{
-				GroupVersion: batchapiv1.SchemeGroupVersion.String(),
-				Version:      batchapiv1.SchemeGroupVersion.Version,
-			},
-			{
-				GroupVersion: batchapiv2alpha1.SchemeGroupVersion.String(),
-				Version:      batchapiv2alpha1.SchemeGroupVersion.Version,
-			},
-		},
-		apps.GroupName: {
-			{
-				GroupVersion: testapi.Apps.GroupVersion().String(),
-				Version:      testapi.Apps.GroupVersion().Version,
-			},
-		},
-		extensions.GroupName: {
-			{
-				GroupVersion: testapi.Extensions.GroupVersion().String(),
-				Version:      testapi.Extensions.GroupVersion().Version,
-			},
-		},
-	}
-	expectPreferredVersion := map[string]unversioned.GroupVersionForDiscovery{
-		autoscaling.GroupName: {
-			GroupVersion: registered.GroupOrDie(autoscaling.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(autoscaling.GroupName).GroupVersion.Version,
-		},
-		batch.GroupName: {
-			GroupVersion: registered.GroupOrDie(batch.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(batch.GroupName).GroupVersion.Version,
-		},
-		apps.GroupName: {
-			GroupVersion: registered.GroupOrDie(apps.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(apps.GroupName).GroupVersion.Version,
-		},
-		extensions.GroupName: {
-			GroupVersion: registered.GroupOrDie(extensions.GroupName).GroupVersion.String(),
-			Version:      registered.GroupOrDie(extensions.GroupName).GroupVersion.Version,
-		},
-	}
-
-	assert.Equal(4, len(groupList.Groups))
-	for _, group := range groupList.Groups {
-		if !expectGroupNames.Has(group.Name) {
-			t.Errorf("got unexpected group %s", group.Name)
-		}
-		assert.Equal(expectVersions[group.Name], group.Versions)
-		assert.Equal(expectPreferredVersion[group.Name], group.PreferredVersion)
-	}
-
-	thirdPartyGV := unversioned.GroupVersionForDiscovery{GroupVersion: "company.com/v1", Version: "v1"}
-	master.thirdPartyResourceServer.InstallThirdPartyResource(&extensions.ThirdPartyResource{
-		ObjectMeta: api.ObjectMeta{Name: "foo.company.com"},
-		Versions:   []extensions.APIVersion{{Name: "v1"}},
-	})
-
-	resp, err = http.Get(server.URL + "/apis")
-	if !assert.NoError(err) {
-		t.Errorf("unexpected error: %v", err)
-	}
-	assert.Equal(http.StatusOK, resp.StatusCode)
-	assert.NoError(decodeResponse(resp, &groupList))
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	assert.Equal(5, len(groupList.Groups))
-
-	expectGroupNames.Insert("company.com")
-	expectVersions["company.com"] = []unversioned.GroupVersionForDiscovery{thirdPartyGV}
-	expectPreferredVersion["company.com"] = thirdPartyGV
-	for _, group := range groupList.Groups {
-		if !expectGroupNames.Has(group.Name) {
-			t.Errorf("got unexpected group %s", group.Name)
-		}
-		assert.Equal(expectVersions[group.Name], group.Versions)
-		assert.Equal(expectPreferredVersion[group.Name], group.PreferredVersion)
-	}
-}
-
-func writeResponseToFile(resp *http.Response, filename string) error {
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(filename, data, 0755)
 }
 
 // TestValidOpenAPISpec verifies that the open api is added

pkg/registry/core/rest/storage_core.go
@@ -20,7 +20,12 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/golang/glog"
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/rest"
@@ -31,6 +36,7 @@ import (
 	"k8s.io/kubernetes/pkg/client/restclient"
 	"k8s.io/kubernetes/pkg/genericapiserver"
 	kubeletclient "k8s.io/kubernetes/pkg/kubelet/client"
+	"k8s.io/kubernetes/pkg/master/ports"
 	"k8s.io/kubernetes/pkg/registry/core/componentstatus"
 	configmapetcd "k8s.io/kubernetes/pkg/registry/core/configmap/etcd"
 	controlleretcd "k8s.io/kubernetes/pkg/registry/core/controller/etcd"
@@ -56,6 +62,7 @@ import (
 	ipallocator "k8s.io/kubernetes/pkg/registry/core/service/ipallocator"
 	"k8s.io/kubernetes/pkg/registry/core/service/portallocator"
 	serviceaccountetcd "k8s.io/kubernetes/pkg/registry/core/serviceaccount/etcd"
+	etcdutil "k8s.io/kubernetes/pkg/storage/etcd/util"
 	utilnet "k8s.io/kubernetes/pkg/util/net"
 )
 
@@ -72,13 +79,8 @@ type LegacyRESTStorageProvider struct {
 	ServiceClusterIPRange *net.IPNet
 	ServiceNodePortRange  utilnet.PortRange
 
-	// ComponentStatusServerFunc is a func used to locate servers to back component status
-	ComponentStatusServerFunc ComponentStatusServerFunc
-
 	LoopbackClientConfig *restclient.Config
 }
-
-type ComponentStatusServerFunc func() map[string]apiserver.Server
 
 // LegacyRESTStorage returns stateful information about particular instances of REST storage to
 // master.go for wiring controllers.
@@ -227,7 +229,7 @@ func (c LegacyRESTStorageProvider) NewLegacyRESTStorage(restOptionsGetter generi
 		"persistentVolumeClaims/status": persistentVolumeClaimStatusStorage,
 		"configMaps":                    configMapStorage,
 
-		"componentStatuses": componentstatus.NewStorage(c.ComponentStatusServerFunc),
+		"componentStatuses": componentstatus.NewStorage(componentStatusStorage{c.StorageFactory}.serversToValidate),
 	}
 	if registered.IsEnabledVersion(unversioned.GroupVersion{Group: "autoscaling", Version: "v1"}) {
 		restStorageMap["replicationControllers/scale"] = controllerStorage.Scale
@@ -238,4 +240,50 @@ func (c LegacyRESTStorageProvider) NewLegacyRESTStorage(restOptionsGetter generi
 	apiGroupInfo.VersionedResourcesStorageMap["v1"] = restStorageMap
 
 	return restStorage, apiGroupInfo, nil
+}
+
+func (p LegacyRESTStorageProvider) GroupName() string {
+	return api.GroupName
+}
+
+type componentStatusStorage struct {
+	storageFactory genericapiserver.StorageFactory
+}
+
+func (s componentStatusStorage) serversToValidate() map[string]apiserver.Server {
+	serversToValidate := map[string]apiserver.Server{
+		"controller-manager": {Addr: "127.0.0.1", Port: ports.ControllerManagerPort, Path: "/healthz"},
+		"scheduler":          {Addr: "127.0.0.1", Port: ports.SchedulerPort, Path: "/healthz"},
+	}
+
+	for ix, machine := range s.storageFactory.Backends() {
+		etcdUrl, err := url.Parse(machine)
+		if err != nil {
+			glog.Errorf("Failed to parse etcd url for validation: %v", err)
+			continue
+		}
+		var port int
+		var addr string
+		if strings.Contains(etcdUrl.Host, ":") {
+			var portString string
+			addr, portString, err = net.SplitHostPort(etcdUrl.Host)
+			if err != nil {
+				glog.Errorf("Failed to split host/port: %s (%v)", etcdUrl.Host, err)
+				continue
+			}
+			port, _ = strconv.Atoi(portString)
+		} else {
+			addr = etcdUrl.Host
+			port = 2379
+		}
+		// TODO: etcd health checking should be abstracted in the storage tier
+		serversToValidate[fmt.Sprintf("etcd-%d", ix)] = apiserver.Server{
+			Addr:        addr,
+			EnableHTTPS: etcdUrl.Scheme == "https",
+			Port:        port,
+			Path:        "/health",
+			Validate:    etcdutil.EtcdHealthCheck,
+		}
+	}
+	return serversToValidate
 }

pkg/registry/core/rest/storage_core_test.go
@@ -1,0 +1,52 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rest
+
+import (
+	"testing"
+
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/storage/storagebackend"
+)
+
+func TestGetServersToValidate(t *testing.T) {
+	servers := componentStatusStorage{fakeStorageFactory{}}.serversToValidate()
+
+	if e, a := 3, len(servers); e != a {
+		t.Errorf("expected %v, got %v", e, a)
+	}
+
+	for _, server := range []string{"scheduler", "controller-manager", "etcd-0"} {
+		if _, ok := servers[server]; !ok {
+			t.Errorf("server list missing: %s", server)
+		}
+	}
+}
+
+type fakeStorageFactory struct{}
+
+func (f fakeStorageFactory) NewConfig(groupResource unversioned.GroupResource) (*storagebackend.Config, error) {
+	return nil, nil
+}
+
+func (f fakeStorageFactory) ResourcePrefix(groupResource unversioned.GroupResource) string {
+	return ""
+}
+
+func (f fakeStorageFactory) Backends() []string {
+	return []string{"etcd-0"}
+}