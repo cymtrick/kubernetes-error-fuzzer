kubernetes/kubernetes

Add --tls-sni-cert-key to the apiserver for SNI support

pkg/genericapiserver/options/server_run_options.go
@@ -115,6 +115,7 @@ type ServerRunOptions struct {
 	TLSCAFile              string
 	TLSCertFile            string
 	TLSPrivateKeyFile      string
+	SNICertKeys            []config.NamedCertKey
 	TokenAuthFile          string
 	EnableAnyToken         bool
 	WatchCacheSizes        []string
@@ -473,13 +474,22 @@ func (s *ServerRunOptions) AddUniversalFlags(fs *pflag.FlagSet) {
 		"Controllers. This must be a valid PEM-encoded CA bundle.")
 
 	fs.StringVar(&s.TLSCertFile, "tls-cert-file", s.TLSCertFile, ""+
-		"File containing x509 Certificate for HTTPS. (CA cert, if any, concatenated "+
+		"File containing the default x509 Certificate for HTTPS. (CA cert, if any, concatenated "+
 		"after server cert). If HTTPS serving is enabled, and --tls-cert-file and "+
 		"--tls-private-key-file are not provided, a self-signed certificate and key "+
 		"are generated for the public address and saved to /var/run/kubernetes.")
 
 	fs.StringVar(&s.TLSPrivateKeyFile, "tls-private-key-file", s.TLSPrivateKeyFile,
-		"File containing x509 private key matching --tls-cert-file.")
+		"File containing the default x509 private key matching --tls-cert-file.")
+
+	fs.Var(config.NewNamedCertKeyArray(&s.SNICertKeys), "tls-sni-cert-key", ""+
+		"A pair of x509 certificate and private key file paths, optionally prefixed with a list of "+
+		"domain patterns which are fully qualified domain names, possibly with prefixed wildcard "+
+		"segments. If no domain patterns are provided, the names of the certificate are "+
+		"extracted. Non-wildcard matches trump over wildcard matches, explicit domain patterns "+
+		"trump over extracted names. For multiple key/certificate pairs, use the "+
+		"--tls-sni-key-cert multiple times. "+
+		"Examples: \"example.key,example.crt\" or \"*.foo.com,foo.com:foo.key,foo.crt\".")
 
 	fs.StringVar(&s.TokenAuthFile, "token-auth-file", s.TokenAuthFile, ""+
 		"If set, the file that will be used to secure the secure port of the API server "+

pkg/util/config/namedcertkey_flag_test.go
@@ -1,0 +1,138 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestNamedKeyCertArrayFlag(t *testing.T) {
+	tests := []struct {
+		args       []string
+		def        []NamedCertKey
+		expected   []NamedCertKey
+		parseError string
+	}{
+		{
+			args:     []string{},
+			expected: nil,
+		},
+		{
+			args: []string{"foo.crt,foo.key"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+			}},
+		},
+		{
+			args: []string{"  foo.crt , foo.key    "},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key:abc"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"abc"},
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key: abc  "},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"abc"},
+			}},
+		},
+		{
+			args:       []string{"foo.crt,foo.key:"},
+			parseError: "empty names list is not allowed",
+		},
+		{
+			args:       []string{""},
+			parseError: "expected comma separated certificate and key file paths",
+		},
+		{
+			args:       []string{"   "},
+			parseError: "expected comma separated certificate and key file paths",
+		},
+		{
+			args:       []string{"a,b,c"},
+			parseError: "expected comma separated certificate and key file paths",
+		},
+		{
+			args: []string{"foo.crt,foo.key:abc,def,ghi"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"abc", "def", "ghi"},
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key:*.*.*"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"*.*.*"},
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key", "bar.crt,bar.key"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+			}, {
+				KeyFile:  "bar.key",
+				CertFile: "bar.crt",
+			}},
+		},
+	}
+	for i, test := range tests {
+		fs := pflag.NewFlagSet("testNamedKeyCertArray", pflag.ContinueOnError)
+		var nkcs []NamedCertKey
+		for _, d := range test.def {
+			nkcs = append(nkcs, d)
+		}
+		fs.Var(NewNamedCertKeyArray(&nkcs), "tls-sni-cert-key", "usage")
+
+		args := []string{}
+		for _, a := range test.args {
+			args = append(args, fmt.Sprintf("--tls-sni-cert-key=%s", a))
+		}
+
+		err := fs.Parse(args)
+		if test.parseError != "" {
+			if err == nil {
+				t.Errorf("%d: expected error %q, got nil", i, test.parseError)
+			} else if !strings.Contains(err.Error(), test.parseError) {
+				t.Errorf("%d: expected error %q, got %q", i, test.parseError, err)
+			}
+		} else if err != nil {
+			t.Errorf("%d: expected nil error, got %v", i, err)
+		}
+		if !reflect.DeepEqual(nkcs, test.expected) {
+			t.Errorf("%d: expected %+v, got %+v", i, test.expected, nkcs)
+		}
+	}
+}

cymtrick/kubernetes-error-fuzzer

pkg/genericapiserver/options/server_run_options.go
@@ -118,6 +118,7 @@ type ServerRunOptions struct {
 	TLSCAFile              string
 	TLSCertFile            string
 	TLSPrivateKeyFile      string
+	SNICertKeys            []config.NamedCertKey
 	TokenAuthFile          string
 	EnableAnyToken         bool
 	WatchCacheSizes        []string
@@ -488,13 +489,22 @@ func (s *ServerRunOptions) AddUniversalFlags(fs *pflag.FlagSet) {
 		"Controllers. This must be a valid PEM-encoded CA bundle.")
 
 	fs.StringVar(&s.TLSCertFile, "tls-cert-file", s.TLSCertFile, ""+
-		"File containing x509 Certificate for HTTPS. (CA cert, if any, concatenated "+
+		"File containing the default x509 Certificate for HTTPS. (CA cert, if any, concatenated "+
 		"after server cert). If HTTPS serving is enabled, and --tls-cert-file and "+
 		"--tls-private-key-file are not provided, a self-signed certificate and key "+
 		"are generated for the public address and saved to /var/run/kubernetes.")
 
 	fs.StringVar(&s.TLSPrivateKeyFile, "tls-private-key-file", s.TLSPrivateKeyFile,
-		"File containing x509 private key matching --tls-cert-file.")
+		"File containing the default x509 private key matching --tls-cert-file.")
+
+	fs.Var(config.NewNamedCertKeyArray(&s.SNICertKeys), "tls-sni-cert-key", ""+
+		"A pair of x509 certificate and private key file paths, optionally suffixed with a list of "+
+		"domain patterns which are fully qualified domain names, possibly with prefixed wildcard "+
+		"segments. If no domain patterns are provided, the names of the certificate are "+
+		"extracted. Non-wildcard matches trump over wildcard matches, explicit domain patterns "+
+		"trump over extracted names. For multiple key/certificate pairs, use the "+
+		"--tls-sni-cert-key multiple times. "+
+		"Examples: \"example.key,example.crt\" or \"*.foo.com,foo.com:foo.key,foo.crt\".")
 
 	fs.StringVar(&s.TokenAuthFile, "token-auth-file", s.TokenAuthFile, ""+
 		"If set, the file that will be used to secure the secure port of the API server "+

pkg/util/config/namedcertkey_flag_test.go
@@ -1,0 +1,138 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestNamedCertKeyArrayFlag(t *testing.T) {
+	tests := []struct {
+		args       []string
+		def        []NamedCertKey
+		expected   []NamedCertKey
+		parseError string
+	}{
+		{
+			args:     []string{},
+			expected: nil,
+		},
+		{
+			args: []string{"foo.crt,foo.key"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+			}},
+		},
+		{
+			args: []string{"  foo.crt , foo.key    "},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key:abc"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"abc"},
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key: abc  "},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"abc"},
+			}},
+		},
+		{
+			args:       []string{"foo.crt,foo.key:"},
+			parseError: "empty names list is not allowed",
+		},
+		{
+			args:       []string{""},
+			parseError: "expected comma separated certificate and key file paths",
+		},
+		{
+			args:       []string{"   "},
+			parseError: "expected comma separated certificate and key file paths",
+		},
+		{
+			args:       []string{"a,b,c"},
+			parseError: "expected comma separated certificate and key file paths",
+		},
+		{
+			args: []string{"foo.crt,foo.key:abc,def,ghi"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"abc", "def", "ghi"},
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key:*.*.*"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+				Names:    []string{"*.*.*"},
+			}},
+		},
+		{
+			args: []string{"foo.crt,foo.key", "bar.crt,bar.key"},
+			expected: []NamedCertKey{{
+				KeyFile:  "foo.key",
+				CertFile: "foo.crt",
+			}, {
+				KeyFile:  "bar.key",
+				CertFile: "bar.crt",
+			}},
+		},
+	}
+	for i, test := range tests {
+		fs := pflag.NewFlagSet("testNamedCertKeyArray", pflag.ContinueOnError)
+		var nkcs []NamedCertKey
+		for _, d := range test.def {
+			nkcs = append(nkcs, d)
+		}
+		fs.Var(NewNamedCertKeyArray(&nkcs), "tls-sni-cert-key", "usage")
+
+		args := []string{}
+		for _, a := range test.args {
+			args = append(args, fmt.Sprintf("--tls-sni-cert-key=%s", a))
+		}
+
+		err := fs.Parse(args)
+		if test.parseError != "" {
+			if err == nil {
+				t.Errorf("%d: expected error %q, got nil", i, test.parseError)
+			} else if !strings.Contains(err.Error(), test.parseError) {
+				t.Errorf("%d: expected error %q, got %q", i, test.parseError, err)
+			}
+		} else if err != nil {
+			t.Errorf("%d: expected nil error, got %v", i, err)
+		}
+		if !reflect.DeepEqual(nkcs, test.expected) {
+			t.Errorf("%d: expected %+v, got %+v", i, test.expected, nkcs)
+		}
+	}
+}