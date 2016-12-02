kubernetes/kubernetes

simplify the authorization attribute getter

pkg/apiserver/filters/audit.go
@@ -18,6 +18,7 @@ package filters
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -28,6 +29,7 @@ import (
 	"github.com/golang/glog"
 	"github.com/pborman/uuid"
 
+	"k8s.io/kubernetes/pkg/api"
 	authenticationapi "k8s.io/kubernetes/pkg/apis/authentication"
 	utilnet "k8s.io/kubernetes/pkg/util/net"
 )
@@ -86,16 +88,22 @@ var _ http.Hijacker = &fancyResponseWriterDelegator{}
 // 2. the response line containing:
 //    - the unique id from 1
 //    - response code
-func WithAudit(handler http.Handler, attributeGetter RequestAttributeGetter, out io.Writer) http.Handler {
+func WithAudit(handler http.Handler, requestContextMapper api.RequestContextMapper, out io.Writer) http.Handler {
 	if out == nil {
 		return handler
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		attribs, err := attributeGetter.GetAttribs(req)
+		ctx, ok := requestContextMapper.Get(req)
+		if !ok {
+			internalError(w, req, errors.New("no context found for request"))
+			return
+		}
+		attribs, err := GetAuthorizationAttributes(ctx)
 		if err != nil {
 			internalError(w, req, err)
 			return
 		}
+
 		asuser := req.Header.Get(authenticationapi.ImpersonateUserHeader)
 		if len(asuser) == 0 {
 			asuser = "<self>"

pkg/apiserver/filters/authorization.go
@@ -28,13 +28,19 @@ import (
 )
 
 // WithAuthorizationCheck passes all authorized requests on to handler, and returns a forbidden error otherwise.
-func WithAuthorization(handler http.Handler, getAttribs RequestAttributeGetter, a authorizer.Authorizer) http.Handler {
+func WithAuthorization(handler http.Handler, requestContextMapper api.RequestContextMapper, a authorizer.Authorizer) http.Handler {
 	if a == nil {
 		glog.Warningf("Authorization is disabled")
 		return handler
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		attrs, err := getAttribs.GetAttribs(req)
+		ctx, ok := requestContextMapper.Get(req)
+		if !ok {
+			internalError(w, req, errors.New("no context found for request"))
+			return
+		}
+
+		attrs, err := GetAuthorizationAttributes(ctx)
 		if err != nil {
 			internalError(w, req, err)
 			return
@@ -54,27 +60,8 @@ func WithAuthorization(handler http.Handler, getAttribs RequestAttributeGetter, 
 	})
 }
 
-// RequestAttributeGetter is a function that extracts authorizer.Attributes from an http.Request
-type RequestAttributeGetter interface {
-	GetAttribs(req *http.Request) (authorizer.Attributes, error)
-}
-
-type requestAttributeGetter struct {
-	requestContextMapper api.RequestContextMapper
-}
-
-// NewAttributeGetter returns an object which implements the RequestAttributeGetter interface.
-func NewRequestAttributeGetter(requestContextMapper api.RequestContextMapper) RequestAttributeGetter {
-	return &requestAttributeGetter{requestContextMapper}
-}
-
-func (r *requestAttributeGetter) GetAttribs(req *http.Request) (authorizer.Attributes, error) {
+func GetAuthorizationAttributes(ctx api.Context) (authorizer.Attributes, error) {
 	attribs := authorizer.AttributesRecord{}
-
-	ctx, ok := r.requestContextMapper.Get(req)
-	if !ok {
-		return nil, errors.New("no context found for request")
-	}
 
 	user, ok := api.UserFrom(ctx)
 	if ok {

pkg/apiserver/filters/authorization_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package filters
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -27,9 +28,8 @@ import (
 	"k8s.io/kubernetes/pkg/auth/authorizer"
 )
 
-func TestGetAttribs(t *testing.T) {
+func TestGetAuthorizationAttributes(t *testing.T) {
 	mapper := api.NewRequestContextMapper()
-	attributeGetter := NewRequestAttributeGetter(mapper)
 
 	testcases := map[string]struct {
 		Verb               string
@@ -108,7 +108,12 @@ func TestGetAttribs(t *testing.T) {
 		var attribs authorizer.Attributes
 		var err error
 		var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			attribs, err = attributeGetter.GetAttribs(req)
+			ctx, ok := mapper.Get(req)
+			if !ok {
+				internalError(w, req, errors.New("no context found for request"))
+				return
+			}
+			attribs, err = GetAuthorizationAttributes(ctx)
 		})
 		handler = WithRequestInfo(handler, newTestRequestInfoResolver(), mapper)
 		handler = api.WithRequestContext(handler, mapper)

cymtrick/kubernetes-error-fuzzer

pkg/apiserver/filters/audit.go
@@ -18,6 +18,7 @@ package filters
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -28,6 +29,7 @@ import (
 	"github.com/golang/glog"
 	"github.com/pborman/uuid"
 
+	"k8s.io/kubernetes/pkg/api"
 	authenticationapi "k8s.io/kubernetes/pkg/apis/authentication"
 	utilnet "k8s.io/kubernetes/pkg/util/net"
 )
@@ -86,12 +88,17 @@ var _ http.Hijacker = &fancyResponseWriterDelegator{}
 // 2. the response line containing:
 //    - the unique id from 1
 //    - response code
-func WithAudit(handler http.Handler, attributeGetter RequestAttributeGetter, out io.Writer) http.Handler {
+func WithAudit(handler http.Handler, requestContextMapper api.RequestContextMapper, out io.Writer) http.Handler {
 	if out == nil {
 		return handler
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		attribs, err := attributeGetter.GetAttribs(req)
+		ctx, ok := requestContextMapper.Get(req)
+		if !ok {
+			internalError(w, req, errors.New("no context found for request"))
+			return
+		}
+		attribs, err := GetAuthorizerAttributes(ctx)
 		if err != nil {
 			internalError(w, req, err)
 			return

pkg/apiserver/filters/authorization.go
@@ -28,13 +28,19 @@ import (
 )
 
 // WithAuthorizationCheck passes all authorized requests on to handler, and returns a forbidden error otherwise.
-func WithAuthorization(handler http.Handler, getAttribs RequestAttributeGetter, a authorizer.Authorizer) http.Handler {
+func WithAuthorization(handler http.Handler, requestContextMapper api.RequestContextMapper, a authorizer.Authorizer) http.Handler {
 	if a == nil {
 		glog.Warningf("Authorization is disabled")
 		return handler
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		attrs, err := getAttribs.GetAttribs(req)
+		ctx, ok := requestContextMapper.Get(req)
+		if !ok {
+			internalError(w, req, errors.New("no context found for request"))
+			return
+		}
+
+		attrs, err := GetAuthorizerAttributes(ctx)
 		if err != nil {
 			internalError(w, req, err)
 			return
@@ -54,27 +60,8 @@ func WithAuthorization(handler http.Handler, getAttribs RequestAttributeGetter, 
 	})
 }
 
-// RequestAttributeGetter is a function that extracts authorizer.Attributes from an http.Request
-type RequestAttributeGetter interface {
-	GetAttribs(req *http.Request) (authorizer.Attributes, error)
-}
-
-type requestAttributeGetter struct {
-	requestContextMapper api.RequestContextMapper
-}
-
-// NewAttributeGetter returns an object which implements the RequestAttributeGetter interface.
-func NewRequestAttributeGetter(requestContextMapper api.RequestContextMapper) RequestAttributeGetter {
-	return &requestAttributeGetter{requestContextMapper}
-}
-
-func (r *requestAttributeGetter) GetAttribs(req *http.Request) (authorizer.Attributes, error) {
+func GetAuthorizerAttributes(ctx api.Context) (authorizer.Attributes, error) {
 	attribs := authorizer.AttributesRecord{}
-
-	ctx, ok := r.requestContextMapper.Get(req)
-	if !ok {
-		return nil, errors.New("no context found for request")
-	}
 
 	user, ok := api.UserFrom(ctx)
 	if ok {

pkg/apiserver/filters/authorization_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package filters
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -27,9 +28,8 @@ import (
 	"k8s.io/kubernetes/pkg/auth/authorizer"
 )
 
-func TestGetAttribs(t *testing.T) {
+func TestGetAuthorizerAttributes(t *testing.T) {
 	mapper := api.NewRequestContextMapper()
-	attributeGetter := NewRequestAttributeGetter(mapper)
 
 	testcases := map[string]struct {
 		Verb               string
@@ -108,7 +108,12 @@ func TestGetAttribs(t *testing.T) {
 		var attribs authorizer.Attributes
 		var err error
 		var handler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			attribs, err = attributeGetter.GetAttribs(req)
+			ctx, ok := mapper.Get(req)
+			if !ok {
+				internalError(w, req, errors.New("no context found for request"))
+				return
+			}
+			attribs, err = GetAuthorizerAttributes(ctx)
 		})
 		handler = WithRequestInfo(handler, newTestRequestInfoResolver(), mapper)
 		handler = api.WithRequestContext(handler, mapper)