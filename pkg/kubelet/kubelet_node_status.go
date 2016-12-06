kubernetes/kubernetes

add a configuration for kubelet to register as a node with taints
and deprecate register-schedulable

pkg/kubelet/kubelet_node_status.go
@@ -17,6 +17,7 @@ limitations under the License.
 package kubelet
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"net"
@@ -112,6 +113,16 @@ func (kl *Kubelet) initialNodeStatus() (*api.Node, error) {
 		Spec: api.NodeSpec{
 			Unschedulable: !kl.registerSchedulable,
 		},
+	}
+	if len(kl.kubeletConfiguration.RegisterWithTaints) > 0 {
+		annotations := make(map[string]string)
+		b, err := json.Marshal(kl.kubeletConfiguration.RegisterWithTaints)
+		if err != nil {
+			return nil, err
+		}
+		annotations[api.TaintsAnnotationKey] = string(b)
+		node.ObjectMeta.Annotations = annotations
+
 	}
 	// Initially, set NodeNetworkUnavailable to true.
 	if kl.providerRequiresNetworkingConfiguration() {

cymtrick/kubernetes-error-fuzzer

pkg/kubelet/kubelet_node_status.go
@@ -17,6 +17,7 @@ limitations under the License.
 package kubelet
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"net"
@@ -184,6 +185,16 @@ func (kl *Kubelet) initialNode() (*v1.Node, error) {
 		Spec: v1.NodeSpec{
 			Unschedulable: !kl.registerSchedulable,
 		},
+	}
+	if len(kl.kubeletConfiguration.RegisterWithTaints) > 0 {
+		annotations := make(map[string]string)
+		b, err := json.Marshal(kl.kubeletConfiguration.RegisterWithTaints)
+		if err != nil {
+			return nil, err
+		}
+		annotations[v1.TaintsAnnotationKey] = string(b)
+		node.ObjectMeta.Annotations = annotations
+
 	}
 	// Initially, set NodeNetworkUnavailable to true.
 	if kl.providerRequiresNetworkingConfiguration() {