kubernetes/kubernetes

regenerate informers

pkg/client/informers/informers_generated/autoscaling/internalversion/horizontalpodautoscaler.go
@@ -24,7 +24,7 @@ import (
 	autoscaling "k8s.io/kubernetes/pkg/apis/autoscaling"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	internalclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 	internalversion "k8s.io/kubernetes/pkg/client/listers/autoscaling/internalversion"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -39,7 +39,7 @@ type HorizontalPodAutoscalerInformer interface {
 }
 
 type horizontalPodAutoscalerInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internal_interfaces.SharedInformerFactory
 }
 
 func newHorizontalPodAutoscalerInformer(client internalclientset.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/autoscaling/v1/interface.go
@@ -19,7 +19,7 @@ limitations under the License.
 package v1
 
 import (
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 )
 
 // Interface provides access to all the informers in this group version.
@@ -29,11 +29,11 @@ type Interface interface {
 }
 
 type version struct {
-	interfaces.SharedInformerFactory
+	internal_interfaces.SharedInformerFactory
 }
 
 // New returns a new Interface.
-func New(f interfaces.SharedInformerFactory) Interface {
+func New(f internal_interfaces.SharedInformerFactory) Interface {
 	return &version{f}
 }
 

pkg/client/informers/informers_generated/certificates/internalversion/certificatesigningrequest.go
@@ -24,7 +24,7 @@ import (
 	certificates "k8s.io/kubernetes/pkg/apis/certificates"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	internalclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 	internalversion "k8s.io/kubernetes/pkg/client/listers/certificates/internalversion"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -39,7 +39,7 @@ type CertificateSigningRequestInformer interface {
 }
 
 type certificateSigningRequestInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internal_interfaces.SharedInformerFactory
 }
 
 func newCertificateSigningRequestInformer(client internalclientset.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/core/v1/interface.go
@@ -19,7 +19,7 @@ limitations under the License.
 package v1
 
 import (
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 )
 
 // Interface provides access to all the informers in this group version.
@@ -59,11 +59,11 @@ type Interface interface {
 }
 
 type version struct {
-	interfaces.SharedInformerFactory
+	internal_interfaces.SharedInformerFactory
 }
 
 // New returns a new Interface.
-func New(f interfaces.SharedInformerFactory) Interface {
+func New(f internal_interfaces.SharedInformerFactory) Interface {
 	return &version{f}
 }
 

pkg/client/informers/informers_generated/core/v1/persistentvolume.go
@@ -22,7 +22,7 @@ import (
 	api_v1 "k8s.io/kubernetes/pkg/api/v1"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	release_1_5 "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 	v1 "k8s.io/kubernetes/pkg/client/listers/core/v1"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -37,7 +37,7 @@ type PersistentVolumeInformer interface {
 }
 
 type persistentVolumeInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internal_interfaces.SharedInformerFactory
 }
 
 func newPersistentVolumeInformer(client release_1_5.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/core/v1/podtemplate.go
@@ -22,7 +22,7 @@ import (
 	api_v1 "k8s.io/kubernetes/pkg/api/v1"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	release_1_5 "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 	v1 "k8s.io/kubernetes/pkg/client/listers/core/v1"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -37,7 +37,7 @@ type PodTemplateInformer interface {
 }
 
 type podTemplateInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internal_interfaces.SharedInformerFactory
 }
 
 func newPodTemplateInformer(client release_1_5.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/extensions/internalversion/deployment.go
@@ -24,7 +24,7 @@ import (
 	extensions "k8s.io/kubernetes/pkg/apis/extensions"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	internalclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 	internalversion "k8s.io/kubernetes/pkg/client/listers/extensions/internalversion"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -39,7 +39,7 @@ type DeploymentInformer interface {
 }
 
 type deploymentInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internal_interfaces.SharedInformerFactory
 }
 
 func newDeploymentInformer(client internalclientset.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/extensions/v1beta1/podsecuritypolicy.go
@@ -23,7 +23,7 @@ import (
 	extensions_v1beta1 "k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	release_1_5 "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internal_interfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internal_interfaces"
 	v1beta1 "k8s.io/kubernetes/pkg/client/listers/extensions/v1beta1"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -38,7 +38,7 @@ type PodSecurityPolicyInformer interface {
 }
 
 type podSecurityPolicyInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internal_interfaces.SharedInformerFactory
 }
 
 func newPodSecurityPolicyInformer(client release_1_5.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

cymtrick/kubernetes-error-fuzzer

pkg/client/informers/informers_generated/autoscaling/internalversion/horizontalpodautoscaler.go
@@ -24,7 +24,7 @@ import (
 	autoscaling "k8s.io/kubernetes/pkg/apis/autoscaling"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	internalclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 	internalversion "k8s.io/kubernetes/pkg/client/listers/autoscaling/internalversion"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -39,7 +39,7 @@ type HorizontalPodAutoscalerInformer interface {
 }
 
 type horizontalPodAutoscalerInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internalinterfaces.SharedInformerFactory
 }
 
 func newHorizontalPodAutoscalerInformer(client internalclientset.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/autoscaling/v1/interface.go
@@ -19,7 +19,7 @@ limitations under the License.
 package v1
 
 import (
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 )
 
 // Interface provides access to all the informers in this group version.
@@ -29,11 +29,11 @@ type Interface interface {
 }
 
 type version struct {
-	interfaces.SharedInformerFactory
+	internalinterfaces.SharedInformerFactory
 }
 
 // New returns a new Interface.
-func New(f interfaces.SharedInformerFactory) Interface {
+func New(f internalinterfaces.SharedInformerFactory) Interface {
 	return &version{f}
 }
 

pkg/client/informers/informers_generated/certificates/internalversion/certificatesigningrequest.go
@@ -24,7 +24,7 @@ import (
 	certificates "k8s.io/kubernetes/pkg/apis/certificates"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	internalclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 	internalversion "k8s.io/kubernetes/pkg/client/listers/certificates/internalversion"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -39,7 +39,7 @@ type CertificateSigningRequestInformer interface {
 }
 
 type certificateSigningRequestInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internalinterfaces.SharedInformerFactory
 }
 
 func newCertificateSigningRequestInformer(client internalclientset.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/core/v1/interface.go
@@ -19,7 +19,7 @@ limitations under the License.
 package v1
 
 import (
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 )
 
 // Interface provides access to all the informers in this group version.
@@ -59,11 +59,11 @@ type Interface interface {
 }
 
 type version struct {
-	interfaces.SharedInformerFactory
+	internalinterfaces.SharedInformerFactory
 }
 
 // New returns a new Interface.
-func New(f interfaces.SharedInformerFactory) Interface {
+func New(f internalinterfaces.SharedInformerFactory) Interface {
 	return &version{f}
 }
 

pkg/client/informers/informers_generated/core/v1/persistentvolume.go
@@ -22,7 +22,7 @@ import (
 	api_v1 "k8s.io/kubernetes/pkg/api/v1"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	release_1_5 "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 	v1 "k8s.io/kubernetes/pkg/client/listers/core/v1"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -37,7 +37,7 @@ type PersistentVolumeInformer interface {
 }
 
 type persistentVolumeInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internalinterfaces.SharedInformerFactory
 }
 
 func newPersistentVolumeInformer(client release_1_5.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/core/v1/podtemplate.go
@@ -22,7 +22,7 @@ import (
 	api_v1 "k8s.io/kubernetes/pkg/api/v1"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	release_1_5 "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 	v1 "k8s.io/kubernetes/pkg/client/listers/core/v1"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -37,7 +37,7 @@ type PodTemplateInformer interface {
 }
 
 type podTemplateInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internalinterfaces.SharedInformerFactory
 }
 
 func newPodTemplateInformer(client release_1_5.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/extensions/internalversion/deployment.go
@@ -24,7 +24,7 @@ import (
 	extensions "k8s.io/kubernetes/pkg/apis/extensions"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	internalclientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 	internalversion "k8s.io/kubernetes/pkg/client/listers/extensions/internalversion"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -39,7 +39,7 @@ type DeploymentInformer interface {
 }
 
 type deploymentInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internalinterfaces.SharedInformerFactory
 }
 
 func newDeploymentInformer(client internalclientset.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {

pkg/client/informers/informers_generated/extensions/v1beta1/podsecuritypolicy.go
@@ -23,7 +23,7 @@ import (
 	extensions_v1beta1 "k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
 	cache "k8s.io/kubernetes/pkg/client/cache"
 	release_1_5 "k8s.io/kubernetes/pkg/client/clientset_generated/release_1_5"
-	interfaces "k8s.io/kubernetes/pkg/client/informers/interfaces"
+	internalinterfaces "k8s.io/kubernetes/pkg/client/informers/informers_generated/internalinterfaces"
 	v1beta1 "k8s.io/kubernetes/pkg/client/listers/extensions/v1beta1"
 	runtime "k8s.io/kubernetes/pkg/runtime"
 	watch "k8s.io/kubernetes/pkg/watch"
@@ -38,7 +38,7 @@ type PodSecurityPolicyInformer interface {
 }
 
 type podSecurityPolicyInformer struct {
-	factory interfaces.SharedInformerFactory
+	factory internalinterfaces.SharedInformerFactory
 }
 
 func newPodSecurityPolicyInformer(client release_1_5.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {