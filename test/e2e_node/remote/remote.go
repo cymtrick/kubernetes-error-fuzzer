kubernetes/kubernetes

Refactor RunRemote to support TestSuite interface.

test/e2e_node/remote/remote.go
@@ -23,12 +23,10 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/golang/glog"
 	utilerrors "k8s.io/kubernetes/pkg/util/errors"
-	"k8s.io/kubernetes/test/e2e_node/builder"
 )
 
 var testTimeoutSeconds = flag.Duration("test-timeout", 45*time.Minute, "How long (in golang duration format) to wait for ginkgo tests to complete.")
@@ -63,177 +61,54 @@ func CreateTestArchive(suite TestSuite) (string, error) {
 	return filepath.Join(dir, archiveName), nil
 }
 
-const (
-	CNIRelease   = "07a8a28637e97b22eb8dfe710eeae1344f69d16e"
-	CNIDirectory = "cni"
-	CNIURL       = "https://storage.googleapis.com/kubernetes-release/network-plugins/cni-" + CNIRelease + ".tar.gz"
-)
-
 // Returns the command output, whether the exit was ok, and any errors
-func RunRemote(archive string, host string, cleanup bool, junitFilePrefix string, testArgs string, ginkgoFlags string) (string, bool, error) {
+// TODO(random-liu): junitFilePrefix is not prefix actually, the file name is junit-junitFilePrefix.xml. Change the variable name.
+func RunRemote(suite TestSuite, archive string, host string, cleanup bool, junitFilePrefix string, testArgs string, ginkgoFlags string) (string, bool, error) {
 	// Create the temp staging directory
-	glog.Infof("Staging test binaries on %s", host)
+	glog.Infof("Staging test binaries on %q", host)
 	workspace := fmt.Sprintf("/tmp/node-e2e-%s", getTimestamp())
 	// Do not sudo here, so that we can use scp to copy test archive to the directdory.
 	if output, err := SSHNoSudo(host, "mkdir", workspace); err != nil {
 		// Exit failure with the error
-		return "", false, fmt.Errorf("failed to create workspace directory: %v output: %q", err, output)
+		return "", false, fmt.Errorf("failed to create workspace directory %q on host %q: %v output: %q", workspace, host, err, output)
 	}
 	if cleanup {
 		defer func() {
 			output, err := SSH(host, "rm", "-rf", workspace)
 			if err != nil {
-				glog.Errorf("failed to cleanup workspace %s on host %v.  Output:\n%s", workspace, err, output)
+				glog.Errorf("failed to cleanup workspace %q on host %q: %v.  Output:\n%s", workspace, host, err, output)
 			}
 		}()
 	}
 
-	// Install the cni plugin.
-	cniPath := filepath.Join(workspace, CNIDirectory)
-	cmd := getSSHCommand(" ; ",
-		fmt.Sprintf("mkdir -p %s", cniPath),
-		fmt.Sprintf("wget -O - %s | tar -xz -C %s", CNIURL, cniPath),
-	)
-	if output, err := SSH(host, "sh", "-c", cmd); err != nil {
-		// Exit failure with the error
-		return "", false, fmt.Errorf("failed to install cni plugin: %v output: %q", err, output)
-	}
-
-	// Configure iptables firewall rules
-	// TODO: consider calling bootstrap script to configure host based on OS
-	output, err := SSH(host, "iptables", "-L", "INPUT")
-	if err != nil {
-		return "", false, fmt.Errorf("failed to get iptables INPUT: %v output: %q", err, output)
-	}
-	if strings.Contains(output, "Chain INPUT (policy DROP)") {
-		cmd = getSSHCommand("&&",
-			"(iptables -C INPUT -w -p TCP -j ACCEPT || iptables -A INPUT -w -p TCP -j ACCEPT)",
-			"(iptables -C INPUT -w -p UDP -j ACCEPT || iptables -A INPUT -w -p UDP -j ACCEPT)",
-			"(iptables -C INPUT -w -p ICMP -j ACCEPT || iptables -A INPUT -w -p ICMP -j ACCEPT)")
-		output, err := SSH(host, "sh", "-c", cmd)
-		if err != nil {
-			return "", false, fmt.Errorf("failed to configured firewall: %v output: %v", err, output)
-		}
-	}
-	output, err = SSH(host, "iptables", "-L", "FORWARD")
-	if err != nil {
-		return "", false, fmt.Errorf("failed to get iptables FORWARD: %v output: %q", err, output)
-	}
-	if strings.Contains(output, "Chain FORWARD (policy DROP)") {
-		cmd = getSSHCommand("&&",
-			"(iptables -C FORWARD -w -p TCP -j ACCEPT || iptables -A FORWARD -w -p TCP -j ACCEPT)",
-			"(iptables -C FORWARD -w -p UDP -j ACCEPT || iptables -A FORWARD -w -p UDP -j ACCEPT)",
-			"(iptables -C FORWARD -w -p ICMP -j ACCEPT || iptables -A FORWARD -w -p ICMP -j ACCEPT)")
-		output, err = SSH(host, "sh", "-c", cmd)
-		if err != nil {
-			return "", false, fmt.Errorf("failed to configured firewall: %v output: %v", err, output)
-		}
-	}
-
 	// Copy the archive to the staging directory
-	if output, err = runSSHCommand("scp", archive, fmt.Sprintf("%s:%s/", GetHostnameOrIp(host), workspace)); err != nil {
+	if output, err := runSSHCommand("scp", archive, fmt.Sprintf("%s:%s/", GetHostnameOrIp(host), workspace)); err != nil {
 		// Exit failure with the error
 		return "", false, fmt.Errorf("failed to copy test archive: %v, output: %q", err, output)
 	}
 
-	// Kill any running node processes
-	cmd = getSSHCommand(" ; ",
-		"pkill kubelet",
-		"pkill kube-apiserver",
-		"pkill etcd",
-	)
-	// No need to log an error if pkill fails since pkill will fail if the commands are not running.
-	// If we are unable to stop existing running k8s processes, we should see messages in the kubelet/apiserver/etcd
-	// logs about failing to bind the required ports.
-	glog.Infof("Killing any existing node processes on %s", host)
-	SSH(host, "sh", "-c", cmd)
-
 	// Extract the archive
-	cmd = getSSHCommand(" && ",
+	cmd := getSSHCommand(" && ",
 		fmt.Sprintf("cd %s", workspace),
 		fmt.Sprintf("tar -xzvf ./%s", archiveName),
 	)
-	glog.Infof("Extracting tar on %s", host)
-	if output, err = SSH(host, "sh", "-c", cmd); err != nil {
+	glog.Infof("Extracting tar on %q", host)
+	if output, err := SSH(host, "sh", "-c", cmd); err != nil {
 		// Exit failure with the error
 		return "", false, fmt.Errorf("failed to extract test archive: %v, output: %q", err, output)
 	}
 
-	// If we are testing on a GCI node, we chmod 544 the mounter and specify a different mounter path in the test args.
-	// We do this here because the local var `workspace` tells us which /tmp/node-e2e-%d is relevant to the current test run.
+	glog.Infof("Running test on %q", host)
+	output, err := suite.RunTest(host, workspace, filepath.Join(workspace, "results"), junitFilePrefix, testArgs, ginkgoFlags, *testTimeoutSeconds)
 
-	// Determine if the GCI mounter script exists locally.
-	k8sDir, err := builder.GetK8sRootDir()
-	if err != nil {
-		return "", false, fmt.Errorf("Could not find K8s root dir! Err: %v", err)
-	}
-	localSource := "cluster/gce/gci/mounter/mounter"
-	source := filepath.Join(k8sDir, localSource)
-
-	// Require the GCI mounter script, we want to make sure the remote test runner stays up to date if the mounter file moves
-	if _, err = os.Stat(source); err != nil {
-		return "", false, fmt.Errorf("Could not find GCI mounter script at %q! If this script has been (re)moved, please update the e2e node remote test runner accordingly! Err: %v", source, err)
-	}
-
-	// Determine if tests will run on a GCI node.
-	output, err = SSH(host, "sh", "-c", "'cat /etc/os-release'")
-	if err != nil {
-		glog.Errorf("Issue detecting node's OS via node's /etc/os-release. Err: %v, Output:\n%s", err, output)
-		return "", false, fmt.Errorf("Issue detecting node's OS via node's /etc/os-release. Err: %v, Output:\n%s", err, output)
-	}
-	if strings.Contains(output, "ID=gci") {
-		glog.Infof("GCI node and GCI mounter both detected, modifying --experimental-mounter-path accordingly")
-		// Note this implicitly requires the script to be where we expect in the tarball, so if that location changes the error
-		// here will tell us to update the remote test runner.
-		mounterPath := filepath.Join(workspace, "cluster/gce/gci/mounter/mounter")
-		output, err = SSH(host, "sh", "-c", fmt.Sprintf("'chmod 544 %s'", mounterPath))
-		if err != nil {
-			glog.Errorf("Unable to chmod 544 GCI mounter script. Err: %v, Output:\n%s", err, output)
-			return "", false, err
-		}
-		// Insert args at beginning of testArgs, so any values from command line take precedence
-		testArgs = fmt.Sprintf("--kubelet-flags=--experimental-mounter-path=%s ", mounterPath) + testArgs
-	}
-
-	// Run the tests
-	cmd = getSSHCommand(" && ",
-		fmt.Sprintf("cd %s", workspace),
-		fmt.Sprintf("timeout -k 30s %fs ./ginkgo %s ./e2e_node.test -- --logtostderr --v 4 --node-name=%s --report-dir=%s/results --report-prefix=%s %s",
-			testTimeoutSeconds.Seconds(), ginkgoFlags, host, workspace, junitFilePrefix, testArgs),
-	)
 	aggErrs := []error{}
-
-	glog.Infof("Starting tests on %s", host)
-	output, err = SSH(host, "sh", "-c", cmd)
 	// Do not log the output here, let the caller deal with the test output.
 	if err != nil {
 		aggErrs = append(aggErrs, err)
-
-		// Encountered an unexpected error. The remote test harness may not
-		// have finished retrieved and stored all the logs in this case. Try
-		// to get some logs for debugging purposes.
-		// TODO: This is a best-effort, temporary hack that only works for
-		// journald nodes. We should have a more robust way to collect logs.
-		var (
-			logName  = "system.log"
-			logPath  = fmt.Sprintf("/tmp/%s-%s", getTimestamp(), logName)
-			destPath = fmt.Sprintf("%s/%s-%s", *resultsDir, host, logName)
-		)
-		glog.Infof("Test failed unexpectedly. Attempting to retreiving system logs (only works for nodes with journald)")
-		// Try getting the system logs from journald and store it to a file.
-		// Don't reuse the original test directory on the remote host because
-		// it could've be been removed if the node was rebooted.
-		if output, err := SSH(host, "sh", "-c", fmt.Sprintf("'journalctl --system --all > %s'", logPath)); err == nil {
-			glog.Infof("Got the system logs from journald; copying it back...")
-			if output, err := runSSHCommand("scp", fmt.Sprintf("%s:%s", GetHostnameOrIp(host), logPath), destPath); err != nil {
-				glog.Infof("Failed to copy the log: err: %v, output: %q", err, output)
-			}
-		} else {
-			glog.Infof("Failed to run journactl (normal if it doesn't exist on the node): %v, output: %q", err, output)
-		}
+		collectSystemLog(host, workspace)
 	}
 
-	glog.Infof("Copying test artifacts from %s", host)
+	glog.Infof("Copying test artifacts from %q", host)
 	scpErr := getTestArtifacts(host, workspace)
 	if scpErr != nil {
 		aggErrs = append(aggErrs, scpErr)
@@ -265,6 +140,33 @@ func getTestArtifacts(host, testDir string) error {
 		return err
 	}
 	return nil
+}
+
+// collectSystemLog is a temporary hack to collect system log when encountered on
+// unexpected error.
+func collectSystemLog(host, workspace string) {
+	// Encountered an unexpected error. The remote test harness may not
+	// have finished retrieved and stored all the logs in this case. Try
+	// to get some logs for debugging purposes.
+	// TODO: This is a best-effort, temporary hack that only works for
+	// journald nodes. We should have a more robust way to collect logs.
+	var (
+		logName  = "system.log"
+		logPath  = fmt.Sprintf("/tmp/%s-%s", getTimestamp(), logName)
+		destPath = fmt.Sprintf("%s/%s-%s", *resultsDir, host, logName)
+	)
+	glog.Infof("Test failed unexpectedly. Attempting to retreiving system logs (only works for nodes with journald)")
+	// Try getting the system logs from journald and store it to a file.
+	// Don't reuse the original test directory on the remote host because
+	// it could've be been removed if the node was rebooted.
+	if output, err := SSH(host, "sh", "-c", fmt.Sprintf("'journalctl --system --all > %s'", logPath)); err == nil {
+		glog.Infof("Got the system logs from journald; copying it back...")
+		if output, err := runSSHCommand("scp", fmt.Sprintf("%s:%s", GetHostnameOrIp(host), logPath), destPath); err != nil {
+			glog.Infof("Failed to copy the log: err: %v, output: %q", err, output)
+		}
+	} else {
+		glog.Infof("Failed to run journactl (normal if it doesn't exist on the node): %v, output: %q", err, output)
+	}
 }
 
 // WriteLog is a temporary function to make it possible to write log

cymtrick/kubernetes-error-fuzzer

test/e2e_node/remote/remote.go
@@ -23,12 +23,10 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/golang/glog"
 	utilerrors "k8s.io/kubernetes/pkg/util/errors"
-	"k8s.io/kubernetes/test/e2e_node/builder"
 )
 
 var testTimeoutSeconds = flag.Duration("test-timeout", 45*time.Minute, "How long (in golang duration format) to wait for ginkgo tests to complete.")
@@ -63,177 +61,54 @@ func CreateTestArchive(suite TestSuite) (string, error) {
 	return filepath.Join(dir, archiveName), nil
 }
 
-const (
-	CNIRelease   = "07a8a28637e97b22eb8dfe710eeae1344f69d16e"
-	CNIDirectory = "cni"
-	CNIURL       = "https://storage.googleapis.com/kubernetes-release/network-plugins/cni-" + CNIRelease + ".tar.gz"
-)
-
 // Returns the command output, whether the exit was ok, and any errors
-func RunRemote(archive string, host string, cleanup bool, junitFilePrefix string, testArgs string, ginkgoFlags string) (string, bool, error) {
+// TODO(random-liu): junitFilePrefix is not prefix actually, the file name is junit-junitFilePrefix.xml. Change the variable name.
+func RunRemote(suite TestSuite, archive string, host string, cleanup bool, junitFilePrefix string, testArgs string, ginkgoArgs string) (string, bool, error) {
 	// Create the temp staging directory
-	glog.Infof("Staging test binaries on %s", host)
+	glog.Infof("Staging test binaries on %q", host)
 	workspace := fmt.Sprintf("/tmp/node-e2e-%s", getTimestamp())
 	// Do not sudo here, so that we can use scp to copy test archive to the directdory.
 	if output, err := SSHNoSudo(host, "mkdir", workspace); err != nil {
 		// Exit failure with the error
-		return "", false, fmt.Errorf("failed to create workspace directory: %v output: %q", err, output)
+		return "", false, fmt.Errorf("failed to create workspace directory %q on host %q: %v output: %q", workspace, host, err, output)
 	}
 	if cleanup {
 		defer func() {
 			output, err := SSH(host, "rm", "-rf", workspace)
 			if err != nil {
-				glog.Errorf("failed to cleanup workspace %s on host %v.  Output:\n%s", workspace, err, output)
+				glog.Errorf("failed to cleanup workspace %q on host %q: %v.  Output:\n%s", workspace, host, err, output)
 			}
 		}()
 	}
 
-	// Install the cni plugin.
-	cniPath := filepath.Join(workspace, CNIDirectory)
-	cmd := getSSHCommand(" ; ",
-		fmt.Sprintf("mkdir -p %s", cniPath),
-		fmt.Sprintf("wget -O - %s | tar -xz -C %s", CNIURL, cniPath),
-	)
-	if output, err := SSH(host, "sh", "-c", cmd); err != nil {
-		// Exit failure with the error
-		return "", false, fmt.Errorf("failed to install cni plugin: %v output: %q", err, output)
-	}
-
-	// Configure iptables firewall rules
-	// TODO: consider calling bootstrap script to configure host based on OS
-	output, err := SSH(host, "iptables", "-L", "INPUT")
-	if err != nil {
-		return "", false, fmt.Errorf("failed to get iptables INPUT: %v output: %q", err, output)
-	}
-	if strings.Contains(output, "Chain INPUT (policy DROP)") {
-		cmd = getSSHCommand("&&",
-			"(iptables -C INPUT -w -p TCP -j ACCEPT || iptables -A INPUT -w -p TCP -j ACCEPT)",
-			"(iptables -C INPUT -w -p UDP -j ACCEPT || iptables -A INPUT -w -p UDP -j ACCEPT)",
-			"(iptables -C INPUT -w -p ICMP -j ACCEPT || iptables -A INPUT -w -p ICMP -j ACCEPT)")
-		output, err := SSH(host, "sh", "-c", cmd)
-		if err != nil {
-			return "", false, fmt.Errorf("failed to configured firewall: %v output: %v", err, output)
-		}
-	}
-	output, err = SSH(host, "iptables", "-L", "FORWARD")
-	if err != nil {
-		return "", false, fmt.Errorf("failed to get iptables FORWARD: %v output: %q", err, output)
-	}
-	if strings.Contains(output, "Chain FORWARD (policy DROP)") {
-		cmd = getSSHCommand("&&",
-			"(iptables -C FORWARD -w -p TCP -j ACCEPT || iptables -A FORWARD -w -p TCP -j ACCEPT)",
-			"(iptables -C FORWARD -w -p UDP -j ACCEPT || iptables -A FORWARD -w -p UDP -j ACCEPT)",
-			"(iptables -C FORWARD -w -p ICMP -j ACCEPT || iptables -A FORWARD -w -p ICMP -j ACCEPT)")
-		output, err = SSH(host, "sh", "-c", cmd)
-		if err != nil {
-			return "", false, fmt.Errorf("failed to configured firewall: %v output: %v", err, output)
-		}
-	}
-
 	// Copy the archive to the staging directory
-	if output, err = runSSHCommand("scp", archive, fmt.Sprintf("%s:%s/", GetHostnameOrIp(host), workspace)); err != nil {
+	if output, err := runSSHCommand("scp", archive, fmt.Sprintf("%s:%s/", GetHostnameOrIp(host), workspace)); err != nil {
 		// Exit failure with the error
 		return "", false, fmt.Errorf("failed to copy test archive: %v, output: %q", err, output)
 	}
 
-	// Kill any running node processes
-	cmd = getSSHCommand(" ; ",
-		"pkill kubelet",
-		"pkill kube-apiserver",
-		"pkill etcd",
-	)
-	// No need to log an error if pkill fails since pkill will fail if the commands are not running.
-	// If we are unable to stop existing running k8s processes, we should see messages in the kubelet/apiserver/etcd
-	// logs about failing to bind the required ports.
-	glog.Infof("Killing any existing node processes on %s", host)
-	SSH(host, "sh", "-c", cmd)
-
 	// Extract the archive
-	cmd = getSSHCommand(" && ",
+	cmd := getSSHCommand(" && ",
 		fmt.Sprintf("cd %s", workspace),
 		fmt.Sprintf("tar -xzvf ./%s", archiveName),
 	)
-	glog.Infof("Extracting tar on %s", host)
-	if output, err = SSH(host, "sh", "-c", cmd); err != nil {
+	glog.Infof("Extracting tar on %q", host)
+	if output, err := SSH(host, "sh", "-c", cmd); err != nil {
 		// Exit failure with the error
 		return "", false, fmt.Errorf("failed to extract test archive: %v, output: %q", err, output)
 	}
 
-	// If we are testing on a GCI node, we chmod 544 the mounter and specify a different mounter path in the test args.
-	// We do this here because the local var `workspace` tells us which /tmp/node-e2e-%d is relevant to the current test run.
+	glog.Infof("Running test on %q", host)
+	output, err := suite.RunTest(host, workspace, filepath.Join(workspace, "results"), junitFilePrefix, testArgs, ginkgoArgs, *testTimeoutSeconds)
 
-	// Determine if the GCI mounter script exists locally.
-	k8sDir, err := builder.GetK8sRootDir()
-	if err != nil {
-		return "", false, fmt.Errorf("Could not find K8s root dir! Err: %v", err)
-	}
-	localSource := "cluster/gce/gci/mounter/mounter"
-	source := filepath.Join(k8sDir, localSource)
-
-	// Require the GCI mounter script, we want to make sure the remote test runner stays up to date if the mounter file moves
-	if _, err = os.Stat(source); err != nil {
-		return "", false, fmt.Errorf("Could not find GCI mounter script at %q! If this script has been (re)moved, please update the e2e node remote test runner accordingly! Err: %v", source, err)
-	}
-
-	// Determine if tests will run on a GCI node.
-	output, err = SSH(host, "sh", "-c", "'cat /etc/os-release'")
-	if err != nil {
-		glog.Errorf("Issue detecting node's OS via node's /etc/os-release. Err: %v, Output:\n%s", err, output)
-		return "", false, fmt.Errorf("Issue detecting node's OS via node's /etc/os-release. Err: %v, Output:\n%s", err, output)
-	}
-	if strings.Contains(output, "ID=gci") {
-		glog.Infof("GCI node and GCI mounter both detected, modifying --experimental-mounter-path accordingly")
-		// Note this implicitly requires the script to be where we expect in the tarball, so if that location changes the error
-		// here will tell us to update the remote test runner.
-		mounterPath := filepath.Join(workspace, "cluster/gce/gci/mounter/mounter")
-		output, err = SSH(host, "sh", "-c", fmt.Sprintf("'chmod 544 %s'", mounterPath))
-		if err != nil {
-			glog.Errorf("Unable to chmod 544 GCI mounter script. Err: %v, Output:\n%s", err, output)
-			return "", false, err
-		}
-		// Insert args at beginning of testArgs, so any values from command line take precedence
-		testArgs = fmt.Sprintf("--kubelet-flags=--experimental-mounter-path=%s ", mounterPath) + testArgs
-	}
-
-	// Run the tests
-	cmd = getSSHCommand(" && ",
-		fmt.Sprintf("cd %s", workspace),
-		fmt.Sprintf("timeout -k 30s %fs ./ginkgo %s ./e2e_node.test -- --logtostderr --v 4 --node-name=%s --report-dir=%s/results --report-prefix=%s %s",
-			testTimeoutSeconds.Seconds(), ginkgoFlags, host, workspace, junitFilePrefix, testArgs),
-	)
 	aggErrs := []error{}
-
-	glog.Infof("Starting tests on %s", host)
-	output, err = SSH(host, "sh", "-c", cmd)
 	// Do not log the output here, let the caller deal with the test output.
 	if err != nil {
 		aggErrs = append(aggErrs, err)
-
-		// Encountered an unexpected error. The remote test harness may not
-		// have finished retrieved and stored all the logs in this case. Try
-		// to get some logs for debugging purposes.
-		// TODO: This is a best-effort, temporary hack that only works for
-		// journald nodes. We should have a more robust way to collect logs.
-		var (
-			logName  = "system.log"
-			logPath  = fmt.Sprintf("/tmp/%s-%s", getTimestamp(), logName)
-			destPath = fmt.Sprintf("%s/%s-%s", *resultsDir, host, logName)
-		)
-		glog.Infof("Test failed unexpectedly. Attempting to retreiving system logs (only works for nodes with journald)")
-		// Try getting the system logs from journald and store it to a file.
-		// Don't reuse the original test directory on the remote host because
-		// it could've be been removed if the node was rebooted.
-		if output, err := SSH(host, "sh", "-c", fmt.Sprintf("'journalctl --system --all > %s'", logPath)); err == nil {
-			glog.Infof("Got the system logs from journald; copying it back...")
-			if output, err := runSSHCommand("scp", fmt.Sprintf("%s:%s", GetHostnameOrIp(host), logPath), destPath); err != nil {
-				glog.Infof("Failed to copy the log: err: %v, output: %q", err, output)
-			}
-		} else {
-			glog.Infof("Failed to run journactl (normal if it doesn't exist on the node): %v, output: %q", err, output)
-		}
+		collectSystemLog(host, workspace)
 	}
 
-	glog.Infof("Copying test artifacts from %s", host)
+	glog.Infof("Copying test artifacts from %q", host)
 	scpErr := getTestArtifacts(host, workspace)
 	if scpErr != nil {
 		aggErrs = append(aggErrs, scpErr)
@@ -265,6 +140,33 @@ func getTestArtifacts(host, testDir string) error {
 		return err
 	}
 	return nil
+}
+
+// collectSystemLog is a temporary hack to collect system log when encountered on
+// unexpected error.
+func collectSystemLog(host, workspace string) {
+	// Encountered an unexpected error. The remote test harness may not
+	// have finished retrieved and stored all the logs in this case. Try
+	// to get some logs for debugging purposes.
+	// TODO: This is a best-effort, temporary hack that only works for
+	// journald nodes. We should have a more robust way to collect logs.
+	var (
+		logName  = "system.log"
+		logPath  = fmt.Sprintf("/tmp/%s-%s", getTimestamp(), logName)
+		destPath = fmt.Sprintf("%s/%s-%s", *resultsDir, host, logName)
+	)
+	glog.Infof("Test failed unexpectedly. Attempting to retreiving system logs (only works for nodes with journald)")
+	// Try getting the system logs from journald and store it to a file.
+	// Don't reuse the original test directory on the remote host because
+	// it could've be been removed if the node was rebooted.
+	if output, err := SSH(host, "sh", "-c", fmt.Sprintf("'journalctl --system --all > %s'", logPath)); err == nil {
+		glog.Infof("Got the system logs from journald; copying it back...")
+		if output, err := runSSHCommand("scp", fmt.Sprintf("%s:%s", GetHostnameOrIp(host), logPath), destPath); err != nil {
+			glog.Infof("Failed to copy the log: err: %v, output: %q", err, output)
+		}
+	} else {
+		glog.Infof("Failed to run journactl (normal if it doesn't exist on the node): %v, output: %q", err, output)
+	}
 }
 
 // WriteLog is a temporary function to make it possible to write log