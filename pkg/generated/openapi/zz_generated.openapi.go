jayunit100/kubernetes

[scheduling] Auto-generated file updates from moving node affinity from annotations to api fields. #35518

pkg/generated/openapi/zz_generated.openapi.go
@@ -11335,12 +11335,18 @@ var OpenAPIDefinitions *common.OpenAPIDefinitions = &common.OpenAPIDefinitions{
 							Format:      "",
 						},
 					},
+					"affinity": {
+						SchemaProps: spec.SchemaProps{
+							Description: "If specified, the pod's scheduling preferences",
+							Ref:         spec.MustCreateRef("#/definitions/v1.Affinity"),
+						},
+					},
 				},
 				Required: []string{"containers"},
 			},
 		},
 		Dependencies: []string{
-			"v1.Container", "v1.LocalObjectReference", "v1.PodSecurityContext", "v1.Volume"},
+			"v1.Affinity", "v1.Container", "v1.LocalObjectReference", "v1.PodSecurityContext", "v1.Volume"},
 	},
 	"v1.PodStatus": {
 		Schema: spec.Schema{

cymtrick/kubernetes-error-fuzzer

pkg/generated/openapi/zz_generated.openapi.go
@@ -5373,12 +5373,18 @@ var OpenAPIDefinitions *common.OpenAPIDefinitions = &common.OpenAPIDefinitions{
 							Format:      "",
 						},
 					},
+					"affinity": {
+						SchemaProps: spec.SchemaProps{
+							Description: "If specified, the pod's scheduling constraints",
+							Ref:         spec.MustCreateRef("#/definitions/v1.Affinity"),
+						},
+					},
 				},
 				Required: []string{"containers"},
 			},
 		},
 		Dependencies: []string{
-			"v1.Container", "v1.LocalObjectReference", "v1.PodSecurityContext", "v1.Volume"},
+			"v1.Affinity", "v1.Container", "v1.LocalObjectReference", "v1.PodSecurityContext", "v1.Volume"},
 	},
 	"v1.PodStatus": {
 		Schema: spec.Schema{