kubernetes/kubernetes

refactored admission to avoid internal client references

plugin/pkg/admission/namespace/autoprovision/admission.go
@@ -17,22 +17,21 @@ limitations under the License.
 package autoprovision
 
 import (
-	"io"
-
-	"k8s.io/kubernetes/pkg/client/cache"
-	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-
 	"fmt"
+	"io"
 
 	"k8s.io/kubernetes/pkg/admission"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/errors"
+	"k8s.io/kubernetes/pkg/client/cache"
+	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/controller/informers"
+	kubeapiserveradmission "k8s.io/kubernetes/pkg/kubeapiserver/admission"
 )
 
 func init() {
-	admission.RegisterPlugin("NamespaceAutoProvision", func(client clientset.Interface, config io.Reader) (admission.Interface, error) {
-		return NewProvision(client), nil
+	admission.RegisterPlugin("NamespaceAutoProvision", func(config io.Reader) (admission.Interface, error) {
+		return NewProvision(), nil
 	})
 }
 
@@ -41,11 +40,12 @@ func init() {
 // It is useful in deployments that do not want to restrict creation of a namespace prior to its usage.
 type provision struct {
 	*admission.Handler
-	client            clientset.Interface
+	client            internalclientset.Interface
 	namespaceInformer cache.SharedIndexInformer
 }
 
-var _ = admission.WantsInformerFactory(&provision{})
+var _ = kubeapiserveradmission.WantsInformerFactory(&provision{})
+var _ = kubeapiserveradmission.WantsInformerFactory(&provision{})
 
 func (p *provision) Admit(a admission.Attributes) (err error) {
 	// if we're here, then we've already passed authentication, so we're allowed to do what we're trying to do
@@ -80,11 +80,14 @@ func (p *provision) Admit(a admission.Attributes) (err error) {
 }
 
 // NewProvision creates a new namespace provision admission control handler
-func NewProvision(c clientset.Interface) admission.Interface {
+func NewProvision() admission.Interface {
 	return &provision{
 		Handler: admission.NewHandler(admission.Create),
-		client:  c,
 	}
+}
+
+func (a *provision) SetInternalClientSet(client internalclientset.Interface) {
+	a.client = client
 }
 
 func (p *provision) SetInformerFactory(f informers.SharedInformerFactory) {
@@ -95,6 +98,9 @@ func (p *provision) SetInformerFactory(f informers.SharedInformerFactory) {
 func (p *provision) Validate() error {
 	if p.namespaceInformer == nil {
 		return fmt.Errorf("missing namespaceInformer")
+	}
+	if p.client == nil {
+		return fmt.Errorf("missing client")
 	}
 	return nil
 }

plugin/pkg/admission/namespace/exists/admission.go
@@ -17,23 +17,22 @@ limitations under the License.
 package exists
 
 import (
-	"io"
-
-	"k8s.io/kubernetes/pkg/client/cache"
-	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-
 	"fmt"
+	"io"
 
 	"k8s.io/kubernetes/pkg/admission"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/errors"
 	metav1 "k8s.io/kubernetes/pkg/apis/meta/v1"
+	"k8s.io/kubernetes/pkg/client/cache"
+	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/controller/informers"
+	kubeapiserveradmission "k8s.io/kubernetes/pkg/kubeapiserver/admission"
 )
 
 func init() {
-	admission.RegisterPlugin("NamespaceExists", func(client clientset.Interface, config io.Reader) (admission.Interface, error) {
-		return NewExists(client), nil
+	admission.RegisterPlugin("NamespaceExists", func(config io.Reader) (admission.Interface, error) {
+		return NewExists(), nil
 	})
 }
 
@@ -42,11 +41,12 @@ func init() {
 // It is useful in deployments that want to enforce pre-declaration of a Namespace resource.
 type exists struct {
 	*admission.Handler
-	client            clientset.Interface
+	client            internalclientset.Interface
 	namespaceInformer cache.SharedIndexInformer
 }
 
-var _ = admission.WantsInformerFactory(&exists{})
+var _ = kubeapiserveradmission.WantsInformerFactory(&exists{})
+var _ = kubeapiserveradmission.WantsInternalClientSet(&exists{})
 
 func (e *exists) Admit(a admission.Attributes) (err error) {
 	// if we're here, then we've already passed authentication, so we're allowed to do what we're trying to do
@@ -88,11 +88,14 @@ func (e *exists) Admit(a admission.Attributes) (err error) {
 }
 
 // NewExists creates a new namespace exists admission control handler
-func NewExists(c clientset.Interface) admission.Interface {
+func NewExists() admission.Interface {
 	return &exists{
-		client:  c,
 		Handler: admission.NewHandler(admission.Create, admission.Update, admission.Delete),
 	}
+}
+
+func (a *exists) SetInternalClientSet(client internalclientset.Interface) {
+	a.client = client
 }
 
 func (e *exists) SetInformerFactory(f informers.SharedInformerFactory) {
@@ -103,6 +106,9 @@ func (e *exists) SetInformerFactory(f informers.SharedInformerFactory) {
 func (e *exists) Validate() error {
 	if e.namespaceInformer == nil {
 		return fmt.Errorf("missing namespaceInformer")
+	}
+	if e.client == nil {
+		return fmt.Errorf("missing client")
 	}
 	return nil
 }

cymtrick/kubernetes-error-fuzzer

plugin/pkg/admission/namespace/autoprovision/admission.go
@@ -17,22 +17,21 @@ limitations under the License.
 package autoprovision
 
 import (
-	"io"
-
-	"k8s.io/kubernetes/pkg/client/cache"
-	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-
 	"fmt"
+	"io"
 
 	"k8s.io/kubernetes/pkg/admission"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/errors"
+	"k8s.io/kubernetes/pkg/client/cache"
+	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/controller/informers"
+	kubeapiserveradmission "k8s.io/kubernetes/pkg/kubeapiserver/admission"
 )
 
 func init() {
-	admission.RegisterPlugin("NamespaceAutoProvision", func(client clientset.Interface, config io.Reader) (admission.Interface, error) {
-		return NewProvision(client), nil
+	admission.RegisterPlugin("NamespaceAutoProvision", func(config io.Reader) (admission.Interface, error) {
+		return NewProvision(), nil
 	})
 }
 
@@ -41,11 +40,12 @@ func init() {
 // It is useful in deployments that do not want to restrict creation of a namespace prior to its usage.
 type provision struct {
 	*admission.Handler
-	client            clientset.Interface
+	client            internalclientset.Interface
 	namespaceInformer cache.SharedIndexInformer
 }
 
-var _ = admission.WantsInformerFactory(&provision{})
+var _ = kubeapiserveradmission.WantsInformerFactory(&provision{})
+var _ = kubeapiserveradmission.WantsInformerFactory(&provision{})
 
 func (p *provision) Admit(a admission.Attributes) (err error) {
 	// if we're here, then we've already passed authentication, so we're allowed to do what we're trying to do
@@ -80,11 +80,14 @@ func (p *provision) Admit(a admission.Attributes) (err error) {
 }
 
 // NewProvision creates a new namespace provision admission control handler
-func NewProvision(c clientset.Interface) admission.Interface {
+func NewProvision() admission.Interface {
 	return &provision{
 		Handler: admission.NewHandler(admission.Create),
-		client:  c,
 	}
+}
+
+func (p *provision) SetInternalClientSet(client internalclientset.Interface) {
+	p.client = client
 }
 
 func (p *provision) SetInformerFactory(f informers.SharedInformerFactory) {
@@ -95,6 +98,9 @@ func (p *provision) SetInformerFactory(f informers.SharedInformerFactory) {
 func (p *provision) Validate() error {
 	if p.namespaceInformer == nil {
 		return fmt.Errorf("missing namespaceInformer")
+	}
+	if p.client == nil {
+		return fmt.Errorf("missing client")
 	}
 	return nil
 }

plugin/pkg/admission/namespace/exists/admission.go
@@ -17,23 +17,22 @@ limitations under the License.
 package exists
 
 import (
-	"io"
-
-	"k8s.io/kubernetes/pkg/client/cache"
-	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
-
 	"fmt"
+	"io"
 
 	"k8s.io/kubernetes/pkg/admission"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/errors"
 	metav1 "k8s.io/kubernetes/pkg/apis/meta/v1"
+	"k8s.io/kubernetes/pkg/client/cache"
+	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/controller/informers"
+	kubeapiserveradmission "k8s.io/kubernetes/pkg/kubeapiserver/admission"
 )
 
 func init() {
-	admission.RegisterPlugin("NamespaceExists", func(client clientset.Interface, config io.Reader) (admission.Interface, error) {
-		return NewExists(client), nil
+	admission.RegisterPlugin("NamespaceExists", func(config io.Reader) (admission.Interface, error) {
+		return NewExists(), nil
 	})
 }
 
@@ -42,11 +41,12 @@ func init() {
 // It is useful in deployments that want to enforce pre-declaration of a Namespace resource.
 type exists struct {
 	*admission.Handler
-	client            clientset.Interface
+	client            internalclientset.Interface
 	namespaceInformer cache.SharedIndexInformer
 }
 
-var _ = admission.WantsInformerFactory(&exists{})
+var _ = kubeapiserveradmission.WantsInformerFactory(&exists{})
+var _ = kubeapiserveradmission.WantsInternalClientSet(&exists{})
 
 func (e *exists) Admit(a admission.Attributes) (err error) {
 	// if we're here, then we've already passed authentication, so we're allowed to do what we're trying to do
@@ -88,11 +88,14 @@ func (e *exists) Admit(a admission.Attributes) (err error) {
 }
 
 // NewExists creates a new namespace exists admission control handler
-func NewExists(c clientset.Interface) admission.Interface {
+func NewExists() admission.Interface {
 	return &exists{
-		client:  c,
 		Handler: admission.NewHandler(admission.Create, admission.Update, admission.Delete),
 	}
+}
+
+func (e *exists) SetInternalClientSet(client internalclientset.Interface) {
+	e.client = client
 }
 
 func (e *exists) SetInformerFactory(f informers.SharedInformerFactory) {
@@ -103,6 +106,9 @@ func (e *exists) SetInformerFactory(f informers.SharedInformerFactory) {
 func (e *exists) Validate() error {
 	if e.namespaceInformer == nil {
 		return fmt.Errorf("missing namespaceInformer")
+	}
+	if e.client == nil {
+		return fmt.Errorf("missing client")
 	}
 	return nil
 }