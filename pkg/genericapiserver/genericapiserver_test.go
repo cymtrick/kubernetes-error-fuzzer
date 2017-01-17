kubernetes/kubernetes

genericapiserver: cut off kube pkg/version dependency

pkg/genericapiserver/genericapiserver_test.go
@@ -25,7 +25,12 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	goruntime "runtime"
 	"testing"
+	"time"
+
+	"github.com/go-openapi/spec"
+	"github.com/stretchr/testify/assert"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/apimachinery"
@@ -36,6 +41,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
+	"k8s.io/apiserver/pkg/version"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/rest"
 	"k8s.io/kubernetes/pkg/api/testapi"
@@ -44,10 +50,6 @@ import (
 	openapigen "k8s.io/kubernetes/pkg/generated/openapi"
 	genericapirequest "k8s.io/kubernetes/pkg/genericapiserver/api/request"
 	etcdtesting "k8s.io/kubernetes/pkg/storage/etcd/testing"
-	"k8s.io/kubernetes/pkg/version"
-
-	"github.com/go-openapi/spec"
-	"github.com/stretchr/testify/assert"
 )
 
 // setUp is a convience function for setting up for (most) tests.
@@ -372,7 +374,7 @@ func TestNotRestRoutesHaveAuth(t *testing.T) {
 	config.EnableProfiling = true
 	config.SwaggerConfig = DefaultSwaggerConfig()
 
-	kubeVersion := version.Get()
+	kubeVersion := fakeVersion()
 	config.Version = &kubeVersion
 
 	s, err := config.SkipComplete().New()
@@ -616,5 +618,19 @@ func (p *testNoVerbsStorage) New() runtime.Object {
 			Kind:       "NoVerbs",
 			APIVersion: p.Version,
 		},
+	}
+}
+
+func fakeVersion() version.Info {
+	return version.Info{
+		Major:        "42",
+		Minor:        "42",
+		GitVersion:   "42",
+		GitCommit:    "34973274ccef6ab4dfaaf86599792fa9c3fe4689",
+		GitTreeState: "Dirty",
+		BuildDate:    time.Now().String(),
+		GoVersion:    goruntime.Version(),
+		Compiler:     goruntime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", goruntime.GOOS, goruntime.GOARCH),
 	}
 }

pkg/version/version.go
@@ -19,29 +19,16 @@ package version
 import (
 	"fmt"
 	"runtime"
-)
 
-// Info contains versioning information.
-// TODO: Add []string of api versions supported? It's still unclear
-// how we'll want to distribute that information.
-type Info struct {
-	Major        string `json:"major"`
-	Minor        string `json:"minor"`
-	GitVersion   string `json:"gitVersion"`
-	GitCommit    string `json:"gitCommit"`
-	GitTreeState string `json:"gitTreeState"`
-	BuildDate    string `json:"buildDate"`
-	GoVersion    string `json:"goVersion"`
-	Compiler     string `json:"compiler"`
-	Platform     string `json:"platform"`
-}
+	apiserverversion "k8s.io/apiserver/pkg/version"
+)
 
 // Get returns the overall codebase version. It's for detecting
 // what code a binary was built from.
-func Get() Info {
+func Get() apiserverversion.Info {
 	// These variables typically come from -ldflags settings and in
 	// their absence fallback to the settings in pkg/version/base.go
-	return Info{
+	return apiserverversion.Info{
 		Major:        gitMajor,
 		Minor:        gitMinor,
 		GitVersion:   gitVersion,
@@ -52,9 +39,4 @@ func Get() Info {
 		Compiler:     runtime.Compiler,
 		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	}
-}
-
-// String returns info as a human-friendly version string.
-func (info Info) String() string {
-	return info.GitVersion
 }

cymtrick/kubernetes-error-fuzzer

pkg/genericapiserver/genericapiserver_test.go
@@ -25,7 +25,12 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	goruntime "runtime"
 	"testing"
+	"time"
+
+	"github.com/go-openapi/spec"
+	"github.com/stretchr/testify/assert"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/apimachinery"
@@ -34,6 +39,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilnet "k8s.io/apimachinery/pkg/util/net"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/version"
 	"k8s.io/apiserver/pkg/authentication/user"
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	genericapirequest "k8s.io/apiserver/pkg/request"
@@ -44,10 +50,6 @@ import (
 	openapigen "k8s.io/kubernetes/pkg/generated/openapi"
 	"k8s.io/kubernetes/pkg/genericapiserver/api/rest"
 	etcdtesting "k8s.io/kubernetes/pkg/storage/etcd/testing"
-	"k8s.io/kubernetes/pkg/version"
-
-	"github.com/go-openapi/spec"
-	"github.com/stretchr/testify/assert"
 )
 
 // setUp is a convience function for setting up for (most) tests.
@@ -372,7 +374,7 @@ func TestNotRestRoutesHaveAuth(t *testing.T) {
 	config.EnableProfiling = true
 	config.SwaggerConfig = DefaultSwaggerConfig()
 
-	kubeVersion := version.Get()
+	kubeVersion := fakeVersion()
 	config.Version = &kubeVersion
 
 	s, err := config.SkipComplete().New()
@@ -616,5 +618,19 @@ func (p *testNoVerbsStorage) New() runtime.Object {
 			Kind:       "NoVerbs",
 			APIVersion: p.Version,
 		},
+	}
+}
+
+func fakeVersion() version.Info {
+	return version.Info{
+		Major:        "42",
+		Minor:        "42",
+		GitVersion:   "42",
+		GitCommit:    "34973274ccef6ab4dfaaf86599792fa9c3fe4689",
+		GitTreeState: "Dirty",
+		BuildDate:    time.Now().String(),
+		GoVersion:    goruntime.Version(),
+		Compiler:     goruntime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", goruntime.GOOS, goruntime.GOARCH),
 	}
 }

pkg/version/version.go
@@ -19,29 +19,16 @@ package version
 import (
 	"fmt"
 	"runtime"
-)
 
-// Info contains versioning information.
-// TODO: Add []string of api versions supported? It's still unclear
-// how we'll want to distribute that information.
-type Info struct {
-	Major        string `json:"major"`
-	Minor        string `json:"minor"`
-	GitVersion   string `json:"gitVersion"`
-	GitCommit    string `json:"gitCommit"`
-	GitTreeState string `json:"gitTreeState"`
-	BuildDate    string `json:"buildDate"`
-	GoVersion    string `json:"goVersion"`
-	Compiler     string `json:"compiler"`
-	Platform     string `json:"platform"`
-}
+	apimachineryversion "k8s.io/apimachinery/pkg/version"
+)
 
 // Get returns the overall codebase version. It's for detecting
 // what code a binary was built from.
-func Get() Info {
+func Get() apimachineryversion.Info {
 	// These variables typically come from -ldflags settings and in
 	// their absence fallback to the settings in pkg/version/base.go
-	return Info{
+	return apimachineryversion.Info{
 		Major:        gitMajor,
 		Minor:        gitMinor,
 		GitVersion:   gitVersion,
@@ -52,9 +39,4 @@ func Get() Info {
 		Compiler:     runtime.Compiler,
 		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	}
-}
-
-// String returns info as a human-friendly version string.
-func (info Info) String() string {
-	return info.GitVersion
 }