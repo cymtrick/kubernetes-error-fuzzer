kubernetes/kubernetes

pass pod annotation to SetUpPod

pkg/kubelet/rkt/rkt.go
@@ -1284,7 +1284,7 @@ func (r *Runtime) setupPodNetwork(pod *v1.Pod) (string, string, error) {
 	// Set up networking with the network plugin
 	glog.V(3).Infof("Calling network plugin %s to setup pod for %s", r.networkPlugin.Name(), format.Pod(pod))
 	containerID := kubecontainer.ContainerID{ID: string(pod.UID)}
-	err = r.networkPlugin.SetUpPod(pod.Namespace, pod.Name, containerID)
+	err = r.networkPlugin.SetUpPod(pod.Namespace, pod.Name, containerID, pod.Annotations)
 	if err != nil {
 		return "", "", fmt.Errorf("failed to set up pod network: %v", err)
 	}

cymtrick/kubernetes-error-fuzzer

pkg/kubelet/rkt/rkt.go
@@ -1297,7 +1297,7 @@ func (r *Runtime) setupPodNetwork(pod *v1.Pod) (string, string, error) {
 
 	// Set up networking with the network plugin
 	containerID := kubecontainer.ContainerID{ID: string(pod.UID)}
-	err = r.network.SetUpPod(pod.Namespace, pod.Name, containerID)
+	err = r.network.SetUpPod(pod.Namespace, pod.Name, containerID, pod.Annotations)
 	if err != nil {
 		return "", "", err
 	}