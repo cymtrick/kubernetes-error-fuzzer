kubernetes/kubernetes

registry changes

staging/src/k8s.io/apiserver/pkg/registry/generic/registry/store.go
@@ -531,52 +531,121 @@ var (
 	errEmptiedFinalizers = fmt.Errorf("emptied finalizers")
 )
 
-// shouldUpdateFinalizers returns if we need to update the finalizers of the
-// object, and the desired list of finalizers. When deciding whether to add
-// the OrphanDependent finalizer, factors in the order of highest to lowest
-// priority are:
-//
-// - options.OrphanDependents,
-// - existing finalizers of the object
-// - e.DeleteStrategy.DefaultGarbageCollectionPolicy
-func shouldUpdateFinalizers(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, newFinalizers []string) {
-	shouldOrphan := false
+// shouldUpdateFinalizerOrphanDependents returns if the finalizers need to be
+// updated for FinalizerOrphanDependents. In the order of highest to lowest
+// priority, there are three factors affect whether to add/remove the
+// FinalizerOrphanDependents: options, existing finalizers of the object,
+// and e.DeleteStrategy.DefaultGarbageCollectionPolicy.
+func shouldUpdateFinalizerOrphanDependents(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, shouldOrphan bool) {
+	shouldOrphan = false
 	// Get default orphan policy from this REST object type
 	if gcStrategy, ok := e.DeleteStrategy.(rest.GarbageCollectionDeleteStrategy); ok {
 		if gcStrategy.DefaultGarbageCollectionPolicy() == rest.OrphanDependents {
 			shouldOrphan = true
 		}
 	}
+
 	// If a finalizer is set in the object, it overrides the default
 	hasOrphanFinalizer := false
 	finalizers := accessor.GetFinalizers()
 	for _, f := range finalizers {
-		if f == metav1.FinalizerOrphan {
+		// validation should make sure the two cases won't be true at the same
+		// time.
+		switch f {
+		case metav1.FinalizerOrphanDependents:
 			shouldOrphan = true
 			hasOrphanFinalizer = true
 			break
+		case metav1.FinalizerDeleteDependents:
+			shouldOrphan = false
+			break
 		}
-		// TODO: update this when we add a finalizer indicating a preference for the other behavior
 	}
+
 	// If an explicit policy was set at deletion time, that overrides both
 	if options != nil && options.OrphanDependents != nil {
 		shouldOrphan = *options.OrphanDependents
 	}
-	if shouldOrphan && !hasOrphanFinalizer {
-		finalizers = append(finalizers, metav1.FinalizerOrphan)
-		return true, finalizers
-	}
-	if !shouldOrphan && hasOrphanFinalizer {
-		var newFinalizers []string
-		for _, f := range finalizers {
-			if f == metav1.FinalizerOrphan {
-				continue
-			}
-			newFinalizers = append(newFinalizers, f)
+	if options != nil && options.PropagationPolicy != nil {
+		switch *options.PropagationPolicy {
+		case metav1.DeletePropagationOrphan:
+			shouldOrphan = true
+		case metav1.DeletePropagationBackground, metav1.DeletePropagationForeground:
+			shouldOrphan = false
 		}
-		return true, newFinalizers
 	}
-	return false, finalizers
+
+	shouldUpdate = shouldOrphan != hasOrphanFinalizer
+	return shouldUpdate, shouldOrphan
+}
+
+// shouldUpdateFinalizerDeleteDependents returns if the finalizers need to be
+// updated for FinalizerDeleteDependents. In the order of highest to lowest
+// priority, there are three factors affect whether to add/remove the
+// FinalizerDeleteDependents: options, existing finalizers of the object, and
+// e.DeleteStrategy.DefaultGarbageCollectionPolicy.
+func shouldUpdateFinalizerDeletingDependents(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, shouldDeleteInForeground bool) {
+	// default to false
+	shouldDeleteInForeground = false
+
+	// If a finalizer is set in the object, it overrides the default
+	hasFinalizerDeleteDependents := false
+	finalizers := accessor.GetFinalizers()
+	for _, f := range finalizers {
+		// validation has made sure the two cases won't be true at the same
+		// time.
+		switch f {
+		case metav1.FinalizerDeleteDependents:
+			shouldDeleteInForeground = true
+			hasFinalizerDeleteDependents = true
+			break
+		case metav1.FinalizerOrphanDependents:
+			shouldDeleteInForeground = false
+			break
+		}
+	}
+
+	// If an explicit policy was set at deletion time, that overrides both
+	if options != nil && options.OrphanDependents != nil {
+		shouldDeleteInForeground = false
+	}
+	if options != nil && options.PropagationPolicy != nil {
+		switch *options.PropagationPolicy {
+		case metav1.DeletePropagationForeground:
+			shouldDeleteInForeground = true
+		case metav1.DeletePropagationBackground, metav1.DeletePropagationOrphan:
+			shouldDeleteInForeground = false
+		}
+	}
+
+	shouldUpdate = shouldDeleteInForeground != hasFinalizerDeleteDependents
+	return shouldUpdate, shouldDeleteInForeground
+}
+
+// shouldUpdateFinalizers returns if we need to update the finalizers of the
+// object, and the desired list of finalizers.
+func shouldUpdateFinalizers(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, newFinalizers []string) {
+	shouldUpdate1, shouldOrphan := shouldUpdateFinalizerOrphanDependents(e, accessor, options)
+	shouldUpdate2, shouldDeleteInForeground := shouldUpdateFinalizerDeletingDependents(e, accessor, options)
+	oldFinalizers := accessor.GetFinalizers()
+	if !shouldUpdate1 && !shouldUpdate2 {
+		return false, oldFinalizers
+	}
+
+	// first remove both finalizers, add them back if needed.
+	for _, f := range oldFinalizers {
+		if f == metav1.FinalizerOrphanDependents || f == metav1.FinalizerDeleteDependents {
+			continue
+		}
+		newFinalizers = append(newFinalizers, f)
+	}
+	if shouldOrphan {
+		newFinalizers = append(newFinalizers, metav1.FinalizerOrphanDependents)
+	}
+	if shouldDeleteInForeground {
+		newFinalizers = append(newFinalizers, metav1.FinalizerDeleteDependents)
+	}
+	return true, newFinalizers
 }
 
 // markAsDeleting sets the obj's DeletionGracePeriodSeconds to 0, and sets the

cymtrick/kubernetes-error-fuzzer

staging/src/k8s.io/apiserver/pkg/registry/generic/registry/store.go
@@ -531,52 +531,121 @@ var (
 	errEmptiedFinalizers = fmt.Errorf("emptied finalizers")
 )
 
-// shouldUpdateFinalizers returns if we need to update the finalizers of the
-// object, and the desired list of finalizers. When deciding whether to add
-// the OrphanDependent finalizer, factors in the order of highest to lowest
-// priority are:
-//
-// - options.OrphanDependents,
-// - existing finalizers of the object
-// - e.DeleteStrategy.DefaultGarbageCollectionPolicy
-func shouldUpdateFinalizers(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, newFinalizers []string) {
-	shouldOrphan := false
+// shouldUpdateFinalizerOrphanDependents returns if the finalizers need to be
+// updated for FinalizerOrphanDependents. In the order of highest to lowest
+// priority, there are three factors affect whether to add/remove the
+// FinalizerOrphanDependents: options, existing finalizers of the object,
+// and e.DeleteStrategy.DefaultGarbageCollectionPolicy.
+func shouldUpdateFinalizerOrphanDependents(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, shouldOrphan bool) {
+	shouldOrphan = false
 	// Get default orphan policy from this REST object type
 	if gcStrategy, ok := e.DeleteStrategy.(rest.GarbageCollectionDeleteStrategy); ok {
 		if gcStrategy.DefaultGarbageCollectionPolicy() == rest.OrphanDependents {
 			shouldOrphan = true
 		}
 	}
+
 	// If a finalizer is set in the object, it overrides the default
 	hasOrphanFinalizer := false
 	finalizers := accessor.GetFinalizers()
 	for _, f := range finalizers {
-		if f == metav1.FinalizerOrphan {
+		// validation should make sure the two cases won't be true at the same
+		// time.
+		switch f {
+		case metav1.FinalizerOrphanDependents:
 			shouldOrphan = true
 			hasOrphanFinalizer = true
 			break
+		case metav1.FinalizerDeleteDependents:
+			shouldOrphan = false
+			break
 		}
-		// TODO: update this when we add a finalizer indicating a preference for the other behavior
 	}
+
 	// If an explicit policy was set at deletion time, that overrides both
 	if options != nil && options.OrphanDependents != nil {
 		shouldOrphan = *options.OrphanDependents
 	}
-	if shouldOrphan && !hasOrphanFinalizer {
-		finalizers = append(finalizers, metav1.FinalizerOrphan)
-		return true, finalizers
-	}
-	if !shouldOrphan && hasOrphanFinalizer {
-		var newFinalizers []string
-		for _, f := range finalizers {
-			if f == metav1.FinalizerOrphan {
-				continue
-			}
-			newFinalizers = append(newFinalizers, f)
+	if options != nil && options.PropagationPolicy != nil {
+		switch *options.PropagationPolicy {
+		case metav1.DeletePropagationOrphan:
+			shouldOrphan = true
+		case metav1.DeletePropagationBackground, metav1.DeletePropagationForeground:
+			shouldOrphan = false
 		}
-		return true, newFinalizers
 	}
-	return false, finalizers
+
+	shouldUpdate = shouldOrphan != hasOrphanFinalizer
+	return shouldUpdate, shouldOrphan
+}
+
+// shouldUpdateFinalizerDeleteDependents returns if the finalizers need to be
+// updated for FinalizerDeleteDependents. In the order of highest to lowest
+// priority, there are three factors affect whether to add/remove the
+// FinalizerDeleteDependents: options, existing finalizers of the object, and
+// e.DeleteStrategy.DefaultGarbageCollectionPolicy.
+func shouldUpdateFinalizerDeleteDependents(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, shouldDeleteDependentInForeground bool) {
+	// default to false
+	shouldDeleteDependentInForeground = false
+
+	// If a finalizer is set in the object, it overrides the default
+	hasFinalizerDeleteDependents := false
+	finalizers := accessor.GetFinalizers()
+	for _, f := range finalizers {
+		// validation has made sure the two cases won't be true at the same
+		// time.
+		switch f {
+		case metav1.FinalizerDeleteDependents:
+			shouldDeleteDependentInForeground = true
+			hasFinalizerDeleteDependents = true
+			break
+		case metav1.FinalizerOrphanDependents:
+			shouldDeleteDependentInForeground = false
+			break
+		}
+	}
+
+	// If an explicit policy was set at deletion time, that overrides both
+	if options != nil && options.OrphanDependents != nil {
+		shouldDeleteDependentInForeground = false
+	}
+	if options != nil && options.PropagationPolicy != nil {
+		switch *options.PropagationPolicy {
+		case metav1.DeletePropagationForeground:
+			shouldDeleteDependentInForeground = true
+		case metav1.DeletePropagationBackground, metav1.DeletePropagationOrphan:
+			shouldDeleteDependentInForeground = false
+		}
+	}
+
+	shouldUpdate = shouldDeleteDependentInForeground != hasFinalizerDeleteDependents
+	return shouldUpdate, shouldDeleteDependentInForeground
+}
+
+// shouldUpdateFinalizers returns if we need to update the finalizers of the
+// object, and the desired list of finalizers.
+func shouldUpdateFinalizers(e *Store, accessor metav1.Object, options *metav1.DeleteOptions) (shouldUpdate bool, newFinalizers []string) {
+	shouldUpdate1, shouldOrphan := shouldUpdateFinalizerOrphanDependents(e, accessor, options)
+	shouldUpdate2, shouldDeleteDependentInForeground := shouldUpdateFinalizerDeleteDependents(e, accessor, options)
+	oldFinalizers := accessor.GetFinalizers()
+	if !shouldUpdate1 && !shouldUpdate2 {
+		return false, oldFinalizers
+	}
+
+	// first remove both finalizers, add them back if needed.
+	for _, f := range oldFinalizers {
+		if f == metav1.FinalizerOrphanDependents || f == metav1.FinalizerDeleteDependents {
+			continue
+		}
+		newFinalizers = append(newFinalizers, f)
+	}
+	if shouldOrphan {
+		newFinalizers = append(newFinalizers, metav1.FinalizerOrphanDependents)
+	}
+	if shouldDeleteDependentInForeground {
+		newFinalizers = append(newFinalizers, metav1.FinalizerDeleteDependents)
+	}
+	return true, newFinalizers
 }
 
 // markAsDeleting sets the obj's DeletionGracePeriodSeconds to 0, and sets the