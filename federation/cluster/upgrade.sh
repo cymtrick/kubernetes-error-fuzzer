kubernetes/kubernetes

Add framework for federation upgrade tests

federation/cluster/upgrade.sh
@@ -1,0 +1,46 @@
+#!/bin/bash
+
+# Copyright 2014 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
+# For `kube::log::status` function since it already sources
+# "${KUBE_ROOT}/cluster/lib/logging.sh"
+source "${KUBE_ROOT}/cluster/common.sh"
+# For $FEDERATION_NAME, $FEDERATION_NAMESPACE, $HOST_CLUSTER_CONTEXT,
+source "${KUBE_ROOT}/federation/cluster/common.sh"
+
+HYPERKUBE_IMAGE_VERSION="${1}"
+
+host_kubectl="${KUBE_ROOT}/cluster/kubectl.sh --context=${HOST_CLUSTER_CONTEXT} --namespace=${FEDERATION_NAMESPACE}"
+
+function upgrade() {
+  local -r project="${KUBE_PROJECT:-${PROJECT:-}}"
+  local -r kube_registry="${KUBE_REGISTRY:-gcr.io/${project}}"
+  local -r hyperkube_image="${kube_registry}/hyperkube-amd64:${HYPERKUBE_IMAGE_VERSION}"
+
+  kube::log::status "Upgrading federation control plane ${FEDERATION_NAME} with image ${hyperkube_image}"
+
+  # Upgrade apiserver image
+  ${host_kubectl} set image deployment/federation-apiserver apiserver=${hyperkube_image}
+
+  # Upgrade controller-manager image
+  ${host_kubectl} set image deployment/federation-controller-manager controller-manager=${hyperkube_image}
+}
+
+upgrade

test/e2e_federation/framework/util.go
@@ -19,7 +19,9 @@ package framework
 import (
 	"fmt"
 	"os"
+	"path"
 	"regexp"
+	"strings"
 	"time"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -109,4 +111,46 @@ func GetValidDNSSubdomainName(name string) (string, error) {
 		return "", fmt.Errorf("errors in converting name to a valid DNS subdomain %s", errors)
 	}
 	return name, nil
+}
+
+func FederationControlPlaneUpgrade(version string) error {
+	version = "v" + version
+	_, _, err := framework.RunCmd(path.Join(framework.TestContext.RepoRoot, "federation/cluster/upgrade.sh"), version)
+	return err
+}
+
+func CheckFederationVersion(c federation_clientset.Interface, want string) error {
+	framework.Logf("Checking federation version")
+	v, err := c.Discovery().ServerVersion()
+	if err != nil {
+		return fmt.Errorf("CheckFederationVersion() couldn't get the master version: %v", err)
+	}
+	// We do prefix trimming and then matching because:
+	// want looks like:  0.19.3-815-g50e67d4
+	// got  looks like: v0.19.3-815-g50e67d4034e858-dirty
+	got := strings.TrimPrefix(v.GitVersion, "v")
+	if !strings.HasPrefix(got, want) {
+		return fmt.Errorf("federation had apiserver version %s which does not start with %s",
+			got, want)
+	}
+	framework.Logf("Federation is at version %s", want)
+	return nil
+}
+
+func MasterUpgrade(context, version string) error {
+	switch framework.TestContext.Provider {
+	case "gce":
+		return masterUpgradeGCE(context, version)
+	default:
+		return fmt.Errorf("MasterUpgrade() is not implemented for provider %s", framework.TestContext.Provider)
+	}
+}
+
+func masterUpgradeGCE(context, rawVersion string) error {
+	version := "v" + rawVersion
+	zone := strings.TrimPrefix(context, "federeation-e2e-"+framework.TestContext.Provider+"-")
+
+	env := append(os.Environ(), "KUBE_CONTEXT="+context, "ZONE="+zone)
+	_, _, err := framework.RunCmdEnv(env, path.Join(framework.TestContext.RepoRoot, "cluster/gce/upgrade.sh"), "-M", version)
+	return err
 }

cymtrick/kubernetes-error-fuzzer

federation/cluster/upgrade.sh
@@ -1,0 +1,46 @@
+#!/bin/bash
+
+# Copyright 2017 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
+# For `kube::log::status` function since it already sources
+# "${KUBE_ROOT}/cluster/lib/logging.sh"
+source "${KUBE_ROOT}/cluster/common.sh"
+# For $FEDERATION_NAME, $FEDERATION_NAMESPACE, $HOST_CLUSTER_CONTEXT,
+source "${KUBE_ROOT}/federation/cluster/common.sh"
+
+KUBE_VERSION="${1}"
+
+host_kubectl="${KUBE_ROOT}/cluster/kubectl.sh --context=${HOST_CLUSTER_CONTEXT} --namespace=${FEDERATION_NAMESPACE}"
+
+function upgrade() {
+  local -r project="${KUBE_PROJECT:-${PROJECT:-}}"
+  local -r kube_registry="${KUBE_REGISTRY:-gcr.io/${project}}"
+  local -r image_version="${kube_registry}/hyperkube-amd64:${KUBE_VERSION}"
+
+  kube::log::status "Upgrading federation control plane ${FEDERATION_NAME} with image ${image_version}"
+
+  # Upgrade apiserver image
+  ${host_kubectl} set image deployment/federation-apiserver apiserver=${image_version}
+
+  # Upgrade controller-manager image
+  ${host_kubectl} set image deployment/federation-controller-manager controller-manager=${image_version}
+}
+
+upgrade

test/e2e_federation/framework/util.go
@@ -19,7 +19,9 @@ package framework
 import (
 	"fmt"
 	"os"
+	"path"
 	"regexp"
+	"strings"
 	"time"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -109,4 +111,47 @@ func GetValidDNSSubdomainName(name string) (string, error) {
 		return "", fmt.Errorf("errors in converting name to a valid DNS subdomain %s", errors)
 	}
 	return name, nil
+}
+
+func FederationControlPlaneUpgrade(version string) error {
+	version = "v" + version
+	_, _, err := framework.RunCmd(path.Join(framework.TestContext.RepoRoot, "federation/cluster/upgrade.sh"), version)
+	return err
+}
+
+func CheckFederationVersion(c federation_clientset.Interface, want string) error {
+	framework.Logf("Checking federation version")
+	v, err := c.Discovery().ServerVersion()
+	if err != nil {
+		return fmt.Errorf("CheckFederationVersion() couldn't get the master version: %v", err)
+	}
+	// We do prefix trimming and then matching because:
+	// want looks like:  0.19.3-815-g50e67d4
+	// got  looks like: v0.19.3-815-g50e67d4034e858-dirty
+	got := strings.TrimPrefix(v.GitVersion, "v")
+	if !strings.HasPrefix(got, want) {
+		return fmt.Errorf("federation had apiserver version %s which does not start with %s",
+			got, want)
+	}
+	framework.Logf("Federation is at version %s", want)
+	return nil
+}
+
+func MasterUpgrade(context, version string) error {
+	switch framework.TestContext.Provider {
+	case "gce":
+		return masterUpgradeGCE(context, version)
+	default:
+		return fmt.Errorf("MasterUpgrade() is not implemented for provider %s", framework.TestContext.Provider)
+	}
+}
+
+func masterUpgradeGCE(context, rawVersion string) error {
+	version := "v" + rawVersion
+	// TODO: this breaks if we want to upgrade 2 clusters in same zone. use alternate methods in future to get zone of a cluster
+	zone := strings.TrimPrefix(context, "federation-e2e-"+framework.TestContext.Provider+"-")
+
+	env := append(os.Environ(), "KUBE_CONTEXT="+context, "ZONE="+zone)
+	_, _, err := framework.RunCmdEnv(env, path.Join(framework.TestContext.RepoRoot, "cluster/gce/upgrade.sh"), "-M", version)
+	return err
 }