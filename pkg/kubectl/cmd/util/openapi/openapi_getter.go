kubernetes/kubernetes

kubectl OpenAPI support

pkg/kubectl/cmd/util/openapi/openapi_getter.go
@@ -1,0 +1,71 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package openapi
+
+import (
+	"sync"
+
+	"k8s.io/client-go/discovery"
+)
+
+// synchronizedOpenAPIGetter fetches the openapi schema once and then caches it in memory
+type synchronizedOpenAPIGetter struct {
+	// Cached results
+	sync.Once
+	openAPISchema *Resources
+	err           error
+
+	serverVersion string
+	cacheDir      string
+	openAPIClient discovery.OpenAPISchemaInterface
+}
+
+var _ Getter = &synchronizedOpenAPIGetter{}
+
+// Getter is an interface for fetching openapi specs and parsing them into an Resources struct
+type Getter interface {
+	// openAPIData returns the parsed openAPIData
+	OpenAPIData() (*Resources, error)
+}
+
+// NewOpenAPIGetter returns an object to return OpenAPIDatas which either read from a
+// local file cache or read from a server, and then stored in memory for subsequent invocations
+func NewOpenAPIGetter(cacheDir, serverVersion string, openAPIClient discovery.OpenAPISchemaInterface) Getter {
+	return &synchronizedOpenAPIGetter{
+		serverVersion: serverVersion,
+		cacheDir:      cacheDir,
+		openAPIClient: openAPIClient,
+	}
+}
+
+// Resources implements Getter
+func (g *synchronizedOpenAPIGetter) OpenAPIData() (*Resources, error) {
+	g.Do(func() {
+		client := newCachingOpenAPIClient(g.openAPIClient, g.serverVersion, g.cacheDir)
+		result, err := client.openAPIData()
+		if err != nil {
+			g.err = err
+			return
+		}
+
+		// Save the result
+		g.openAPISchema = result
+	})
+
+	// Return the save result
+	return g.openAPISchema, g.err
+}

pkg/kubectl/cmd/util/openapi/openapi_getter_test.go
@@ -1,0 +1,74 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package openapi
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Getting the Resources", func() {
+	var client *fakeOpenAPIClient
+	var expectedData *Resources
+	var instance Getter
+
+	BeforeEach(func() {
+		client = &fakeOpenAPIClient{}
+		d, err := data.OpenAPISchema()
+		Expect(err).To(BeNil())
+
+		expectedData, err = newOpenAPIData(d)
+		Expect(err).To(BeNil())
+
+		instance = NewOpenAPIGetter("", "", client)
+	})
+
+	Context("when the server returns a successful result", func() {
+		It("should return the same data for multiple calls", func() {
+			Expect(client.calls).To(Equal(0))
+
+			result, err := instance.OpenAPIData()
+			Expect(err).To(BeNil())
+			expectEqual(result, expectedData)
+			Expect(client.calls).To(Equal(1))
+
+			result, err = instance.OpenAPIData()
+			Expect(err).To(BeNil())
+			expectEqual(result, expectedData)
+			// No additional client calls expected
+			Expect(client.calls).To(Equal(1))
+		})
+	})
+
+	Context("when the server returns an unsuccessful result", func() {
+		It("should return the same instance for multiple calls.", func() {
+			Expect(client.calls).To(Equal(0))
+
+			client.err = fmt.Errorf("expected error")
+			_, err := instance.OpenAPIData()
+			Expect(err).To(Equal(client.err))
+			Expect(client.calls).To(Equal(1))
+
+			_, err = instance.OpenAPIData()
+			Expect(err).To(Equal(client.err))
+			// No additional client calls expected
+			Expect(client.calls).To(Equal(1))
+		})
+	})
+})

cymtrick/kubernetes-error-fuzzer

pkg/kubectl/cmd/util/openapi/openapi_getter.go
@@ -1,0 +1,71 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package openapi
+
+import (
+	"sync"
+
+	"k8s.io/client-go/discovery"
+)
+
+// synchronizedOpenAPIGetter fetches the openapi schema once and then caches it in memory
+type synchronizedOpenAPIGetter struct {
+	// Cached results
+	sync.Once
+	openAPISchema *Resources
+	err           error
+
+	serverVersion string
+	cacheDir      string
+	openAPIClient discovery.OpenAPISchemaInterface
+}
+
+var _ Getter = &synchronizedOpenAPIGetter{}
+
+// Getter is an interface for fetching openapi specs and parsing them into an Resources struct
+type Getter interface {
+	// openAPIData returns the parsed openAPIData
+	Get() (*Resources, error)
+}
+
+// NewOpenAPIGetter returns an object to return OpenAPIDatas which either read from a
+// local file cache or read from a server, and then stored in memory for subsequent invocations
+func NewOpenAPIGetter(cacheDir, serverVersion string, openAPIClient discovery.OpenAPISchemaInterface) Getter {
+	return &synchronizedOpenAPIGetter{
+		serverVersion: serverVersion,
+		cacheDir:      cacheDir,
+		openAPIClient: openAPIClient,
+	}
+}
+
+// Resources implements Getter
+func (g *synchronizedOpenAPIGetter) Get() (*Resources, error) {
+	g.Do(func() {
+		client := newCachingOpenAPIClient(g.openAPIClient, g.serverVersion, g.cacheDir)
+		result, err := client.openAPIData()
+		if err != nil {
+			g.err = err
+			return
+		}
+
+		// Save the result
+		g.openAPISchema = result
+	})
+
+	// Return the save result
+	return g.openAPISchema, g.err
+}

pkg/kubectl/cmd/util/openapi/openapi_getter_test.go
@@ -1,0 +1,74 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package openapi
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Getting the Resources", func() {
+	var client *fakeOpenAPIClient
+	var expectedData *Resources
+	var instance Getter
+
+	BeforeEach(func() {
+		client = &fakeOpenAPIClient{}
+		d, err := data.OpenAPISchema()
+		Expect(err).To(BeNil())
+
+		expectedData, err = newOpenAPIData(d)
+		Expect(err).To(BeNil())
+
+		instance = NewOpenAPIGetter("", "", client)
+	})
+
+	Context("when the server returns a successful result", func() {
+		It("should return the same data for multiple calls", func() {
+			Expect(client.calls).To(Equal(0))
+
+			result, err := instance.Get()
+			Expect(err).To(BeNil())
+			expectEqual(result, expectedData)
+			Expect(client.calls).To(Equal(1))
+
+			result, err = instance.Get()
+			Expect(err).To(BeNil())
+			expectEqual(result, expectedData)
+			// No additional client calls expected
+			Expect(client.calls).To(Equal(1))
+		})
+	})
+
+	Context("when the server returns an unsuccessful result", func() {
+		It("should return the same instance for multiple calls.", func() {
+			Expect(client.calls).To(Equal(0))
+
+			client.err = fmt.Errorf("expected error")
+			_, err := instance.Get()
+			Expect(err).To(Equal(client.err))
+			Expect(client.calls).To(Equal(1))
+
+			_, err = instance.Get()
+			Expect(err).To(Equal(client.err))
+			// No additional client calls expected
+			Expect(client.calls).To(Equal(1))
+		})
+	})
+})