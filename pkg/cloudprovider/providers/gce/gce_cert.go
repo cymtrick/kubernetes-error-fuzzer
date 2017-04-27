kubernetes/kubernetes

Add metrics to all major gce operations {latency, errors}
The new metrics is:

  cloudprovider_gce_api_duration_seconds{request, region, zone}
  cloudprovider_gce_api_errors{request, region, zone}

`request` is the specific function that is used.
`region` is the target region (Will be "<n/a>" if not applicable)
`zone` is the target zone (Will be "<n/a>" if not applicable)

Note: this fixes some issues with the previous implementation of
metrics for disks:
- Time duration tracked was of the initial API call, not the entire
  operation.
- Metrics label tuple would have resulted in many independent
  histograms stored, one for each disk. (Did not aggregate well).

pkg/cloudprovider/providers/gce/gce_cert.go
@@ -18,11 +18,17 @@ package gce
 
 import (
 	"net/http"
+	"time"
 
 	compute "google.golang.org/api/compute/v1"
 )
 
-// SSL Certificate management
+func newCertMetricContext(request string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"cert_" + request, unusedLabel, unusedLabel},
+	}
+}
 
 // GetSslCertificate returns the SslCertificate by name.
 func (gce *GCECloud) GetSslCertificate(name string) (*compute.SslCertificate, error) {
@@ -31,26 +37,34 @@ func (gce *GCECloud) GetSslCertificate(name string) (*compute.SslCertificate, er
 
 // CreateSslCertificate creates and returns a SslCertificate.
 func (gce *GCECloud) CreateSslCertificate(sslCerts *compute.SslCertificate) (*compute.SslCertificate, error) {
+	mc := newCertMetricContext("create")
 	op, err := gce.service.SslCertificates.Insert(gce.projectID, sslCerts).Do()
+
 	if err != nil {
-		return nil, err
+		return nil, mc.Observe(err)
 	}
-	if err = gce.waitForGlobalOp(op); err != nil {
-		return nil, err
+
+	if err = gce.waitForGlobalOp(op, mc); err != nil {
+		return nil, mc.Observe(err)
 	}
+
 	return gce.GetSslCertificate(sslCerts.Name)
 }
 
 // DeleteSslCertificate deletes the SslCertificate by name.
 func (gce *GCECloud) DeleteSslCertificate(name string) error {
+	mc := newCertMetricContext("delete")
 	op, err := gce.service.SslCertificates.Delete(gce.projectID, name).Do()
+
 	if err != nil {
 		if isHTTPErrorCode(err, http.StatusNotFound) {
 			return nil
 		}
-		return err
+
+		return mc.Observe(err)
 	}
-	return gce.waitForGlobalOp(op)
+
+	return gce.waitForGlobalOp(op, mc)
 }
 
 // ListSslCertificates lists all SslCertificates in the project.

pkg/cloudprovider/providers/gce/gce_firewall.go
@@ -17,14 +17,20 @@ limitations under the License.
 package gce
 
 import (
+	"time"
+
 	"k8s.io/kubernetes/pkg/api/v1"
 	netsets "k8s.io/kubernetes/pkg/util/net/sets"
 
 	compute "google.golang.org/api/compute/v1"
 )
 
-// Firewall management: These methods are just passthrough to the existing
-// internal firewall creation methods used to manage TCPLoadBalancer.
+func newFirewallMetricContext(request string, region string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"firewall_" + request, region, unusedLabel},
+	}
+}
 
 // GetFirewall returns the Firewall by name.
 func (gce *GCECloud) GetFirewall(name string) (*compute.Firewall, error) {
@@ -37,22 +43,29 @@ func (gce *GCECloud) CreateFirewall(name, desc string, sourceRanges netsets.IPNe
 	if err != nil {
 		return err
 	}
-	// TODO: This completely breaks modularity in the cloudprovider but the methods
-	// shared with the TCPLoadBalancer take v1.ServicePorts.
+
+	mc := newFirewallMetricContext("create", region)
+
+	// TODO: This completely breaks modularity in the cloudprovider but
+	// the methods shared with the TCPLoadBalancer take v1.ServicePorts.
 	svcPorts := []v1.ServicePort{}
 	// TODO: Currently the only consumer of this method is the GCE L7
-	// loadbalancer controller, which never needs a protocol other than TCP.
-	// We should pipe through a mapping of port:protocol and default to TCP
-	// if UDP ports are required. This means the method signature will change
-	// forcing downstream clients to refactor interfaces.
+	// loadbalancer controller, which never needs a protocol other than
+	// TCP.  We should pipe through a mapping of port:protocol and
+	// default to TCP if UDP ports are required. This means the method
+	// signature will change forcing downstream clients to refactor
+	// interfaces.
 	for _, p := range ports {
 		svcPorts = append(svcPorts, v1.ServicePort{Port: int32(p), Protocol: v1.ProtocolTCP})
 	}
+
 	hosts, err := gce.getInstancesByNames(hostNames)
 	if err != nil {
+		mc.Observe(err)
 		return err
 	}
-	return gce.createFirewall(name, region, desc, sourceRanges, svcPorts, hosts)
+
+	return mc.Observe(gce.createFirewall(name, region, desc, sourceRanges, svcPorts, hosts))
 }
 
 // DeleteFirewall deletes the given firewall rule.
@@ -61,30 +74,41 @@ func (gce *GCECloud) DeleteFirewall(name string) error {
 	if err != nil {
 		return err
 	}
-	return gce.deleteFirewall(name, region)
+
+	mc := newFirewallMetricContext("delete", region)
+
+	return mc.Observe(gce.deleteFirewall(name, region))
 }
 
-// UpdateFirewall applies the given firewall rule as an update to an existing
-// firewall rule with the same name.
+// UpdateFirewall applies the given firewall rule as an update to an
+// existing firewall rule with the same name.
 func (gce *GCECloud) UpdateFirewall(name, desc string, sourceRanges netsets.IPNet, ports []int64, hostNames []string) error {
+
 	region, err := GetGCERegion(gce.localZone)
 	if err != nil {
 		return err
 	}
-	// TODO: This completely breaks modularity in the cloudprovider but the methods
-	// shared with the TCPLoadBalancer take v1.ServicePorts.
+
+	mc := newFirewallMetricContext("update", region)
+
+	// TODO: This completely breaks modularity in the cloudprovider but
+	// the methods shared with the TCPLoadBalancer take v1.ServicePorts.
 	svcPorts := []v1.ServicePort{}
 	// TODO: Currently the only consumer of this method is the GCE L7
-	// loadbalancer controller, which never needs a protocol other than TCP.
-	// We should pipe through a mapping of port:protocol and default to TCP
-	// if UDP ports are required. This means the method signature will change,
-	// forcing downstream clients to refactor interfaces.
+	// loadbalancer controller, which never needs a protocol other than
+	// TCP.  We should pipe through a mapping of port:protocol and
+	// default to TCP if UDP ports are required. This means the method
+	// signature will change, forcing downstream clients to refactor
+	// interfaces.
 	for _, p := range ports {
 		svcPorts = append(svcPorts, v1.ServicePort{Port: int32(p), Protocol: v1.ProtocolTCP})
 	}
+
 	hosts, err := gce.getInstancesByNames(hostNames)
 	if err != nil {
+		mc.Observe(err)
 		return err
 	}
-	return gce.updateFirewall(name, region, desc, sourceRanges, svcPorts, hosts)
+
+	return mc.Observe(gce.updateFirewall(name, region, desc, sourceRanges, svcPorts, hosts))
 }

pkg/cloudprovider/providers/gce/gce_routes.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 	"path"
 	"strings"
+	"time"
 
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/kubernetes/pkg/cloudprovider"
@@ -28,6 +29,13 @@ import (
 	"github.com/golang/glog"
 	compute "google.golang.org/api/compute/v1"
 )
+
+func newRoutesMetricContext(request string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"routes_" + request, unusedLabel, unusedLabel},
+	}
+}
 
 func (gce *GCECloud) ListRoutes(clusterName string) ([]*cloudprovider.Route, error) {
 	var routes []*cloudprovider.Route
@@ -80,6 +88,8 @@ func (gce *GCECloud) CreateRoute(clusterName string, nameHint string, route *clo
 	if err != nil {
 		return err
 	}
+
+	mc := newRoutesMetricContext("create")
 	insertOp, err := gce.service.Routes.Insert(gce.projectID, &compute.Route{
 		Name:            routeName,
 		DestRange:       route.DestinationCIDR,
@@ -93,18 +103,19 @@ func (gce *GCECloud) CreateRoute(clusterName string, nameHint string, route *clo
 			glog.Info("Route %v already exists.")
 			return nil
 		} else {
-			return err
+			return mc.Observe(err)
 		}
 	}
-	return gce.waitForGlobalOp(insertOp)
+	return gce.waitForGlobalOp(insertOp, mc)
 }
 
 func (gce *GCECloud) DeleteRoute(clusterName string, route *cloudprovider.Route) error {
+	mc := newRoutesMetricContext("create")
 	deleteOp, err := gce.service.Routes.Delete(gce.projectID, route.Name).Do()
 	if err != nil {
-		return err
+		return mc.Observe(err)
 	}
-	return gce.waitForGlobalOp(deleteOp)
+	return gce.waitForGlobalOp(deleteOp, mc)
 }
 
 func truncateClusterName(clusterName string) string {

pkg/cloudprovider/providers/gce/gce_urlmap.go
@@ -18,11 +18,17 @@ package gce
 
 import (
 	"net/http"
+	"time"
 
 	compute "google.golang.org/api/compute/v1"
 )
 
-// UrlMap management
+func newUrlMapMetricContext(request string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"urlmap_" + request, unusedLabel, unusedLabel},
+	}
+}
 
 // GetUrlMap returns the UrlMap by name.
 func (gce *GCECloud) GetUrlMap(name string) (*compute.UrlMap, error) {
@@ -35,11 +41,12 @@ func (gce *GCECloud) CreateUrlMap(backend *compute.BackendService, name string) 
 		Name:           name,
 		DefaultService: backend.SelfLink,
 	}
+	mc := newUrlMapMetricContext("create")
 	op, err := gce.service.UrlMaps.Insert(gce.projectID, urlMap).Do()
 	if err != nil {
-		return nil, err
+		return nil, mc.Observe(err)
 	}
-	if err = gce.waitForGlobalOp(op); err != nil {
+	if err = gce.waitForGlobalOp(op, mc); err != nil {
 		return nil, err
 	}
 	return gce.GetUrlMap(name)
@@ -47,11 +54,12 @@ func (gce *GCECloud) CreateUrlMap(backend *compute.BackendService, name string) 
 
 // UpdateUrlMap applies the given UrlMap as an update, and returns the new UrlMap.
 func (gce *GCECloud) UpdateUrlMap(urlMap *compute.UrlMap) (*compute.UrlMap, error) {
+	mc := newUrlMapMetricContext("update")
 	op, err := gce.service.UrlMaps.Update(gce.projectID, urlMap.Name, urlMap).Do()
 	if err != nil {
-		return nil, err
+		return nil, mc.Observe(err)
 	}
-	if err = gce.waitForGlobalOp(op); err != nil {
+	if err = gce.waitForGlobalOp(op, mc); err != nil {
 		return nil, err
 	}
 	return gce.service.UrlMaps.Get(gce.projectID, urlMap.Name).Do()
@@ -59,14 +67,15 @@ func (gce *GCECloud) UpdateUrlMap(urlMap *compute.UrlMap) (*compute.UrlMap, erro
 
 // DeleteUrlMap deletes a url map by name.
 func (gce *GCECloud) DeleteUrlMap(name string) error {
+	mc := newUrlMapMetricContext("delete")
 	op, err := gce.service.UrlMaps.Delete(gce.projectID, name).Do()
 	if err != nil {
 		if isHTTPErrorCode(err, http.StatusNotFound) {
 			return nil
 		}
-		return err
+		return mc.Observe(err)
 	}
-	return gce.waitForGlobalOp(op)
+	return gce.waitForGlobalOp(op, mc)
 }
 
 // ListUrlMaps lists all UrlMaps in the project.

cymtrick/kubernetes-error-fuzzer

  cloudprovider_gce_api_request_duration_seconds{request, region, zone}
  cloudprovider_gce_api_request_errors{request, region, zone}

pkg/cloudprovider/providers/gce/gce_cert.go
@@ -18,11 +18,17 @@ package gce
 
 import (
 	"net/http"
+	"time"
 
 	compute "google.golang.org/api/compute/v1"
 )
 
-// SSL Certificate management
+func newCertMetricContext(request string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"cert_" + request, unusedMetricLabel, unusedMetricLabel},
+	}
+}
 
 // GetSslCertificate returns the SslCertificate by name.
 func (gce *GCECloud) GetSslCertificate(name string) (*compute.SslCertificate, error) {
@@ -31,26 +37,34 @@ func (gce *GCECloud) GetSslCertificate(name string) (*compute.SslCertificate, er
 
 // CreateSslCertificate creates and returns a SslCertificate.
 func (gce *GCECloud) CreateSslCertificate(sslCerts *compute.SslCertificate) (*compute.SslCertificate, error) {
+	mc := newCertMetricContext("create")
 	op, err := gce.service.SslCertificates.Insert(gce.projectID, sslCerts).Do()
+
 	if err != nil {
-		return nil, err
+		return nil, mc.Observe(err)
 	}
-	if err = gce.waitForGlobalOp(op); err != nil {
-		return nil, err
+
+	if err = gce.waitForGlobalOp(op, mc); err != nil {
+		return nil, mc.Observe(err)
 	}
+
 	return gce.GetSslCertificate(sslCerts.Name)
 }
 
 // DeleteSslCertificate deletes the SslCertificate by name.
 func (gce *GCECloud) DeleteSslCertificate(name string) error {
+	mc := newCertMetricContext("delete")
 	op, err := gce.service.SslCertificates.Delete(gce.projectID, name).Do()
+
 	if err != nil {
 		if isHTTPErrorCode(err, http.StatusNotFound) {
 			return nil
 		}
-		return err
+
+		return mc.Observe(err)
 	}
-	return gce.waitForGlobalOp(op)
+
+	return gce.waitForGlobalOp(op, mc)
 }
 
 // ListSslCertificates lists all SslCertificates in the project.

pkg/cloudprovider/providers/gce/gce_firewall.go
@@ -17,14 +17,20 @@ limitations under the License.
 package gce
 
 import (
+	"time"
+
 	"k8s.io/kubernetes/pkg/api/v1"
 	netsets "k8s.io/kubernetes/pkg/util/net/sets"
 
 	compute "google.golang.org/api/compute/v1"
 )
 
-// Firewall management: These methods are just passthrough to the existing
-// internal firewall creation methods used to manage TCPLoadBalancer.
+func newFirewallMetricContext(request string, region string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"firewall_" + request, region, unusedMetricLabel},
+	}
+}
 
 // GetFirewall returns the Firewall by name.
 func (gce *GCECloud) GetFirewall(name string) (*compute.Firewall, error) {
@@ -37,22 +43,29 @@ func (gce *GCECloud) CreateFirewall(name, desc string, sourceRanges netsets.IPNe
 	if err != nil {
 		return err
 	}
-	// TODO: This completely breaks modularity in the cloudprovider but the methods
-	// shared with the TCPLoadBalancer take v1.ServicePorts.
+
+	mc := newFirewallMetricContext("create", region)
+
+	// TODO: This completely breaks modularity in the cloudprovider but
+	// the methods shared with the TCPLoadBalancer take v1.ServicePorts.
 	svcPorts := []v1.ServicePort{}
 	// TODO: Currently the only consumer of this method is the GCE L7
-	// loadbalancer controller, which never needs a protocol other than TCP.
-	// We should pipe through a mapping of port:protocol and default to TCP
-	// if UDP ports are required. This means the method signature will change
-	// forcing downstream clients to refactor interfaces.
+	// loadbalancer controller, which never needs a protocol other than
+	// TCP.  We should pipe through a mapping of port:protocol and
+	// default to TCP if UDP ports are required. This means the method
+	// signature will change forcing downstream clients to refactor
+	// interfaces.
 	for _, p := range ports {
 		svcPorts = append(svcPorts, v1.ServicePort{Port: int32(p), Protocol: v1.ProtocolTCP})
 	}
+
 	hosts, err := gce.getInstancesByNames(hostNames)
 	if err != nil {
+		mc.Observe(err)
 		return err
 	}
-	return gce.createFirewall(name, region, desc, sourceRanges, svcPorts, hosts)
+
+	return mc.Observe(gce.createFirewall(name, region, desc, sourceRanges, svcPorts, hosts))
 }
 
 // DeleteFirewall deletes the given firewall rule.
@@ -61,30 +74,41 @@ func (gce *GCECloud) DeleteFirewall(name string) error {
 	if err != nil {
 		return err
 	}
-	return gce.deleteFirewall(name, region)
+
+	mc := newFirewallMetricContext("delete", region)
+
+	return mc.Observe(gce.deleteFirewall(name, region))
 }
 
-// UpdateFirewall applies the given firewall rule as an update to an existing
-// firewall rule with the same name.
+// UpdateFirewall applies the given firewall rule as an update to an
+// existing firewall rule with the same name.
 func (gce *GCECloud) UpdateFirewall(name, desc string, sourceRanges netsets.IPNet, ports []int64, hostNames []string) error {
+
 	region, err := GetGCERegion(gce.localZone)
 	if err != nil {
 		return err
 	}
-	// TODO: This completely breaks modularity in the cloudprovider but the methods
-	// shared with the TCPLoadBalancer take v1.ServicePorts.
+
+	mc := newFirewallMetricContext("update", region)
+
+	// TODO: This completely breaks modularity in the cloudprovider but
+	// the methods shared with the TCPLoadBalancer take v1.ServicePorts.
 	svcPorts := []v1.ServicePort{}
 	// TODO: Currently the only consumer of this method is the GCE L7
-	// loadbalancer controller, which never needs a protocol other than TCP.
-	// We should pipe through a mapping of port:protocol and default to TCP
-	// if UDP ports are required. This means the method signature will change,
-	// forcing downstream clients to refactor interfaces.
+	// loadbalancer controller, which never needs a protocol other than
+	// TCP.  We should pipe through a mapping of port:protocol and
+	// default to TCP if UDP ports are required. This means the method
+	// signature will change, forcing downstream clients to refactor
+	// interfaces.
 	for _, p := range ports {
 		svcPorts = append(svcPorts, v1.ServicePort{Port: int32(p), Protocol: v1.ProtocolTCP})
 	}
+
 	hosts, err := gce.getInstancesByNames(hostNames)
 	if err != nil {
+		mc.Observe(err)
 		return err
 	}
-	return gce.updateFirewall(name, region, desc, sourceRanges, svcPorts, hosts)
+
+	return mc.Observe(gce.updateFirewall(name, region, desc, sourceRanges, svcPorts, hosts))
 }

pkg/cloudprovider/providers/gce/gce_routes.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 	"path"
 	"strings"
+	"time"
 
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/kubernetes/pkg/cloudprovider"
@@ -28,6 +29,13 @@ import (
 	"github.com/golang/glog"
 	compute "google.golang.org/api/compute/v1"
 )
+
+func newRoutesMetricContext(request string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"routes_" + request, unusedMetricLabel, unusedMetricLabel},
+	}
+}
 
 func (gce *GCECloud) ListRoutes(clusterName string) ([]*cloudprovider.Route, error) {
 	var routes []*cloudprovider.Route
@@ -80,6 +88,8 @@ func (gce *GCECloud) CreateRoute(clusterName string, nameHint string, route *clo
 	if err != nil {
 		return err
 	}
+
+	mc := newRoutesMetricContext("create")
 	insertOp, err := gce.service.Routes.Insert(gce.projectID, &compute.Route{
 		Name:            routeName,
 		DestRange:       route.DestinationCIDR,
@@ -93,18 +103,19 @@ func (gce *GCECloud) CreateRoute(clusterName string, nameHint string, route *clo
 			glog.Info("Route %v already exists.")
 			return nil
 		} else {
-			return err
+			return mc.Observe(err)
 		}
 	}
-	return gce.waitForGlobalOp(insertOp)
+	return gce.waitForGlobalOp(insertOp, mc)
 }
 
 func (gce *GCECloud) DeleteRoute(clusterName string, route *cloudprovider.Route) error {
+	mc := newRoutesMetricContext("create")
 	deleteOp, err := gce.service.Routes.Delete(gce.projectID, route.Name).Do()
 	if err != nil {
-		return err
+		return mc.Observe(err)
 	}
-	return gce.waitForGlobalOp(deleteOp)
+	return gce.waitForGlobalOp(deleteOp, mc)
 }
 
 func truncateClusterName(clusterName string) string {

pkg/cloudprovider/providers/gce/gce_urlmap.go
@@ -18,11 +18,17 @@ package gce
 
 import (
 	"net/http"
+	"time"
 
 	compute "google.golang.org/api/compute/v1"
 )
 
-// UrlMap management
+func newUrlMapMetricContext(request string) *metricContext {
+	return &metricContext{
+		start:      time.Now(),
+		attributes: []string{"urlmap_" + request, unusedMetricLabel, unusedMetricLabel},
+	}
+}
 
 // GetUrlMap returns the UrlMap by name.
 func (gce *GCECloud) GetUrlMap(name string) (*compute.UrlMap, error) {
@@ -35,11 +41,12 @@ func (gce *GCECloud) CreateUrlMap(backend *compute.BackendService, name string) 
 		Name:           name,
 		DefaultService: backend.SelfLink,
 	}
+	mc := newUrlMapMetricContext("create")
 	op, err := gce.service.UrlMaps.Insert(gce.projectID, urlMap).Do()
 	if err != nil {
-		return nil, err
+		return nil, mc.Observe(err)
 	}
-	if err = gce.waitForGlobalOp(op); err != nil {
+	if err = gce.waitForGlobalOp(op, mc); err != nil {
 		return nil, err
 	}
 	return gce.GetUrlMap(name)
@@ -47,11 +54,12 @@ func (gce *GCECloud) CreateUrlMap(backend *compute.BackendService, name string) 
 
 // UpdateUrlMap applies the given UrlMap as an update, and returns the new UrlMap.
 func (gce *GCECloud) UpdateUrlMap(urlMap *compute.UrlMap) (*compute.UrlMap, error) {
+	mc := newUrlMapMetricContext("update")
 	op, err := gce.service.UrlMaps.Update(gce.projectID, urlMap.Name, urlMap).Do()
 	if err != nil {
-		return nil, err
+		return nil, mc.Observe(err)
 	}
-	if err = gce.waitForGlobalOp(op); err != nil {
+	if err = gce.waitForGlobalOp(op, mc); err != nil {
 		return nil, err
 	}
 	return gce.service.UrlMaps.Get(gce.projectID, urlMap.Name).Do()
@@ -59,14 +67,15 @@ func (gce *GCECloud) UpdateUrlMap(urlMap *compute.UrlMap) (*compute.UrlMap, erro
 
 // DeleteUrlMap deletes a url map by name.
 func (gce *GCECloud) DeleteUrlMap(name string) error {
+	mc := newUrlMapMetricContext("delete")
 	op, err := gce.service.UrlMaps.Delete(gce.projectID, name).Do()
 	if err != nil {
 		if isHTTPErrorCode(err, http.StatusNotFound) {
 			return nil
 		}
-		return err
+		return mc.Observe(err)
 	}
-	return gce.waitForGlobalOp(op)
+	return gce.waitForGlobalOp(op, mc)
 }
 
 // ListUrlMaps lists all UrlMaps in the project.