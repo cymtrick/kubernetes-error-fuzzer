kubernetes/kubernetes

kubelet/envvars: Adding brackets to IPv6 addresses
Signed-off-by: André Martins <aanm90@gmail.com>

pkg/kubelet/envvars/envvars_test.go
@@ -79,6 +79,17 @@ func TestFromServices(t *testing.T) {
 				},
 			},
 		},
+		{
+			ObjectMeta: metav1.ObjectMeta{Name: "super-ipv6"},
+			Spec: v1.ServiceSpec{
+				Selector:  map[string]string{"bar": "baz"},
+				ClusterIP: "2001:DB8::",
+				Ports: []v1.ServicePort{
+					{Name: "u-d-p", Port: 8081, Protocol: "UDP"},
+					{Name: "t-c-p", Port: 8081, Protocol: "TCP"},
+				},
+			},
+		},
 	}
 	vars := envvars.FromServices(sl)
 	expected := []v1.EnvVar{
@@ -114,6 +125,19 @@ func TestFromServices(t *testing.T) {
 		{Name: "Q_U_U_X_PORT_8083_TCP_PROTO", Value: "tcp"},
 		{Name: "Q_U_U_X_PORT_8083_TCP_PORT", Value: "8083"},
 		{Name: "Q_U_U_X_PORT_8083_TCP_ADDR", Value: "9.8.7.6"},
+		{Name: "SUPER_IPV6_SERVICE_HOST", Value: "2001:DB8::"},
+		{Name: "SUPER_IPV6_SERVICE_PORT", Value: "8081"},
+		{Name: "SUPER_IPV6_SERVICE_PORT_U_D_P", Value: "8081"},
+		{Name: "SUPER_IPV6_SERVICE_PORT_T_C_P", Value: "8081"},
+		{Name: "SUPER_IPV6_PORT", Value: "udp://[2001:DB8::]:8081"},
+		{Name: "SUPER_IPV6_PORT_8081_UDP", Value: "udp://[2001:DB8::]:8081"},
+		{Name: "SUPER_IPV6_PORT_8081_UDP_PROTO", Value: "udp"},
+		{Name: "SUPER_IPV6_PORT_8081_UDP_PORT", Value: "8081"},
+		{Name: "SUPER_IPV6_PORT_8081_UDP_ADDR", Value: "2001:DB8::"},
+		{Name: "SUPER_IPV6_PORT_8081_TCP", Value: "tcp://[2001:DB8::]:8081"},
+		{Name: "SUPER_IPV6_PORT_8081_TCP_PROTO", Value: "tcp"},
+		{Name: "SUPER_IPV6_PORT_8081_TCP_PORT", Value: "8081"},
+		{Name: "SUPER_IPV6_PORT_8081_TCP_ADDR", Value: "2001:DB8::"},
 	}
 	if len(vars) != len(expected) {
 		t.Errorf("Expected %d env vars, got: %+v", len(expected), vars)

cymtrick/kubernetes-error-fuzzer

pkg/kubelet/envvars/envvars_test.go
@@ -79,6 +79,17 @@ func TestFromServices(t *testing.T) {
 				},
 			},
 		},
+		{
+			ObjectMeta: metav1.ObjectMeta{Name: "super-ipv6"},
+			Spec: v1.ServiceSpec{
+				Selector:  map[string]string{"bar": "baz"},
+				ClusterIP: "2001:DB8::",
+				Ports: []v1.ServicePort{
+					{Name: "u-d-p", Port: 8084, Protocol: "UDP"},
+					{Name: "t-c-p", Port: 8084, Protocol: "TCP"},
+				},
+			},
+		},
 	}
 	vars := envvars.FromServices(sl)
 	expected := []v1.EnvVar{
@@ -114,6 +125,19 @@ func TestFromServices(t *testing.T) {
 		{Name: "Q_U_U_X_PORT_8083_TCP_PROTO", Value: "tcp"},
 		{Name: "Q_U_U_X_PORT_8083_TCP_PORT", Value: "8083"},
 		{Name: "Q_U_U_X_PORT_8083_TCP_ADDR", Value: "9.8.7.6"},
+		{Name: "SUPER_IPV6_SERVICE_HOST", Value: "2001:DB8::"},
+		{Name: "SUPER_IPV6_SERVICE_PORT", Value: "8084"},
+		{Name: "SUPER_IPV6_SERVICE_PORT_U_D_P", Value: "8084"},
+		{Name: "SUPER_IPV6_SERVICE_PORT_T_C_P", Value: "8084"},
+		{Name: "SUPER_IPV6_PORT", Value: "udp://[2001:DB8::]:8084"},
+		{Name: "SUPER_IPV6_PORT_8084_UDP", Value: "udp://[2001:DB8::]:8084"},
+		{Name: "SUPER_IPV6_PORT_8084_UDP_PROTO", Value: "udp"},
+		{Name: "SUPER_IPV6_PORT_8084_UDP_PORT", Value: "8084"},
+		{Name: "SUPER_IPV6_PORT_8084_UDP_ADDR", Value: "2001:DB8::"},
+		{Name: "SUPER_IPV6_PORT_8084_TCP", Value: "tcp://[2001:DB8::]:8084"},
+		{Name: "SUPER_IPV6_PORT_8084_TCP_PROTO", Value: "tcp"},
+		{Name: "SUPER_IPV6_PORT_8084_TCP_PORT", Value: "8084"},
+		{Name: "SUPER_IPV6_PORT_8084_TCP_ADDR", Value: "2001:DB8::"},
 	}
 	if len(vars) != len(expected) {
 		t.Errorf("Expected %d env vars, got: %+v", len(expected), vars)