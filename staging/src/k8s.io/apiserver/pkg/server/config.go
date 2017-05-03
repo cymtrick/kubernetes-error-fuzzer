kubernetes/kubernetes

use our own serve mux that directs how we want

staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -479,7 +479,14 @@ func (c completedConfig) buildHandlers(s *GenericAPIServer, delegate http.Handle
 		}
 	}
 
-	installAPI(s, c.Config, delegate)
+	installAPI(s, c.Config)
+	if delegate != nil {
+		s.FallThroughHandler.NotFoundHandler(delegate)
+	} else if c.EnableIndex {
+		s.FallThroughHandler.NotFoundHandler(routes.NotFoundIndex{
+			PathProvider: s.listedPathProvider,
+		})
+	}
 
 	s.Handler = c.BuildHandlerChainFunc(s.HandlerContainer.ServeMux, c.Config)
 
@@ -500,15 +507,9 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	return handler
 }
 
-func installAPI(s *GenericAPIServer, c *Config, delegate http.Handler) {
-	switch {
-	case c.EnableIndex:
-		routes.Index{}.Install(s.listedPathProvider, c.FallThroughHandler, delegate)
-
-	case delegate != nil:
-		// if we have a delegate, allow it to handle everything that's unmatched even if
-		// the index is disabled.
-		s.FallThroughHandler.UnlistedHandleFunc("/", delegate.ServeHTTP)
+func installAPI(s *GenericAPIServer, c *Config) {
+	if c.EnableIndex {
+		routes.Index{}.Install(s.listedPathProvider, c.FallThroughHandler)
 	}
 	if c.SwaggerConfig != nil && c.EnableSwaggerUI {
 		routes.SwaggerUI{}.Install(s.FallThroughHandler)

staging/src/k8s.io/apiserver/pkg/server/routes/index.go
@@ -50,20 +50,21 @@ func (p ListedPathProviders) ListedPaths() []string {
 type Index struct{}
 
 // Install adds the Index webservice to the given mux.
-func (i Index) Install(pathProvider ListedPathProvider, mux *mux.PathRecorderMux, delegate http.Handler) {
-	mux.UnlistedHandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		status := http.StatusOK
-		if r.URL.Path != "/" && r.URL.Path != "/index.html" {
-			// Since "/" matches all paths, handleIndex is called for all paths for which there is no handler api.Registry.
-			// if we have a delegate, we should call to it and simply return
-			if delegate != nil {
-				delegate.ServeHTTP(w, r)
-				return
-			}
-
-			// If we have no delegate, we want to return a 404 status with a list of all valid paths, incase of an invalid URL request.
-			status = http.StatusNotFound
-		}
-		responsewriters.WriteRawJSON(status, metav1.RootPaths{Paths: pathProvider.ListedPaths()}, w)
+func (i Index) Install(pathProvider ListedPathProvider, mux *mux.PathRecorderMux) {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		responsewriters.WriteRawJSON(http.StatusOK, metav1.RootPaths{Paths: pathProvider.ListedPaths()}, w)
 	})
+
+	mux.UnlistedHandle("/", fn)
+	mux.UnlistedHandle("/index.html", fn)
+}
+
+// NotFoundIndex is like `Index`, but intended for use as a `NotFoundHandler`, so it returns a 404 status code.
+type NotFoundIndex struct {
+	PathProvider ListedPathProvider
+}
+
+// Install adds the Index webservice to the given mux.
+func (i NotFoundIndex) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	responsewriters.WriteRawJSON(http.StatusNotFound, metav1.RootPaths{Paths: i.PathProvider.ListedPaths()}, w)
 }

cymtrick/kubernetes-error-fuzzer

staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -479,7 +479,15 @@ func (c completedConfig) buildHandlers(s *GenericAPIServer, delegate http.Handle
 		}
 	}
 
-	installAPI(s, c.Config, delegate)
+	installAPI(s, c.Config)
+	if delegate != nil {
+		s.FallThroughHandler.NotFoundHandler(delegate)
+	} else if c.EnableIndex {
+		s.FallThroughHandler.NotFoundHandler(routes.IndexLister{
+			StatusCode:   http.StatusNotFound,
+			PathProvider: s.listedPathProvider,
+		})
+	}
 
 	s.Handler = c.BuildHandlerChainFunc(s.HandlerContainer.ServeMux, c.Config)
 
@@ -500,15 +508,9 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	return handler
 }
 
-func installAPI(s *GenericAPIServer, c *Config, delegate http.Handler) {
-	switch {
-	case c.EnableIndex:
-		routes.Index{}.Install(s.listedPathProvider, c.FallThroughHandler, delegate)
-
-	case delegate != nil:
-		// if we have a delegate, allow it to handle everything that's unmatched even if
-		// the index is disabled.
-		s.FallThroughHandler.UnlistedHandleFunc("/", delegate.ServeHTTP)
+func installAPI(s *GenericAPIServer, c *Config) {
+	if c.EnableIndex {
+		routes.Index{}.Install(s.listedPathProvider, c.FallThroughHandler)
 	}
 	if c.SwaggerConfig != nil && c.EnableSwaggerUI {
 		routes.SwaggerUI{}.Install(s.FallThroughHandler)

staging/src/k8s.io/apiserver/pkg/server/routes/index.go
@@ -50,20 +50,20 @@ func (p ListedPathProviders) ListedPaths() []string {
 type Index struct{}
 
 // Install adds the Index webservice to the given mux.
-func (i Index) Install(pathProvider ListedPathProvider, mux *mux.PathRecorderMux, delegate http.Handler) {
-	mux.UnlistedHandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		status := http.StatusOK
-		if r.URL.Path != "/" && r.URL.Path != "/index.html" {
-			// Since "/" matches all paths, handleIndex is called for all paths for which there is no handler api.Registry.
-			// if we have a delegate, we should call to it and simply return
-			if delegate != nil {
-				delegate.ServeHTTP(w, r)
-				return
-			}
+func (i Index) Install(pathProvider ListedPathProvider, mux *mux.PathRecorderMux) {
+	handler := IndexLister{StatusCode: http.StatusOK, PathProvider: pathProvider}
 
-			// If we have no delegate, we want to return a 404 status with a list of all valid paths, incase of an invalid URL request.
-			status = http.StatusNotFound
-		}
-		responsewriters.WriteRawJSON(status, metav1.RootPaths{Paths: pathProvider.ListedPaths()}, w)
-	})
+	mux.UnlistedHandle("/", handler)
+	mux.UnlistedHandle("/index.html", handler)
+}
+
+// IndexLister lists the available indexes with the status code provided
+type IndexLister struct {
+	StatusCode   int
+	PathProvider ListedPathProvider
+}
+
+// ServeHTTP serves the available paths.
+func (i IndexLister) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	responsewriters.WriteRawJSON(i.StatusCode, metav1.RootPaths{Paths: i.PathProvider.ListedPaths()}, w)
 }