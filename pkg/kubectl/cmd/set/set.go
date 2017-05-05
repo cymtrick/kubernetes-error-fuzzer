kubernetes/kubernetes

add set rolebinding/clusterrolebinding command

pkg/kubectl/cmd/set/set.go
@@ -44,6 +44,7 @@ func NewCmdSet(f cmdutil.Factory, out, err io.Writer) *cobra.Command {
 	cmd.AddCommand(NewCmdImage(f, out, err))
 	cmd.AddCommand(NewCmdResources(f, out, err))
 	cmd.AddCommand(NewCmdSelector(f, out))
+	cmd.AddCommand(NewCmdSubjects(f, out, err))
 
 	return cmd
 }

cymtrick/kubernetes-error-fuzzer

pkg/kubectl/cmd/set/set.go
@@ -44,6 +44,7 @@ func NewCmdSet(f cmdutil.Factory, out, err io.Writer) *cobra.Command {
 	cmd.AddCommand(NewCmdImage(f, out, err))
 	cmd.AddCommand(NewCmdResources(f, out, err))
 	cmd.AddCommand(NewCmdSelector(f, out))
+	cmd.AddCommand(NewCmdSubject(f, out, err))
 
 	return cmd
 }