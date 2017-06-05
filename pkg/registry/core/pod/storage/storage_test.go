kubernetes/kubernetes

Add group alias names to discovery in registry

pkg/registry/core/pod/storage/storage_test.go
@@ -803,3 +803,11 @@ func TestShortNames(t *testing.T) {
 	expected := []string{"po"}
 	registrytest.AssertShortNames(t, storage, expected)
 }
+
+func TestGroupNames(t *testing.T) {
+	storage, _, _, server := newStorage(t)
+	defer server.Terminate(t)
+	defer storage.Store.DestroyFunc()
+	expected := []string{"all"}
+	registrytest.AssertGroupNames(t, storage, expected)
+}

pkg/registry/core/replicationcontroller/storage/storage_test.go
@@ -335,3 +335,11 @@ func TestShortNames(t *testing.T) {
 	expected := []string{"rc"}
 	registrytest.AssertShortNames(t, storage.Controller, expected)
 }
+
+func TestGroupNames(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	defer storage.Controller.Store.DestroyFunc()
+	expected := []string{"all"}
+	registrytest.AssertGroupNames(t, storage.Controller, expected)
+}

cymtrick/kubernetes-error-fuzzer

pkg/registry/core/pod/storage/storage_test.go
@@ -888,3 +888,11 @@ func TestShortNames(t *testing.T) {
 	expected := []string{"po"}
 	registrytest.AssertShortNames(t, storage, expected)
 }
+
+func TestCategories(t *testing.T) {
+	storage, _, _, server := newStorage(t)
+	defer server.Terminate(t)
+	defer storage.Store.DestroyFunc()
+	expected := []string{"all"}
+	registrytest.AssertCategories(t, storage, expected)
+}

pkg/registry/core/replicationcontroller/storage/storage_test.go
@@ -335,3 +335,11 @@ func TestShortNames(t *testing.T) {
 	expected := []string{"rc"}
 	registrytest.AssertShortNames(t, storage.Controller, expected)
 }
+
+func TestCategories(t *testing.T) {
+	storage, server := newStorage(t)
+	defer server.Terminate(t)
+	defer storage.Controller.Store.DestroyFunc()
+	expected := []string{"all"}
+	registrytest.AssertCategories(t, storage.Controller, expected)
+}