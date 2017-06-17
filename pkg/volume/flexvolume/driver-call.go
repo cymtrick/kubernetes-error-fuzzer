cosmoskitten/kubernetes

Remove controller node plugin driver dependency for non-attachable flex volume drivers (Ex: NFS).

pkg/volume/flexvolume/driver-call.go
@@ -57,6 +57,8 @@ const (
 	optionKeyPodUID       = "kubernetes.io/pod.uid"
 
 	optionKeyServiceAccountName = "kubernetes.io/serviceAccount.name"
+
+	attachCapability = "attach"
 )
 
 const (
@@ -198,6 +200,10 @@ type DriverStatus struct {
 	VolumeName string `json:"volumeName,omitempty"`
 	// Represents volume is attached on the node
 	Attached bool `json:"attached,omitempty"`
+	// Returns capabilites of the driver.
+	// By default we assume all the capabilities are supported.
+	// If the plugin does not support a capability, it can return false for that capability.
+	CapabilityMap map[string]bool
 }
 
 // isCmdNotSupportedErr checks if the error corresponds to command not supported by

pkg/volume/flexvolume/mounter-defaults.go
@@ -32,14 +32,12 @@ type mounterDefaults flexVolumeMounter
 func (f *mounterDefaults) SetUpAt(dir string, fsGroup *types.UnixGroupID) error {
 	glog.Warning(logPrefix(f.plugin), "using default SetUpAt to ", dir)
 
-	a, err := f.plugin.NewAttacher()
+	volumeName, err := f.plugin.GetVolumeName(f.spec)
 	if err != nil {
-		return fmt.Errorf("NewAttacher failed: %v", err)
+		return fmt.Errorf("GetVolumeName failed from GetDeviceMountPath: %s", err)
 	}
-	src, err := a.GetDeviceMountPath(f.spec)
-	if err != nil {
-		return fmt.Errorf("GetDeviceMountPath failed: %v", err)
-	}
+
+	src := makeGlobalMountPath(f.plugin.host, f.plugin.driverName, volumeName)
 
 	if err := doMount(f.mounter, src, dir, "auto", []string{"bind"}); err != nil {
 		return err

cymtrick/kubernetes-error-fuzzer

pkg/volume/flexvolume/driver-call.go
@@ -58,6 +58,8 @@ const (
 	optionKeyPodUID       = "kubernetes.io/pod.uid"
 
 	optionKeyServiceAccountName = "kubernetes.io/serviceAccount.name"
+
+	attachCapability = "attach"
 )
 
 const (
@@ -201,6 +203,10 @@ type DriverStatus struct {
 	VolumeName string `json:"volumeName,omitempty"`
 	// Represents volume is attached on the node
 	Attached bool `json:"attached,omitempty"`
+	// Returns capabilities of the driver.
+	// By default we assume all the capabilities are supported.
+	// If the plugin does not support a capability, it can return false for that capability.
+	Capabilities map[string]bool
 }
 
 // isCmdNotSupportedErr checks if the error corresponds to command not supported by

pkg/volume/flexvolume/mounter-defaults.go
@@ -17,8 +17,6 @@ limitations under the License.
 package flexvolume
 
 import (
-	"fmt"
-
 	"github.com/golang/glog"
 
 	"k8s.io/apimachinery/pkg/types"
@@ -32,13 +30,9 @@ type mounterDefaults flexVolumeMounter
 func (f *mounterDefaults) SetUpAt(dir string, fsGroup *types.UnixGroupID) error {
 	glog.Warning(logPrefix(f.plugin), "using default SetUpAt to ", dir)
 
-	a, err := f.plugin.NewAttacher()
+	src, err := f.plugin.getDeviceMountPath(f.spec)
 	if err != nil {
-		return fmt.Errorf("NewAttacher failed: %v", err)
-	}
-	src, err := a.GetDeviceMountPath(f.spec)
-	if err != nil {
-		return fmt.Errorf("GetDeviceMountPath failed: %v", err)
+		return err
 	}
 
 	if err := doMount(f.mounter, src, dir, "auto", []string{"bind"}); err != nil {