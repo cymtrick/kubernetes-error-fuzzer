cosmoskitten/kubernetes

Don't register the kubeletconfig group with the default Scheme

pkg/kubelet/kubeletconfig/checkpoint/configmap_test.go
@@ -26,8 +26,8 @@ import (
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig"
+	kubeletapiutil "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/util"
 	kubeletconfigv1alpha1 "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/v1alpha1"
 	utiltest "k8s.io/kubernetes/pkg/kubelet/kubeletconfig/util/test"
 )
@@ -82,11 +82,13 @@ func TestConfigMapCheckpointUID(t *testing.T) {
 }
 
 func TestConfigMapCheckpointParse(t *testing.T) {
+	kubeletScheme, kubeletCodecs, err := kubeletapiutil.NewSchemeAndCodecs()
+
 	// get the built-in default configuration
 	external := &kubeletconfigv1alpha1.KubeletConfiguration{}
-	api.Scheme.Default(external)
+	kubeletScheme.Default(external)
 	defaultConfig := &kubeletconfig.KubeletConfiguration{}
-	err := api.Scheme.Convert(external, defaultConfig, nil)
+	err = kubeletScheme.Convert(external, defaultConfig, nil)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
@@ -124,7 +126,7 @@ apiVersion: kubeletconfig/v1alpha1`}}, defaultConfig, ""},
 	}
 	for _, c := range cases {
 		cpt := &configMapCheckpoint{c.cm}
-		kc, err := cpt.Parse()
+		kc, err := cpt.Parse(kubeletCodecs)
 		if utiltest.SkipRest(t, c.desc, err, c.err) {
 			continue
 		}

test/e2e_node/util.go
@@ -36,6 +36,7 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig"
+	kubeletapiutil "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/util"
 	kubeletconfigv1alpha1 "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/v1alpha1"
 	stats "k8s.io/kubernetes/pkg/kubelet/apis/stats/v1alpha1"
 	kubeletmetrics "k8s.io/kubernetes/pkg/kubelet/metrics"
@@ -261,7 +262,7 @@ func makeKubeletConfigMap(internalKC *kubeletconfig.KubeletConfiguration) *v1.Co
 	externalKC := &kubeletconfigv1alpha1.KubeletConfiguration{}
 	api.Scheme.Convert(internalKC, externalKC, nil)
 
-	encoder, err := newJSONEncoder(kubeletconfig.GroupName)
+	encoder, err := newKubeletConfigJSONEncoder()
 	framework.ExpectNoError(err)
 
 	data, err := runtime.Encode(encoder, externalKC)
@@ -309,19 +310,16 @@ func logKubeletMetrics(metricKeys ...string) {
 	}
 }
 
-func newJSONEncoder(groupName string) (runtime.Encoder, error) {
-	// encode to json
+func newKubeletConfigJSONEncoder() (runtime.Encoder, error) {
+	_, kubeletCodecs, err := kubeletapiutil.NewSchemeAndCodecs()
+	if err != nil {
+		return nil, err
+	}
+
 	mediaType := "application/json"
-	info, ok := runtime.SerializerInfoForMediaType(api.Codecs.SupportedMediaTypes(), mediaType)
+	info, ok := runtime.SerializerInfoForMediaType(kubeletCodecs.SupportedMediaTypes(), mediaType)
 	if !ok {
 		return nil, fmt.Errorf("unsupported media type %q", mediaType)
 	}
-
-	versions := api.Registry.EnabledVersionsForGroup(groupName)
-	if len(versions) == 0 {
-		return nil, fmt.Errorf("no enabled versions for group %q", groupName)
-	}
-
-	// the "best" version supposedly comes first in the list returned from api.Registry.EnabledVersionsForGroup
-	return api.Codecs.EncoderForVersion(info.Serializer, versions[0]), nil
+	return kubeletCodecs.EncoderForVersion(info.Serializer, kubeletconfigv1alpha1.SchemeGroupVersion), nil
 }

cymtrick/kubernetes-error-fuzzer

pkg/kubelet/kubeletconfig/checkpoint/configmap_test.go
@@ -26,8 +26,8 @@ import (
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig"
+	kubeletscheme "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/scheme"
 	kubeletconfigv1alpha1 "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/v1alpha1"
 	utiltest "k8s.io/kubernetes/pkg/kubelet/kubeletconfig/util/test"
 )
@@ -66,9 +66,15 @@ func TestNewConfigMapCheckpoint(t *testing.T) {
 }
 
 func TestConfigMapCheckpointUID(t *testing.T) {
+	_, kubeletCodecs, err := kubeletscheme.NewSchemeAndCodecs()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
 	cases := []string{"", "uid", "376dfb73-56db-11e7-a01e-42010a800002"}
 	for _, uidIn := range cases {
 		cpt := &configMapCheckpoint{
+			kubeletCodecs,
 			&apiv1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{UID: types.UID(uidIn)},
 			},
@@ -82,11 +88,16 @@ func TestConfigMapCheckpointUID(t *testing.T) {
 }
 
 func TestConfigMapCheckpointParse(t *testing.T) {
+	kubeletScheme, kubeletCodecs, err := kubeletscheme.NewSchemeAndCodecs()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
 	// get the built-in default configuration
 	external := &kubeletconfigv1alpha1.KubeletConfiguration{}
-	api.Scheme.Default(external)
+	kubeletScheme.Default(external)
 	defaultConfig := &kubeletconfig.KubeletConfiguration{}
-	err := api.Scheme.Convert(external, defaultConfig, nil)
+	err = kubeletScheme.Convert(external, defaultConfig, nil)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
@@ -123,7 +134,7 @@ apiVersion: kubeletconfig/v1alpha1`}}, defaultConfig, ""},
 			"kubelet": `{"kind":"KubeletConfiguration","apiVersion":"kubeletconfig/v1alpha1"}`}}, defaultConfig, ""},
 	}
 	for _, c := range cases {
-		cpt := &configMapCheckpoint{c.cm}
+		cpt := &configMapCheckpoint{kubeletCodecs, c.cm}
 		kc, err := cpt.Parse()
 		if utiltest.SkipRest(t, c.desc, err, c.err) {
 			continue
@@ -136,6 +147,11 @@ apiVersion: kubeletconfig/v1alpha1`}}, defaultConfig, ""},
 }
 
 func TestConfigMapCheckpointEncode(t *testing.T) {
+	_, kubeletCodecs, err := kubeletscheme.NewSchemeAndCodecs()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
 	// only one case, based on output from the existing encoder, and since
 	// this is hard to test (key order isn't guaranteed), we should probably
 	// just stick to this test case and mostly rely on the round-trip test.
@@ -146,7 +162,7 @@ func TestConfigMapCheckpointEncode(t *testing.T) {
 	}{
 		// we expect Checkpoints to be encoded as a json representation of the underlying API object
 		{"one-key",
-			&configMapCheckpoint{&apiv1.ConfigMap{
+			&configMapCheckpoint{kubeletCodecs, &apiv1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{Name: "one-key"},
 				Data:       map[string]string{"one": ""}}},
 			`{"kind":"ConfigMap","apiVersion":"v1","metadata":{"name":"one-key","creationTimestamp":null},"data":{"one":""}}
@@ -166,6 +182,11 @@ func TestConfigMapCheckpointEncode(t *testing.T) {
 }
 
 func TestConfigMapCheckpointRoundTrip(t *testing.T) {
+	_, kubeletCodecs, err := kubeletscheme.NewSchemeAndCodecs()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
 	cases := []struct {
 		desc      string
 		cpt       *configMapCheckpoint
@@ -173,7 +194,7 @@ func TestConfigMapCheckpointRoundTrip(t *testing.T) {
 	}{
 		// empty data
 		{"empty data",
-			&configMapCheckpoint{&apiv1.ConfigMap{
+			&configMapCheckpoint{kubeletCodecs, &apiv1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "empty-data-sha256-e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
 					UID:  "uid",
@@ -182,7 +203,7 @@ func TestConfigMapCheckpointRoundTrip(t *testing.T) {
 			""},
 		// two keys
 		{"two keys",
-			&configMapCheckpoint{&apiv1.ConfigMap{
+			&configMapCheckpoint{kubeletCodecs, &apiv1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "two-keys-sha256-2bff03d6249c8a9dc9a1436d087c124741361ccfac6615b81b67afcff5c42431",
 					UID:  "uid",
@@ -191,7 +212,7 @@ func TestConfigMapCheckpointRoundTrip(t *testing.T) {
 			""},
 		// missing uid
 		{"missing uid",
-			&configMapCheckpoint{&apiv1.ConfigMap{
+			&configMapCheckpoint{kubeletCodecs, &apiv1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "two-keys-sha256-2bff03d6249c8a9dc9a1436d087c124741361ccfac6615b81b67afcff5c42431",
 					UID:  "",

test/e2e_node/util.go
@@ -37,6 +37,7 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig"
+	kubeletscheme "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/scheme"
 	kubeletconfigv1alpha1 "k8s.io/kubernetes/pkg/kubelet/apis/kubeletconfig/v1alpha1"
 	stats "k8s.io/kubernetes/pkg/kubelet/apis/stats/v1alpha1"
 	kubeletmetrics "k8s.io/kubernetes/pkg/kubelet/metrics"
@@ -262,7 +263,7 @@ func makeKubeletConfigMap(internalKC *kubeletconfig.KubeletConfiguration) *v1.Co
 	externalKC := &kubeletconfigv1alpha1.KubeletConfiguration{}
 	api.Scheme.Convert(internalKC, externalKC, nil)
 
-	encoder, err := newJSONEncoder(kubeletconfig.GroupName)
+	encoder, err := newKubeletConfigJSONEncoder()
 	framework.ExpectNoError(err)
 
 	data, err := runtime.Encode(encoder, externalKC)
@@ -310,21 +311,18 @@ func logKubeletMetrics(metricKeys ...string) {
 	}
 }
 
-func newJSONEncoder(groupName string) (runtime.Encoder, error) {
-	// encode to json
+func newKubeletConfigJSONEncoder() (runtime.Encoder, error) {
+	_, kubeletCodecs, err := kubeletscheme.NewSchemeAndCodecs()
+	if err != nil {
+		return nil, err
+	}
+
 	mediaType := "application/json"
-	info, ok := runtime.SerializerInfoForMediaType(api.Codecs.SupportedMediaTypes(), mediaType)
+	info, ok := runtime.SerializerInfoForMediaType(kubeletCodecs.SupportedMediaTypes(), mediaType)
 	if !ok {
 		return nil, fmt.Errorf("unsupported media type %q", mediaType)
 	}
-
-	versions := api.Registry.EnabledVersionsForGroup(groupName)
-	if len(versions) == 0 {
-		return nil, fmt.Errorf("no enabled versions for group %q", groupName)
-	}
-
-	// the "best" version supposedly comes first in the list returned from api.Registry.EnabledVersionsForGroup
-	return api.Codecs.EncoderForVersion(info.Serializer, versions[0]), nil
+	return kubeletCodecs.EncoderForVersion(info.Serializer, kubeletconfigv1alpha1.SchemeGroupVersion), nil
 }
 
 // runCommand runs the cmd and returns the combined stdout and stderr, or an