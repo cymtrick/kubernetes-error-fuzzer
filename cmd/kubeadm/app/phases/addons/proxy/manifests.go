cosmoskitten/kubernetes

update related files

cmd/kubeadm/app/phases/addons/manifests.go
@@ -96,6 +96,7 @@ spec:
       - name: xtables-lock
         hostPath:
           path: /run/xtables.lock
+          type: File
 `
 
 	KubeDNSVersion = "1.14.4"

cymtrick/kubernetes-error-fuzzer

cmd/kubeadm/app/phases/addons/proxy/manifests.go
@@ -81,7 +81,6 @@ spec:
         volumeMounts:
         - mountPath: /var/lib/kube-proxy
           name: kube-proxy
-        # TODO: Make this a file hostpath mount
         - mountPath: /run/xtables.lock
           name: xtables-lock
           readOnly: false
@@ -100,5 +99,6 @@ spec:
       - name: xtables-lock
         hostPath:
           path: /run/xtables.lock
+          type: FileOrCreate
 `
 )