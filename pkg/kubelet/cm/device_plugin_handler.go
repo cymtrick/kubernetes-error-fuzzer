cosmoskitten/kubernetes

Kubelet side extension to support device allocation

pkg/kubelet/cm/device_plugin_handler.go
@@ -18,38 +18,220 @@ package cm
 
 import (
 	"fmt"
+	"strings"
+	"sync"
 
 	"github.com/golang/glog"
 
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/util/sets"
 	pluginapi "k8s.io/kubernetes/pkg/kubelet/apis/deviceplugin/v1alpha1"
 	"k8s.io/kubernetes/pkg/kubelet/deviceplugin"
 )
 
+// podDevices represents a list of pod to Device mappings.
+type containerToDevice map[string]sets.String
+type podDevices struct {
+	podDeviceMapping map[string]containerToDevice
+}
+
+func newPodDevices() *podDevices {
+	return &podDevices{
+		podDeviceMapping: make(map[string]containerToDevice),
+	}
+}
+func (pdev *podDevices) pods() sets.String {
+	ret := sets.NewString()
+	for k := range pdev.podDeviceMapping {
+		ret.Insert(k)
+	}
+	return ret
+}
+
+func (pdev *podDevices) insert(podUID, contName string, device string) {
+	if _, exists := pdev.podDeviceMapping[podUID]; !exists {
+		pdev.podDeviceMapping[podUID] = make(containerToDevice)
+	}
+	if _, exists := pdev.podDeviceMapping[podUID][contName]; !exists {
+		pdev.podDeviceMapping[podUID][contName] = sets.NewString()
+	}
+	pdev.podDeviceMapping[podUID][contName].Insert(device)
+}
+
+func (pdev *podDevices) getDevices(podUID, contName string) sets.String {
+	containers, exists := pdev.podDeviceMapping[podUID]
+	if !exists {
+		return nil
+	}
+	devices, exists := containers[contName]
+	if !exists {
+		return nil
+	}
+	return devices
+}
+
+func (pdev *podDevices) delete(pods []string) {
+	for _, uid := range pods {
+		delete(pdev.podDeviceMapping, uid)
+	}
+}
+
+func (pdev *podDevices) devices() sets.String {
+	ret := sets.NewString()
+	for _, containerToDevice := range pdev.podDeviceMapping {
+		for _, deviceSet := range containerToDevice {
+			ret = ret.Union(deviceSet)
+		}
+	}
+	return ret
+}
+
+// updateCapacityCallback is called to update ContainerManager capacity when
+// device capacity changes.
+type updateCapacityCallback func(v1.ResourceList)
+
 type DevicePluginHandlerImpl struct {
-	devicePluginManager deviceplugin.Manager
+	sync.Mutex
+	devicePluginManager *deviceplugin.ManagerImpl
+	// allDevices and allocated are keyed by resource_name.
+	allDevices map[string]sets.String
+	allocated  map[string]*podDevices
+	updateCapacity updateCapacityCallback
 }
 
 // NewDevicePluginHandler create a DevicePluginHandler
-func NewDevicePluginHandler() (*DevicePluginHandlerImpl, error) {
+func NewDevicePluginHandler(enabled bool, updateCapacity updateCapacityCallback) (*DevicePluginHandlerImpl, error) {
+	if !enabled {
+		return &DevicePluginHandlerImpl{}, nil
+	}
 	glog.V(2).Infof("Starting Device Plugin Handler")
+	handler := &DevicePluginHandlerImpl{
+		allDevices:          make(map[string]sets.String),
+		allocated:           devicesInUse(),
+		updateCapacity:	     updateCapacity,
+	}
 
-	mgr, err := deviceplugin.NewManagerImpl(pluginapi.DevicePluginPath,
-		func(r string, a, u, d []*pluginapi.Device) {})
+	deviceManagerMonitorCallback := func(resourceName string, added, updated, deleted []*pluginapi.Device) {
+		var capacity = v1.ResourceList{}
+		kept := append(updated, added...)
+		for _, dev := range kept {
+			if dev.Health == pluginapi.Healthy {
+				handler.allDevices[resourceName].Insert(dev.ID)
+			} else {
+				handler.allDevices[resourceName].Delete(dev.ID)
+			}
+		}
+		for _, dev := range deleted {
+			handler.allDevices[resourceName].Delete(dev.ID)
+		}
+		capacity[v1.ResourceName(resourceName)] = *resource.NewQuantity(int64(handler.allDevices[resourceName].Len()), resource.DecimalSI)
+		updateCapacity(capacity)
+	}
+
+	mgr, err := deviceplugin.NewManagerImpl(pluginapi.KubeletSocket, deviceManagerMonitorCallback)
 
 	if err != nil {
 		return nil, fmt.Errorf("Failed to initialize device plugin: %+v", err)
 	}
 
-	if err := mgr.Start(); err != nil {
-		return nil, err
-	}
-
-	return &DevicePluginHandlerImpl{
-		devicePluginManager: mgr,
-	}, nil
+	handler.devicePluginManager = mgr
+	return handler, nil
 }
 
-// TODO cache this
+func (h *DevicePluginHandlerImpl) Start() error {
+	if h.devicePluginManager == nil {
+		return nil
+	}
+	return h.devicePluginManager.Start()
+}
+
 func (h *DevicePluginHandlerImpl) Devices() map[string][]*pluginapi.Device {
+	if h.devicePluginManager == nil {
+		return make(map[string][]*pluginapi.Device)
+	}
 	return h.devicePluginManager.Devices()
+}
+
+func (h *DevicePluginHandlerImpl) Allocate(pod *v1.Pod, container *v1.Container, activePods []*v1.Pod) ([]*pluginapi.AllocateResponse, error) {
+	var ret []*pluginapi.AllocateResponse
+	if h.devicePluginManager == nil {
+		return ret, nil
+	}
+	glog.V(2).Infof("Allocate called")
+	h.Lock()
+	defer h.Unlock()
+	h.updateAllDevices()
+	h.updateAllocatedDevices(activePods)
+	for k, v := range container.Resources.Limits {
+		key := string(k)
+		needed := int(v.Value())
+		glog.V(2).Infof("needs %d %s", needed, key)
+		if !deviceplugin.IsDeviceName(k) || needed == 0 {
+			continue
+		}
+		// Gets list of devices that have already been allocated.
+		// This can happen if a container restarts for example.
+		if h.allocated[key] == nil {
+			h.allocated[key] = newPodDevices()
+		}
+		devices := h.allocated[key].getDevices(string(pod.UID), container.Name)
+		if devices != nil {
+			glog.V(2).Infof("Found pre-allocated devices for container %q in Pod %q: %v", container.Name, pod.UID, devices.List())
+			needed = needed - devices.Len()
+		}
+		// Get Devices in use.
+		devicesInUse := h.allocated[key].devices()
+		glog.V(2).Infof("all devices: %v", h.allDevices[key].List())
+		glog.V(2).Infof("devices in use: %v", devicesInUse.List())
+		// Get a list of available devices.
+		available := h.allDevices[key].Difference(devicesInUse)
+		glog.V(2).Infof("devices available: %v", available.List())
+		if int(available.Len()) < needed {
+			return nil, fmt.Errorf("requested number of devices unavailable. Requested: %d, Available: %d", v, available.Len())
+		}
+		resp, err := h.devicePluginManager.Allocate(strings.TrimPrefix(key, v1.ResourceOpaqueIntPrefix),
+			append(devices.UnsortedList(), available.UnsortedList()[:needed]...))
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, resp)
+	}
+	return ret, nil
+}
+
+// devicesInUse returns a list of custom devices in use along with the
+// respective pods that are using them.
+func devicesInUse() map[string]*podDevices {
+	// TODO: gets the initial state from checkpointing.
+	return make(map[string]*podDevices)
+}
+// updateAllDevices gets all healthy devices.
+// TODO: consider how to make this more efficient.
+func (h *DevicePluginHandlerImpl) updateAllDevices() {
+	h.allDevices = make(map[string]sets.String)
+	for k, devs := range h.devicePluginManager.Devices() {
+		key := v1.ResourceOpaqueIntPrefix + k
+		h.allDevices[key] = sets.NewString()
+		for _, dev := range devs {
+			glog.V(2).Infof("insert device %s for resource %s", dev.ID, key)
+			h.allDevices[key].Insert(dev.ID)
+		}
+	}
+}
+
+// updateAllocatedGPUs updates the list of GPUs in use.
+// It gets a list of active pods and then frees any GPUs that are bound to
+// terminated pods. Returns error on failure.
+func (h *DevicePluginHandlerImpl) updateAllocatedDevices(activePods []*v1.Pod) {
+	activePodUids := sets.NewString()
+	for _, pod := range activePods {
+		activePodUids.Insert(string(pod.UID))
+	}
+	for _, v := range h.allocated {
+		allocatedPodUids := v.pods()
+		podsToBeRemoved := allocatedPodUids.Difference(activePodUids)
+		glog.V(5).Infof("pods to be removed: %v", podsToBeRemoved.List())
+		v.delete(podsToBeRemoved.List())
+	}
 }

cymtrick/kubernetes-error-fuzzer

pkg/kubelet/cm/device_plugin_handler.go
@@ -18,38 +18,214 @@ package cm
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/golang/glog"
 
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/util/sets"
 	pluginapi "k8s.io/kubernetes/pkg/kubelet/apis/deviceplugin/v1alpha1"
 	"k8s.io/kubernetes/pkg/kubelet/deviceplugin"
 )
 
+// podDevices represents a list of pod to device Id mappings.
+type containerDevices map[string]sets.String
+type podDevices map[string]containerDevices
+
+func (pdev podDevices) pods() sets.String {
+	ret := sets.NewString()
+	for k := range pdev {
+		ret.Insert(k)
+	}
+	return ret
+}
+
+func (pdev podDevices) insert(podUID, contName string, device string) {
+	if _, exists := pdev[podUID]; !exists {
+		pdev[podUID] = make(containerDevices)
+	}
+	if _, exists := pdev[podUID][contName]; !exists {
+		pdev[podUID][contName] = sets.NewString()
+	}
+	pdev[podUID][contName].Insert(device)
+}
+
+func (pdev podDevices) getDevices(podUID, contName string) sets.String {
+	containers, exists := pdev[podUID]
+	if !exists {
+		return nil
+	}
+	devices, exists := containers[contName]
+	if !exists {
+		return nil
+	}
+	return devices
+}
+
+func (pdev podDevices) delete(pods []string) {
+	for _, uid := range pods {
+		delete(pdev, uid)
+	}
+}
+
+func (pdev podDevices) devices() sets.String {
+	ret := sets.NewString()
+	for _, containerDevices := range pdev {
+		for _, deviceSet := range containerDevices {
+			ret = ret.Union(deviceSet)
+		}
+	}
+	return ret
+}
+
+type DevicePluginHandler interface {
+	// Start starts device plugin registration service.
+	Start() error
+	// Devices returns all of registered devices keyed by resourceName.
+	Devices() map[string][]*pluginapi.Device
+	// Allocate attempts to allocate all of required extended resources for
+	// the input container, issues an Allocate rpc request for each of such
+	// resources, and returns their AllocateResponses on success.
+	Allocate(pod *v1.Pod, container *v1.Container, activePods []*v1.Pod) ([]*pluginapi.AllocateResponse, error)
+}
+
 type DevicePluginHandlerImpl struct {
+	sync.Mutex
 	devicePluginManager deviceplugin.Manager
+	// devicePluginManagerMonitorCallback is used for testing only.
+	devicePluginManagerMonitorCallback deviceplugin.MonitorCallback
+	// allDevices contains all of registered resourceNames and their exported device IDs.
+	allDevices map[string]sets.String
+	// allocatedDevices contains pod to allocated device mapping, keyed by resourceName.
+	allocatedDevices map[string]podDevices
 }
 
 // NewDevicePluginHandler create a DevicePluginHandler
-func NewDevicePluginHandler() (*DevicePluginHandlerImpl, error) {
-	glog.V(2).Infof("Starting Device Plugin Handler")
+// updateCapacityFunc is called to update ContainerManager capacity when
+// device capacity changes.
+func NewDevicePluginHandlerImpl(updateCapacityFunc func(v1.ResourceList)) (*DevicePluginHandlerImpl, error) {
+	glog.V(2).Infof("Creating Device Plugin Handler")
+	handler := &DevicePluginHandlerImpl{
+		allDevices:       make(map[string]sets.String),
+		allocatedDevices: devicesInUse(),
+	}
 
-	mgr, err := deviceplugin.NewManagerImpl(pluginapi.DevicePluginPath,
-		func(r string, a, u, d []*pluginapi.Device) {})
+	deviceManagerMonitorCallback := func(resourceName string, added, updated, deleted []*pluginapi.Device) {
+		var capacity = v1.ResourceList{}
+		kept := append(updated, added...)
+		if _, ok := handler.allDevices[resourceName]; !ok {
+			handler.allDevices[resourceName] = sets.NewString()
+		}
+		// For now, DevicePluginHandler only keeps track of healthy devices.
+		// We can revisit this later when the need comes to track unhealthy devices here.
+		for _, dev := range kept {
+			if dev.Health == pluginapi.Healthy {
+				handler.allDevices[resourceName].Insert(dev.ID)
+			} else {
+				handler.allDevices[resourceName].Delete(dev.ID)
+			}
+		}
+		for _, dev := range deleted {
+			handler.allDevices[resourceName].Delete(dev.ID)
+		}
+		capacity[v1.ResourceName(resourceName)] = *resource.NewQuantity(int64(handler.allDevices[resourceName].Len()), resource.DecimalSI)
+		updateCapacityFunc(capacity)
+	}
 
+	mgr, err := deviceplugin.NewManagerImpl(pluginapi.KubeletSocket, deviceManagerMonitorCallback)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to initialize device plugin: %+v", err)
+		return nil, fmt.Errorf("Failed to initialize device plugin manager: %+v", err)
 	}
 
-	if err := mgr.Start(); err != nil {
-		return nil, err
-	}
-
-	return &DevicePluginHandlerImpl{
-		devicePluginManager: mgr,
-	}, nil
+	handler.devicePluginManager = mgr
+	handler.devicePluginManagerMonitorCallback = deviceManagerMonitorCallback
+	return handler, nil
 }
 
-// TODO cache this
+func (h *DevicePluginHandlerImpl) Start() error {
+	return h.devicePluginManager.Start()
+}
+
 func (h *DevicePluginHandlerImpl) Devices() map[string][]*pluginapi.Device {
 	return h.devicePluginManager.Devices()
+}
+
+func (h *DevicePluginHandlerImpl) Allocate(pod *v1.Pod, container *v1.Container, activePods []*v1.Pod) ([]*pluginapi.AllocateResponse, error) {
+	var ret []*pluginapi.AllocateResponse
+	h.updateAllocatedDevices(activePods)
+	for k, v := range container.Resources.Limits {
+		resource := string(k)
+		needed := int(v.Value())
+		glog.V(3).Infof("needs %d %s", needed, resource)
+		if !deviceplugin.IsDeviceName(k) || needed == 0 {
+			continue
+		}
+		h.Lock()
+		// Gets list of devices that have already been allocated.
+		// This can happen if a container restarts for example.
+		if h.allocatedDevices[resource] == nil {
+			h.allocatedDevices[resource] = make(podDevices)
+		}
+		devices := h.allocatedDevices[resource].getDevices(string(pod.UID), container.Name)
+		if devices != nil {
+			glog.V(3).Infof("Found pre-allocated devices for resource %s container %q in Pod %q: %v", resource, container.Name, pod.UID, devices.List())
+			needed = needed - devices.Len()
+		}
+		// Get Devices in use.
+		devicesInUse := h.allocatedDevices[resource].devices()
+		// Get a list of available devices.
+		available := h.allDevices[resource].Difference(devicesInUse)
+		if int(available.Len()) < needed {
+			h.Unlock()
+			return nil, fmt.Errorf("requested number of devices unavailable for %s. Requested: %d, Available: %d", resource, needed, available.Len())
+		}
+		allocated := available.UnsortedList()[:needed]
+		for _, device := range allocated {
+			// Update internal allocated device cache.
+			h.allocatedDevices[resource].insert(string(pod.UID), container.Name, device)
+		}
+		h.Unlock()
+		// devicePluginManager.Allocate involves RPC calls to device plugin, which
+		// could be heavy-weight. Therefore we want to perform this operation outside
+		// mutex lock. Note if Allcate call fails, we may leave container resources
+		// partially allocated for the failed container. We rely on updateAllocatedDevices()
+		// to garbage collect these resources later. Another side effect is that if
+		// we have X resource A and Y resource B in total, and two containers, container1
+		// and container2 both require X resource A and Y resource B. Both allocation
+		// requests may fail if we serve them in mixed order.
+		// TODO: may revisit this part later if we see inefficient resource allocation
+		// in real use as the result of this.
+		resp, err := h.devicePluginManager.Allocate(resource, append(devices.UnsortedList(), allocated...))
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, resp)
+	}
+	return ret, nil
+}
+
+// devicesInUse returns a list of custom devices in use along with the
+// respective pods that are using them.
+func devicesInUse() map[string]podDevices {
+	// TODO: gets the initial state from checkpointing.
+	return make(map[string]podDevices)
+}
+
+// updateAllocatedDevices updates the list of GPUs in use.
+// It gets a list of active pods and then frees any GPUs that are bound to
+// terminated pods. Returns error on failure.
+func (h *DevicePluginHandlerImpl) updateAllocatedDevices(activePods []*v1.Pod) {
+	h.Lock()
+	defer h.Unlock()
+	activePodUids := sets.NewString()
+	for _, pod := range activePods {
+		activePodUids.Insert(string(pod.UID))
+	}
+	for _, podDevs := range h.allocatedDevices {
+		allocatedPodUids := podDevs.pods()
+		podsToBeRemoved := allocatedPodUids.Difference(activePodUids)
+		glog.V(5).Infof("pods to be removed: %v", podsToBeRemoved.List())
+		podDevs.delete(podsToBeRemoved.List())
+	}
 }