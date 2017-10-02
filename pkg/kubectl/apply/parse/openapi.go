cosmoskitten/kubernetes

Beginning of rewrite apply merge-logic using visitor pattern.
Major changes:
- Don't generate a patch, instead generate the merged object so it can be used with PUT
- Separate tree parsing logic to collate items in a list from the delete / merge / replace / add logic when merging
- Use openapi for merge strategy metadata so it works with extensions and version skew
- Support multi-field mergekeys when merging lists
- Support replace strategy for maps
- Reduce complexity of generating order when merging lists - keep the locally defined order and append remote only-items

Continue to support:
- Explicitly setting fields to null
- Merging lists of primitives
- Don't randomize ordering when merging lists

TODO:
- Retain keys
- Conflict detection

pkg/kubectl/apply/parse/openapi.go
@@ -1,0 +1,158 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package parse
+
+import (
+	"fmt"
+	"k8s.io/kubernetes/pkg/kubectl/cmd/util/openapi"
+)
+
+// Contains functions for casting openapi interfaces to their underlying types
+
+// GetSchemaType returns the string type of the schema - e.g. array, primitive, map, kind, reference
+func GetSchemaType(schema openapi.Schema) string {
+	if schema == nil {
+		return ""
+	}
+	visitor := &BaseSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Kind
+}
+
+// GetKind converts schema to an *openapi.Kind object
+func GetKind(schema openapi.Schema) (*openapi.Kind, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &KindSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+// GetArray converts schema to an *openapi.Array object
+func GetArray(schema openapi.Schema) (*openapi.Array, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &ArraySchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+// GetMap converts schema to an *openapi.Map object
+func GetMap(schema openapi.Schema) (*openapi.Map, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &MapSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+// GetPrimitive converts schema to an *openapi.Primitive object
+func GetPrimitive(schema openapi.Schema) (*openapi.Primitive, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &PrimitiveSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+type BaseSchemaVisitor struct {
+	Err  error
+	Kind string
+}
+
+func (v *BaseSchemaVisitor) VisitArray(array *openapi.Array) {
+	v.Kind = "array"
+	v.Err = fmt.Errorf("Array type not expected")
+}
+func (v *BaseSchemaVisitor) VisitMap(*openapi.Map) {
+	v.Kind = "map"
+	v.Err = fmt.Errorf("Map type not expected")
+}
+
+func (v *BaseSchemaVisitor) VisitPrimitive(*openapi.Primitive) {
+	v.Kind = "primitive"
+	v.Err = fmt.Errorf("Primitive type not expected")
+}
+func (v *BaseSchemaVisitor) VisitKind(*openapi.Kind) {
+	v.Kind = "kind"
+	v.Err = fmt.Errorf("Kind type not expected")
+}
+
+func (v *BaseSchemaVisitor) VisitReference(reference openapi.Reference) {
+	v.Kind = "reference"
+	v.Err = fmt.Errorf("Reference type not expected")
+}
+
+type KindSchemaVisitor struct {
+	BaseSchemaVisitor
+	Result *openapi.Kind
+}
+
+func (v *KindSchemaVisitor) VisitKind(result *openapi.Kind) {
+	v.Result = result
+	v.Kind = "kind"
+}
+
+func (v *KindSchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}
+
+type MapSchemaVisitor struct {
+	BaseSchemaVisitor
+	Result *openapi.Map
+}
+
+func (v *MapSchemaVisitor) VisitMap(result *openapi.Map) {
+	v.Result = result
+	v.Kind = "map"
+}
+
+func (v *MapSchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}
+
+type ArraySchemaVisitor struct {
+	BaseSchemaVisitor
+	Result *openapi.Array
+}
+
+func (v *ArraySchemaVisitor) VisitArray(result *openapi.Array) {
+	v.Result = result
+	v.Kind = "array"
+}
+
+func (v *ArraySchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}
+
+type PrimitiveSchemaVisitor struct {
+	BaseSchemaVisitor
+	Result *openapi.Primitive
+}
+
+func (v *PrimitiveSchemaVisitor) VisitPrimitive(result *openapi.Primitive) {
+	v.Result = result
+	v.Kind = "primitive"
+}
+
+func (v *PrimitiveSchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}

pkg/kubectl/apply/merge/merge_visitor.go
@@ -1,0 +1,149 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package merge
+
+import (
+	"fmt"
+
+	"k8s.io/kubernetes/pkg/kubectl/apply"
+)
+
+func createMergeVisitor(options Options, strategic *strategicVisitor) mergeVisitor {
+	return mergeVisitor{
+		strategic,
+		options,
+	}
+}
+
+// mergeVisitor creates a patch to merge a local file value into a remote field value
+type mergeVisitor struct {
+	strategic *strategicVisitor
+	options   Options
+}
+
+// VisitList creates a patch to merge a local list field value into a remote list field value
+func (v mergeVisitor) VisitList(e apply.ListElement) (apply.Result, error) {
+	// No merge logic if adding or deleting a field
+	if result, done := v.doAddOrDelete(e); done {
+		return result, nil
+	}
+
+	// Merge each item in the list and append it to the list
+	merged := []interface{}{}
+	for _, value := range e.Values {
+		// Recursively merge the list element before adding the value to the list
+		result, err := value.Accept(v.strategic)
+		if err != nil {
+			return apply.Result{}, err
+		}
+
+		switch result.Operation {
+		case apply.SET:
+			// Keep the list item value
+			merged = append(merged, result.MergedResult)
+		case apply.DROP:
+			// Drop the list item value
+		default:
+			panic(fmt.Errorf("Unexpected result operation type %+v", result))
+		}
+	}
+
+	if len(merged) == 0 {
+		// If the list is empty, return a nil entry
+		return apply.Result{Operation: apply.SET, MergedResult: nil}, nil
+	} else {
+		// Return the merged list, and tell the caller to keep it
+		return apply.Result{Operation: apply.SET, MergedResult: merged}, nil
+	}
+}
+
+// VisitMap creates a patch to merge a local map field into a remote map field
+func (v mergeVisitor) VisitMap(e apply.MapElement) (apply.Result, error) {
+	return v.doMergeMap(e)
+}
+
+// VisitType creates a patch to merge a local map field into a remote map field
+func (v mergeVisitor) VisitType(e apply.TypeElement) (apply.Result, error) {
+	return v.doMergeMap(e)
+}
+
+// do merges a recorded, local and remote map into a new object
+func (v mergeVisitor) doMergeMap(e apply.MapValuesElement) (apply.Result, error) {
+	// No merge logic if adding or deleting a field
+	if result, done := v.doAddOrDelete(e); done {
+		return result, nil
+	}
+
+	// Merge each item in the list
+	merged := map[string]interface{}{}
+	for key, value := range e.GetValues() {
+		// Recursively merge the map element before adding the value to the map
+		result, err := value.Accept(v.strategic)
+		if err != nil {
+			return apply.Result{}, err
+		}
+
+		switch result.Operation {
+		case apply.SET:
+			// Keep the map item value
+			merged[key] = result.MergedResult
+		case apply.DROP:
+			// Drop the map item value
+		default:
+			panic(fmt.Errorf("Unexpected result operation type %+v", result))
+		}
+	}
+
+	// Return the merged map, and tell the caller to keep it
+	if len(merged) == 0 {
+		// Special case the empty map to set the field value to nil, but keep the field key
+		// This is how the tests expect the structures to look when parsed from yaml
+		return apply.Result{Operation: apply.SET, MergedResult: nil}, nil
+	} else {
+		return apply.Result{Operation: apply.SET, MergedResult: merged}, nil
+	}
+}
+
+type MapIterable struct {
+	element apply.MapValuesElement
+	result  map[string]interface{}
+}
+
+func (v mergeVisitor) doAddOrDelete(e apply.Element) (apply.Result, bool) {
+	if apply.IsAdd(e) {
+		return apply.Result{Operation: apply.SET, MergedResult: e.GetLocal()}, true
+	}
+
+	// Delete the List
+	if apply.IsDrop(e) {
+		return apply.Result{Operation: apply.DROP}, true
+	}
+
+	return apply.Result{}, false
+}
+
+// VisitPrimitive returns and error.  Primitive elements can't be merged, only replaced.
+func (v mergeVisitor) VisitPrimitive(diff apply.PrimitiveElement) (apply.Result, error) {
+	return apply.Result{}, fmt.Errorf("Cannot merge primitive element %v", diff.Name)
+}
+
+// VisitEmpty
+func (v mergeVisitor) VisitEmpty(diff apply.EmptyElement) (apply.Result, error) {
+	return apply.Result{Operation: apply.SET}, nil
+}
+
+var _ apply.Visitor = &mergeVisitor{}

pkg/kubectl/apply/merge/strategic_visitor.go
@@ -1,0 +1,92 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package merge
+
+import "k8s.io/kubernetes/pkg/kubectl/apply"
+
+// strategicVisitor delegates merging fields to other visitor implementations
+// based on the merge strategy preferred by the field.
+type strategicVisitor struct {
+	options Options
+	merge   mergeVisitor
+	replace replaceVisitor
+}
+
+// createStrategicVisitor returns a new strategicVisitor
+func createStrategicVisitor(options Options) *strategicVisitor {
+	v := &strategicVisitor{
+		options: options,
+	}
+	v.replace = createReplaceVisitor(options, v)
+	v.merge = createMergeVisitor(options, v)
+	return v
+}
+
+// VisitList delegates visiting a list based on the field patch strategy.
+// Defaults to "replace"
+func (v strategicVisitor) VisitList(diff apply.ListElement) (apply.Result, error) {
+	// TODO: Support retainkeys
+	switch diff.GetFieldMergeType() {
+	case "merge":
+		return v.merge.VisitList(diff)
+	case "replace":
+		return v.replace.VisitList(diff)
+	default:
+		return v.replace.VisitList(diff)
+	}
+}
+
+// VisitMap delegates visiting a map based on the field patch strategy.
+// Defaults to "merge"
+func (v strategicVisitor) VisitMap(diff apply.MapElement) (apply.Result, error) {
+	// TODO: Support retainkeys
+	switch diff.GetFieldMergeType() {
+	case "merge":
+		return v.merge.VisitMap(diff)
+	case "replace":
+		return v.replace.VisitMap(diff)
+	default:
+		return v.merge.VisitMap(diff)
+	}
+}
+
+// VisitType delegates visiting a map based on the field patch strategy.
+// Defaults to "merge"
+func (v strategicVisitor) VisitType(diff apply.TypeElement) (apply.Result, error) {
+	// TODO: Support retainkeys
+	switch diff.GetFieldMergeType() {
+	case "merge":
+		return v.merge.VisitType(diff)
+	case "replace":
+		return v.replace.VisitType(diff)
+	default:
+		return v.merge.VisitType(diff)
+	}
+}
+
+// VisitPrimitive delegates visiting a primitive to the ReplaceVisitorSingleton.
+func (v strategicVisitor) VisitPrimitive(diff apply.PrimitiveElement) (apply.Result, error) {
+	// Always replace primitives
+	return v.replace.VisitPrimitive(diff)
+}
+
+// VisitEmpty
+func (v strategicVisitor) VisitEmpty(diff apply.EmptyElement) (apply.Result, error) {
+	return v.merge.VisitEmpty(diff)
+}
+
+var _ apply.Visitor = &strategicVisitor{}

pkg/kubectl/apply/visitor.go
@@ -1,0 +1,61 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apply
+
+// Visitor implements a strategy for merging recorded, local and remote values contained
+// in an element and returns the merged result.
+type Visitor interface {
+	// VisitList is invoked by ListElements when Accept is called
+	VisitList(ListElement) (Result, error)
+
+	// VisitMap is invoked by MapElements when Accept is called
+	VisitMap(MapElement) (Result, error)
+
+	// VisitMap is invoked by MapElements when Accept is called
+	VisitType(TypeElement) (Result, error)
+	// VisitPrimitive is invoked by PrimitiveElements when Accept is called
+
+	VisitPrimitive(PrimitiveElement) (Result, error)
+
+	// VisitEmpty is invoked by EmptyElements when Accept is called
+	VisitEmpty(EmptyElement) (Result, error)
+}
+
+// Operation records whether a field should be set or dropped
+type Operation int
+
+const (
+	// ERROR is an error during merge
+	ERROR Operation = iota
+	// SET sets the field on an object
+	SET
+	// DROP drops the field from an object
+	DROP
+)
+
+// Result is the result of merging fields
+type Result struct {
+	// Operation is the operation that should be performed for the merged field
+	Operation Operation
+	// MergedResult is the new merged value
+	MergedResult interface{}
+}
+
+type MapValuesElement interface {
+	Element
+	GetValues() map[string]Element
+}

cymtrick/kubernetes-error-fuzzer

pkg/kubectl/apply/parse/openapi.go
@@ -1,0 +1,173 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package parse
+
+import (
+	"fmt"
+	"k8s.io/kubernetes/pkg/kubectl/cmd/util/openapi"
+)
+
+// Contains functions for casting openapi interfaces to their underlying types
+
+// getSchemaType returns the string type of the schema - e.g. array, primitive, map, kind, reference
+func getSchemaType(schema openapi.Schema) string {
+	if schema == nil {
+		return ""
+	}
+	visitor := &baseSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Kind
+}
+
+// getKind converts schema to an *openapi.Kind object
+func getKind(schema openapi.Schema) (*openapi.Kind, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &kindSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+// getArray converts schema to an *openapi.Array object
+func getArray(schema openapi.Schema) (*openapi.Array, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &arraySchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+// getMap converts schema to an *openapi.Map object
+func getMap(schema openapi.Schema) (*openapi.Map, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &mapSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+// getPrimitive converts schema to an *openapi.Primitive object
+func getPrimitive(schema openapi.Schema) (*openapi.Primitive, error) {
+	if schema == nil {
+		return nil, nil
+	}
+	visitor := &primitiveSchemaVisitor{}
+	schema.Accept(visitor)
+	return visitor.Result, visitor.Err
+}
+
+type baseSchemaVisitor struct {
+	Err  error
+	Kind string
+}
+
+// VisitArray implements openapi
+func (v *baseSchemaVisitor) VisitArray(array *openapi.Array) {
+	v.Kind = "array"
+	v.Err = fmt.Errorf("Array type not expected")
+}
+
+// MergeMap implements openapi
+func (v *baseSchemaVisitor) VisitMap(*openapi.Map) {
+	v.Kind = "map"
+	v.Err = fmt.Errorf("Map type not expected")
+}
+
+// MergePrimitive implements openapi
+func (v *baseSchemaVisitor) VisitPrimitive(*openapi.Primitive) {
+	v.Kind = "primitive"
+	v.Err = fmt.Errorf("Primitive type not expected")
+}
+
+// VisitKind implements openapi
+func (v *baseSchemaVisitor) VisitKind(*openapi.Kind) {
+	v.Kind = "kind"
+	v.Err = fmt.Errorf("Kind type not expected")
+}
+
+// VisitReference implements openapi
+func (v *baseSchemaVisitor) VisitReference(reference openapi.Reference) {
+	v.Kind = "reference"
+	v.Err = fmt.Errorf("Reference type not expected")
+}
+
+type kindSchemaVisitor struct {
+	baseSchemaVisitor
+	Result *openapi.Kind
+}
+
+// VisitKind implements openapi
+func (v *kindSchemaVisitor) VisitKind(result *openapi.Kind) {
+	v.Result = result
+	v.Kind = "kind"
+}
+
+// VisitReference implements openapi
+func (v *kindSchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}
+
+type mapSchemaVisitor struct {
+	baseSchemaVisitor
+	Result *openapi.Map
+}
+
+// MergeMap implements openapi
+func (v *mapSchemaVisitor) VisitMap(result *openapi.Map) {
+	v.Result = result
+	v.Kind = "map"
+}
+
+// VisitReference implements openapi
+func (v *mapSchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}
+
+type arraySchemaVisitor struct {
+	baseSchemaVisitor
+	Result *openapi.Array
+}
+
+// VisitArray implements openapi
+func (v *arraySchemaVisitor) VisitArray(result *openapi.Array) {
+	v.Result = result
+	v.Kind = "array"
+}
+
+// MergePrimitive implements openapi
+func (v *arraySchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}
+
+type primitiveSchemaVisitor struct {
+	baseSchemaVisitor
+	Result *openapi.Primitive
+}
+
+// MergePrimitive implements openapi
+func (v *primitiveSchemaVisitor) VisitPrimitive(result *openapi.Primitive) {
+	v.Result = result
+	v.Kind = "primitive"
+}
+
+// VisitReference implements openapi
+func (v *primitiveSchemaVisitor) VisitReference(reference openapi.Reference) {
+	reference.SubSchema().Accept(v)
+}

pkg/kubectl/apply/strategy/merge_visitor.go
@@ -1,0 +1,142 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package strategy
+
+import (
+	"fmt"
+
+	"k8s.io/kubernetes/pkg/kubectl/apply"
+)
+
+func createMergeStrategy(options Options, strategic *delegatingStrategy) mergeStrategy {
+	return mergeStrategy{
+		strategic,
+		options,
+	}
+}
+
+// mergeStrategy creates a patch to merge a local file value into a remote field value
+type mergeStrategy struct {
+	strategic *delegatingStrategy
+	options   Options
+}
+
+// MergeList creates a patch to merge a local list field value into a remote list field value
+func (v mergeStrategy) MergeList(e apply.ListElement) (apply.Result, error) {
+	// No merge logic if adding or deleting a field
+	if result, done := v.doAddOrDelete(e); done {
+		return result, nil
+	}
+
+	// Merge each item in the list and append it to the list
+	merged := []interface{}{}
+	for _, value := range e.Values {
+		// Recursively merge the list element before adding the value to the list
+		result, err := value.Merge(v.strategic)
+		if err != nil {
+			return apply.Result{}, err
+		}
+
+		switch result.Operation {
+		case apply.SET:
+			// Keep the list item value
+			merged = append(merged, result.MergedResult)
+		case apply.DROP:
+			// Drop the list item value
+		default:
+			panic(fmt.Errorf("Unexpected result operation type %+v", result))
+		}
+	}
+
+	if len(merged) == 0 {
+		// If the list is empty, return a nil entry
+		return apply.Result{Operation: apply.SET, MergedResult: nil}, nil
+	}
+	// Return the merged list, and tell the caller to keep it
+	return apply.Result{Operation: apply.SET, MergedResult: merged}, nil
+}
+
+// MergeMap creates a patch to merge a local map field into a remote map field
+func (v mergeStrategy) MergeMap(e apply.MapElement) (apply.Result, error) {
+	return v.doMergeMap(e)
+}
+
+// MergeType creates a patch to merge a local map field into a remote map field
+func (v mergeStrategy) MergeType(e apply.TypeElement) (apply.Result, error) {
+	return v.doMergeMap(e)
+}
+
+// do merges a recorded, local and remote map into a new object
+func (v mergeStrategy) doMergeMap(e apply.MapValuesElement) (apply.Result, error) {
+	// No merge logic if adding or deleting a field
+	if result, done := v.doAddOrDelete(e); done {
+		return result, nil
+	}
+
+	// Merge each item in the list
+	merged := map[string]interface{}{}
+	for key, value := range e.GetValues() {
+		// Recursively merge the map element before adding the value to the map
+		result, err := value.Merge(v.strategic)
+		if err != nil {
+			return apply.Result{}, err
+		}
+
+		switch result.Operation {
+		case apply.SET:
+			// Keep the map item value
+			merged[key] = result.MergedResult
+		case apply.DROP:
+			// Drop the map item value
+		default:
+			panic(fmt.Errorf("Unexpected result operation type %+v", result))
+		}
+	}
+
+	// Return the merged map, and tell the caller to keep it
+	if len(merged) == 0 {
+		// Special case the empty map to set the field value to nil, but keep the field key
+		// This is how the tests expect the structures to look when parsed from yaml
+		return apply.Result{Operation: apply.SET, MergedResult: nil}, nil
+	}
+	return apply.Result{Operation: apply.SET, MergedResult: merged}, nil
+}
+
+func (v mergeStrategy) doAddOrDelete(e apply.Element) (apply.Result, bool) {
+	if apply.IsAdd(e) {
+		return apply.Result{Operation: apply.SET, MergedResult: e.GetLocal()}, true
+	}
+
+	// Delete the List
+	if apply.IsDrop(e) {
+		return apply.Result{Operation: apply.DROP}, true
+	}
+
+	return apply.Result{}, false
+}
+
+// MergePrimitive returns and error.  Primitive elements can't be merged, only replaced.
+func (v mergeStrategy) MergePrimitive(diff apply.PrimitiveElement) (apply.Result, error) {
+	return apply.Result{}, fmt.Errorf("Cannot merge primitive element %v", diff.Name)
+}
+
+// MergeEmpty
+func (v mergeStrategy) MergeEmpty(diff apply.EmptyElement) (apply.Result, error) {
+	return apply.Result{Operation: apply.SET}, nil
+}
+
+var _ apply.Strategy = &mergeStrategy{}

pkg/kubectl/apply/strategy/strategic_visitor.go
@@ -1,0 +1,92 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package strategy
+
+import "k8s.io/kubernetes/pkg/kubectl/apply"
+
+// delegatingStrategy delegates merging fields to other visitor implementations
+// based on the merge strategy preferred by the field.
+type delegatingStrategy struct {
+	options Options
+	merge   mergeStrategy
+	replace replaceStrategy
+}
+
+// createDelegatingStrategy returns a new delegatingStrategy
+func createDelegatingStrategy(options Options) *delegatingStrategy {
+	v := &delegatingStrategy{
+		options: options,
+	}
+	v.replace = createReplaceStrategy(options, v)
+	v.merge = createMergeStrategy(options, v)
+	return v
+}
+
+// MergeList delegates visiting a list based on the field patch strategy.
+// Defaults to "replace"
+func (v delegatingStrategy) MergeList(diff apply.ListElement) (apply.Result, error) {
+	// TODO: Support retainkeys
+	switch diff.GetFieldMergeType() {
+	case "merge":
+		return v.merge.MergeList(diff)
+	case "replace":
+		return v.replace.MergeList(diff)
+	default:
+		return v.replace.MergeList(diff)
+	}
+}
+
+// MergeMap delegates visiting a map based on the field patch strategy.
+// Defaults to "merge"
+func (v delegatingStrategy) MergeMap(diff apply.MapElement) (apply.Result, error) {
+	// TODO: Support retainkeys
+	switch diff.GetFieldMergeType() {
+	case "merge":
+		return v.merge.MergeMap(diff)
+	case "replace":
+		return v.replace.MergeMap(diff)
+	default:
+		return v.merge.MergeMap(diff)
+	}
+}
+
+// MergeType delegates visiting a map based on the field patch strategy.
+// Defaults to "merge"
+func (v delegatingStrategy) MergeType(diff apply.TypeElement) (apply.Result, error) {
+	// TODO: Support retainkeys
+	switch diff.GetFieldMergeType() {
+	case "merge":
+		return v.merge.MergeType(diff)
+	case "replace":
+		return v.replace.MergeType(diff)
+	default:
+		return v.merge.MergeType(diff)
+	}
+}
+
+// MergePrimitive delegates visiting a primitive to the ReplaceVisitorSingleton.
+func (v delegatingStrategy) MergePrimitive(diff apply.PrimitiveElement) (apply.Result, error) {
+	// Always replace primitives
+	return v.replace.MergePrimitive(diff)
+}
+
+// MergeEmpty
+func (v delegatingStrategy) MergeEmpty(diff apply.EmptyElement) (apply.Result, error) {
+	return v.merge.MergeEmpty(diff)
+}
+
+var _ apply.Strategy = &delegatingStrategy{}

pkg/kubectl/apply/visitor.go
@@ -1,0 +1,63 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apply
+
+// Strategy implements a strategy for merging recorded, local and remote values contained
+// in an element and returns the merged result.
+// Follows the visitor pattern
+type Strategy interface {
+	// MergeList is invoked by ListElements when Merge is called
+	MergeList(ListElement) (Result, error)
+
+	// MergeMap is invoked by MapElements when Merge is called
+	MergeMap(MapElement) (Result, error)
+
+	// MergeType is invoked by TypeElements when Merge is called
+	MergeType(TypeElement) (Result, error)
+
+	// MergePrimitive is invoked by PrimitiveElements when Merge is called
+	MergePrimitive(PrimitiveElement) (Result, error)
+
+	// MergeEmpty is invoked by EmptyElements when Merge is called
+	MergeEmpty(EmptyElement) (Result, error)
+}
+
+// Operation records whether a field should be set or dropped
+type Operation int
+
+const (
+	// ERROR is an error during merge
+	ERROR Operation = iota
+	// SET sets the field on an object
+	SET
+	// DROP drops the field from an object
+	DROP
+)
+
+// Result is the result of merging fields
+type Result struct {
+	// Operation is the operation that should be performed for the merged field
+	Operation Operation
+	// MergedResult is the new merged value
+	MergedResult interface{}
+}
+
+// MapValuesElement exposes how to get the field / key - value pairs out of a Map or Type Element
+type MapValuesElement interface {
+	Element
+	GetValues() map[string]Element
+}