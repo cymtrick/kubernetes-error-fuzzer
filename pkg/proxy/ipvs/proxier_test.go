cosmoskitten/kubernetes

fix duplicate unbind action

pkg/proxy/ipvs/proxier_test.go
@@ -34,6 +34,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/sets"
 	proxyutil "k8s.io/kubernetes/pkg/proxy/util"
+	proxytest "k8s.io/kubernetes/pkg/proxy/util/testing"
 	utiliptables "k8s.io/kubernetes/pkg/util/iptables"
 	iptablestest "k8s.io/kubernetes/pkg/util/iptables/testing"
 	utilipvs "k8s.io/kubernetes/pkg/util/ipvs"
@@ -116,6 +117,7 @@ func NewFakeProxier(ipt utiliptables.Interface, ipvs utilipvs.Interface, nodeIPs
 		iptablesData:     bytes.NewBuffer(nil),
 		natChains:        bytes.NewBuffer(nil),
 		natRules:         bytes.NewBuffer(nil),
+		netlinkHandle:    proxytest.NewFakeNetlinkHandle(),
 	}
 }
 

cymtrick/kubernetes-error-fuzzer

pkg/proxy/ipvs/proxier_test.go
@@ -31,6 +31,7 @@ import (
 	fakeexec "k8s.io/utils/exec/testing"
 
 	"k8s.io/apimachinery/pkg/util/sets"
+	netlinktest "k8s.io/kubernetes/pkg/proxy/ipvs/testing"
 	proxyutil "k8s.io/kubernetes/pkg/proxy/util"
 	utiliptables "k8s.io/kubernetes/pkg/util/iptables"
 	iptablestest "k8s.io/kubernetes/pkg/util/iptables/testing"
@@ -114,6 +115,7 @@ func NewFakeProxier(ipt utiliptables.Interface, ipvs utilipvs.Interface, nodeIPs
 		iptablesData:     bytes.NewBuffer(nil),
 		natChains:        bytes.NewBuffer(nil),
 		natRules:         bytes.NewBuffer(nil),
+		netlinkHandle:    netlinktest.NewFakeNetlinkHandle(),
 	}
 }
 