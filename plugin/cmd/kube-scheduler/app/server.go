cosmoskitten/kubernetes

update scheduler to use schedulerName selector

plugin/cmd/kube-scheduler/app/server.go
@@ -75,7 +75,7 @@ func Run(s *options.SchedulerServer) error {
 
 	informerFactory := informers.NewSharedInformerFactory(kubecli, 0)
 	// cache only non-terminal pods
-	podInformer := factory.NewPodInformer(kubecli, 0)
+	podInformer := factory.NewPodInformer(kubecli, 0, s.SchedulerName)
 
 	sched, err := CreateScheduler(
 		s,

cymtrick/kubernetes-error-fuzzer

plugin/cmd/kube-scheduler/app/server.go
@@ -79,8 +79,8 @@ func Run(s *options.SchedulerServer) error {
 
 	informerFactory := informers.NewSharedInformerFactory(kubeClient, 0)
 	// cache only non-terminal pods
-	podInformer := factory.NewPodInformer(kubeClient, 0)
 
+	podInformer := factory.NewPodInformer(kubeClient, 0, s.SchedulerName)
 	// Apply algorithms based on feature gates.
 	algorithmprovider.ApplyFeatureGates()
 