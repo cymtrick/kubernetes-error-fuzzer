xdrus/kubernetes

kubeadm: Detect CRIs automatically
In order to allow for a smoother UX with CRIs different than Docker, we have to
make the --cri-socket command line flag optional when just one CRI is
installed.

This change does that by doing the following:

- Introduce a new runtime function (DetectCRISocket) that will attempt to
  detect a CRI socket, or return an appropriate error.
- Default to using the above function if --cri-socket is not specified and
  CRISocket in NodeRegistrationOptions is empty.
- Stop static defaulting to DefaultCRISocket. And rename it to
  DefaultDockerCRISocket. Its use is now narrowed to "Docker or not"
  distinguishment and tests.
- Introduce AddCRISocketFlag function that adds --cri-socket flag to a flagSet.
  Use that in all commands, that support --cri-socket.
- Remove the deprecated --cri-socket-path flag from kubeadm config images pull
  and deprecate --cri-socket in kubeadm upgrade apply.

Signed-off-by: Rostislav M. Georgiev <rostislavg@vmware.com>

cmd/kubeadm/app/cmd/reset.go
@@ -64,6 +64,12 @@ func NewCmdReset(in io.Reader, out io.Writer) *cobra.Command {
 			client, err := getClientset(kubeConfigFile, false)
 			kubeadmutil.CheckErr(err)
 
+			if criSocketPath != "" {
+				criSocketPath, err = resetDetectCRISocket(client)
+				kubeadmutil.CheckErr(err)
+				glog.V(1).Infof("[reset] detected and using CRI socket: %s", criSocketPath)
+			}
+
 			r, err := NewReset(in, ignorePreflightErrorsSet, forceReset, certsDir, criSocketPath)
 			kubeadmutil.CheckErr(err)
 			kubeadmutil.CheckErr(r.Run(out, client))
@@ -78,10 +84,7 @@ func NewCmdReset(in io.Reader, out io.Writer) *cobra.Command {
 		"The path to the directory where the certificates are stored. If specified, clean this directory.",
 	)
 
-	cmd.PersistentFlags().StringVar(
-		&criSocketPath, "cri-socket", kubeadmapiv1beta1.DefaultCRISocket,
-		"The path to the CRI socket to use with crictl when cleaning up containers.",
-	)
+	cmdutil.AddCRISocketFlag(cmd.PersistentFlags(), &criSocketPath, false)
 
 	cmd.PersistentFlags().BoolVarP(
 		&forceReset, "force", "f", false,
@@ -278,4 +281,15 @@ func resetConfigDir(configPathDir, pkiPathDir string) {
 			glog.Errorf("[reset] failed to remove file: %q [%v]\n", path, err)
 		}
 	}
+}
+
+func resetDetectCRISocket(client clientset.Interface) (string, error) {
+	// first try to connect to the cluster for the CRI socket
+	cfg, err := configutil.FetchConfigFromFileOrCluster(client, os.Stdout, "reset", "", false)
+	if err == nil {
+		return cfg.NodeRegistration.CRISocket, nil
+	}
+
+	// if this fails, try to detect it
+	return utilruntime.DetectCRISocket()
 }

cmd/kubeadm/app/phases/kubelet/flags.go
@@ -73,7 +73,7 @@ func WriteKubeletDynamicEnvFile(nodeRegOpts *kubeadmapi.NodeRegistrationOptions,
 func buildKubeletArgMap(opts kubeletFlagsOpts) map[string]string {
 	kubeletFlags := map[string]string{}
 
-	if opts.nodeRegOpts.CRISocket == kubeadmapiv1beta1.DefaultCRISocket {
+	if opts.nodeRegOpts.CRISocket == kubeadmapiv1beta1.DefaultDockerCRISocket {
 		// These flags should only be set when running docker
 		kubeletFlags["network-plugin"] = "cni"
 		driver, err := kubeadmutil.GetCgroupDriverDocker(opts.execer)

cmd/kubeadm/app/util/runtime/runtime.go
@@ -17,6 +17,7 @@ limitations under the License.
 package util
 
 import (
+	"os"
 	"path/filepath"
 	goruntime "runtime"
 	"strings"
@@ -54,7 +55,7 @@ func NewContainerRuntime(execer utilsexec.Interface, criSocket string) (Containe
 	var toolName string
 	var runtime ContainerRuntime
 
-	if criSocket != kubeadmapiv1beta1.DefaultCRISocket {
+	if criSocket != kubeadmapiv1beta1.DefaultDockerCRISocket {
 		toolName = "crictl"
 		// !!! temporary work around crictl warning:
 		// Using "/var/run/crio/crio.sock" as endpoint is deprecated,
@@ -179,4 +180,53 @@ func (runtime *CRIRuntime) ImageExists(image string) (bool, error) {
 func (runtime *DockerRuntime) ImageExists(image string) (bool, error) {
 	err := runtime.exec.Command("docker", "inspect", image).Run()
 	return err == nil, nil
+}
+
+// isExistingSocket checks if path exists and is domain socket
+func isExistingSocket(path string) bool {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return fileInfo.Mode()&os.ModeSocket != 0
+}
+
+// detectCRISocketImpl is separated out only for test purposes, DON'T call it directly, use DetectCRISocket instead
+func detectCRISocketImpl(isSocket func(string) bool) (string, error) {
+	foundCRISockets := []string{}
+	knownCRISockets := []string{
+		// /var/run/dockershim.sock is deliberately omitted from here, we have a separate Docker check below
+		"/run/containerd/containerd.sock",
+		"/var/run/crio/crio.sock",
+	}
+
+	const dockerSocket = "/var/run/docker.sock" // The Docker socket is not CRI compatible
+	if isSocket(dockerSocket) {
+		foundCRISockets = append(foundCRISockets, kubeadmapiv1beta1.DefaultDockerCRISocket)
+	}
+
+	for _, socket := range knownCRISockets {
+		if isSocket(socket) {
+			foundCRISockets = append(foundCRISockets, socket)
+		}
+	}
+
+	switch len(foundCRISockets) {
+	case 0:
+		return "", pkgerrors.New("No CRI socket detected, please use the --cri-socket option")
+	case 1:
+		return foundCRISockets[0], nil
+	default:
+		return "", pkgerrors.Errorf("Found multiple CRI sockets, please use --cri-socket to select one: %s", strings.Join(foundCRISockets, ", "))
+	}
+}
+
+// DetectCRISocket uses a list of known CRI sockets to detect one. If more than one or none is discovered, an error is returned.
+func DetectCRISocket() (string, error) {
+	if goruntime.GOOS != "linux" {
+		return kubeadmapiv1beta1.DefaultDockerCRISocket, nil
+	}
+
+	return detectCRISocketImpl(isExistingSocket)
 }

cmd/kubeadm/app/util/runtime/runtime_test.go
@@ -17,7 +17,11 @@ limitations under the License.
 package util
 
 import (
+	"io/ioutil"
+	"net"
+	"os"
 	"reflect"
+	"runtime"
 	"testing"
 
 	"github.com/pkg/errors"
@@ -41,7 +45,7 @@ func TestNewContainerRuntime(t *testing.T) {
 		isDocker  bool
 		isError   bool
 	}{
-		{"valid: default cri socket", execLookPathOK, kubeadmapiv1beta1.DefaultCRISocket, true, false},
+		{"valid: default cri socket", execLookPathOK, kubeadmapiv1beta1.DefaultDockerCRISocket, true, false},
 		{"valid: cri-o socket url", execLookPathOK, "unix:///var/run/crio/crio.sock", false, false},
 		{"valid: cri-o socket path", execLookPathOK, "/var/run/crio/crio.sock", false, false},
 		{"invalid: no crictl", execLookPathErr, "unix:///var/run/crio/crio.sock", false, true},
@@ -105,8 +109,8 @@ func TestIsRunning(t *testing.T) {
 	}{
 		{"valid: CRI-O is running", "unix:///var/run/crio/crio.sock", criExecer, false},
 		{"invalid: CRI-O is not running", "unix:///var/run/crio/crio.sock", criExecer, true},
-		{"valid: docker is running", kubeadmapiv1beta1.DefaultCRISocket, dockerExecer, false},
-		{"invalid: docker is not running", kubeadmapiv1beta1.DefaultCRISocket, dockerExecer, true},
+		{"valid: docker is running", kubeadmapiv1beta1.DefaultDockerCRISocket, dockerExecer, false},
+		{"invalid: docker is not running", kubeadmapiv1beta1.DefaultDockerCRISocket, dockerExecer, true},
 	}
 
 	for _, tc := range cases {
@@ -146,7 +150,7 @@ func TestListKubeContainers(t *testing.T) {
 	}{
 		{"valid: list containers using CRI socket url", "unix:///var/run/crio/crio.sock", false},
 		{"invalid: list containers using CRI socket url", "unix:///var/run/crio/crio.sock", true},
-		{"valid: list containers using docker", kubeadmapiv1beta1.DefaultCRISocket, false},
+		{"valid: list containers using docker", kubeadmapiv1beta1.DefaultDockerCRISocket, false},
 	}
 
 	for _, tc := range cases {
@@ -199,8 +203,8 @@ func TestRemoveContainers(t *testing.T) {
 		{"valid: remove containers using CRI", "unix:///var/run/crio/crio.sock", []string{"k8s_p1", "k8s_p2", "k8s_p3"}, false}, // Test case 1
 		{"invalid: CRI rmp failure", "unix:///var/run/crio/crio.sock", []string{"k8s_p1", "k8s_p2", "k8s_p3"}, true},
 		{"invalid: CRI stopp failure", "unix:///var/run/crio/crio.sock", []string{"k8s_p1", "k8s_p2", "k8s_p3"}, true},
-		{"valid: remove containers using docker", kubeadmapiv1beta1.DefaultCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, false},
-		{"invalid: remove containers using docker", kubeadmapiv1beta1.DefaultCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, true},
+		{"valid: remove containers using docker", kubeadmapiv1beta1.DefaultDockerCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, false},
+		{"invalid: remove containers using docker", kubeadmapiv1beta1.DefaultDockerCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, true},
 	}
 
 	for _, tc := range cases {
@@ -243,8 +247,8 @@ func TestPullImage(t *testing.T) {
 	}{
 		{"valid: pull image using CRI", "unix:///var/run/crio/crio.sock", "image1", false},
 		{"invalid: CRI pull error", "unix:///var/run/crio/crio.sock", "image2", true},
-		{"valid: pull image using docker", kubeadmapiv1beta1.DefaultCRISocket, "image1", false},
-		{"invalide: docer pull error", kubeadmapiv1beta1.DefaultCRISocket, "image2", true},
+		{"valid: pull image using docker", kubeadmapiv1beta1.DefaultDockerCRISocket, "image1", false},
+		{"invalide: docer pull error", kubeadmapiv1beta1.DefaultDockerCRISocket, "image2", true},
 	}
 
 	for _, tc := range cases {
@@ -287,8 +291,8 @@ func TestImageExists(t *testing.T) {
 	}{
 		{"valid: test if image exists using CRI", "unix:///var/run/crio/crio.sock", "image1", false},
 		{"invalid: CRI inspecti failure", "unix:///var/run/crio/crio.sock", "image2", true},
-		{"valid: test if image exists using docker", kubeadmapiv1beta1.DefaultCRISocket, "image1", false},
-		{"invalid: docker inspect failure", kubeadmapiv1beta1.DefaultCRISocket, "image2", true},
+		{"valid: test if image exists using docker", kubeadmapiv1beta1.DefaultDockerCRISocket, "image1", false},
+		{"invalid: docker inspect failure", kubeadmapiv1beta1.DefaultDockerCRISocket, "image2", true},
 	}
 
 	for _, tc := range cases {
@@ -301,6 +305,135 @@ func TestImageExists(t *testing.T) {
 			result, err := runtime.ImageExists(tc.image)
 			if !tc.result != result {
 				t.Errorf("unexpected ImageExists result: %t, criSocket: %s, image: %s, expected result: %t", err, tc.criSocket, tc.image, tc.result)
+			}
+		})
+	}
+}
+
+func TestIsExistingSocket(t *testing.T) {
+	// this test is not expected to work on Windows
+	if runtime.GOOS == "windows" {
+		return
+	}
+
+	const tempPrefix = "test.kubeadm.runtime.isExistingSocket."
+	tests := []struct {
+		name string
+		proc func(*testing.T)
+	}{
+		{
+			name: "Valid domain socket is detected as such",
+			proc: func(t *testing.T) {
+				tmpFile, err := ioutil.TempFile("", tempPrefix)
+				if err != nil {
+					t.Fatalf("unexpected error by TempFile: %v", err)
+				}
+				theSocket := tmpFile.Name()
+				os.Remove(theSocket)
+				tmpFile.Close()
+
+				con, err := net.Listen("unix", theSocket)
+				if err != nil {
+					t.Fatalf("unexpected error while dialing a socket: %v", err)
+				}
+				defer con.Close()
+
+				if !isExistingSocket(theSocket) {
+					t.Fatalf("isExistingSocket(%q) gave unexpected result. Should have been true, instead of false", theSocket)
+				}
+			},
+		},
+		{
+			name: "Regular file is not a domain socket",
+			proc: func(t *testing.T) {
+				tmpFile, err := ioutil.TempFile("", tempPrefix)
+				if err != nil {
+					t.Fatalf("unexpected error by TempFile: %v", err)
+				}
+				theSocket := tmpFile.Name()
+				defer os.Remove(theSocket)
+				tmpFile.Close()
+
+				if isExistingSocket(theSocket) {
+					t.Fatalf("isExistingSocket(%q) gave unexpected result. Should have been false, instead of true", theSocket)
+				}
+			},
+		},
+		{
+			name: "Non existent socket is not a domain socket",
+			proc: func(t *testing.T) {
+				const theSocket = "/non/existent/socket"
+				if isExistingSocket(theSocket) {
+					t.Fatalf("isExistingSocket(%q) gave unexpected result. Should have been false, instead of true", theSocket)
+				}
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, test.proc)
+	}
+}
+
+func TestDetectCRISocketImpl(t *testing.T) {
+	tests := []struct {
+		name            string
+		existingSockets []string
+		expectedError   bool
+		expectedSocket  string
+	}{
+		{
+			name:            "No existing sockets leads to an error",
+			existingSockets: []string{},
+			expectedError:   true,
+		},
+		{
+			name:            "One valid CRI socket leads to success",
+			existingSockets: []string{"/var/run/crio/crio.sock"},
+			expectedError:   false,
+			expectedSocket:  "/var/run/crio/crio.sock",
+		},
+		{
+			name:            "Correct Docker CRI socket is returned",
+			existingSockets: []string{"/var/run/docker.sock"},
+			expectedError:   false,
+			expectedSocket:  kubeadmapiv1beta1.DefaultDockerCRISocket,
+		},
+		{
+			name: "CRI and Docker sockets lead to an error",
+			existingSockets: []string{
+				"/var/run/docker.sock",
+				"/var/run/crio/crio.sock",
+			},
+			expectedError: true,
+		},
+		{
+			name: "A couple of CRI sockets lead to an error",
+			existingSockets: []string{
+				"/var/run/crio/crio.sock",
+				"/run/containerd/containerd.sock",
+			},
+			expectedError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			socket, err := detectCRISocketImpl(func(path string) bool {
+				for _, existing := range test.existingSockets {
+					if path == existing {
+						return true
+					}
+				}
+
+				return false
+			})
+			if (err != nil) != test.expectedError {
+				t.Fatalf("detectCRISocketImpl returned unexpected result\n\tExpected error: %t\n\tGot error: %t", test.expectedError, err != nil)
+			}
+			if !test.expectedError && socket != test.expectedSocket {
+				t.Fatalf("detectCRISocketImpl returned unexpected CRI socket\n\tExpected socket: %s\n\tReturned socket: %s",
+					test.expectedSocket, socket)
 			}
 		})
 	}

cymtrick/kubernetes-error-fuzzer

cmd/kubeadm/app/cmd/reset.go
@@ -67,6 +67,12 @@ func NewCmdReset(in io.Reader, out io.Writer) *cobra.Command {
 				kubeadmutil.CheckErr(err)
 			}
 
+			if criSocketPath != "" {
+				criSocketPath, err = resetDetectCRISocket(client)
+				kubeadmutil.CheckErr(err)
+				klog.V(1).Infof("[reset] detected and using CRI socket: %s", criSocketPath)
+			}
+
 			r, err := NewReset(in, ignorePreflightErrorsSet, forceReset, certsDir, criSocketPath)
 			kubeadmutil.CheckErr(err)
 			kubeadmutil.CheckErr(r.Run(out, client))
@@ -81,10 +87,7 @@ func NewCmdReset(in io.Reader, out io.Writer) *cobra.Command {
 		"The path to the directory where the certificates are stored. If specified, clean this directory.",
 	)
 
-	cmd.PersistentFlags().StringVar(
-		&criSocketPath, "cri-socket", kubeadmapiv1beta1.DefaultCRISocket,
-		"The path to the CRI socket to use with crictl when cleaning up containers.",
-	)
+	cmdutil.AddCRISocketFlag(cmd.PersistentFlags(), &criSocketPath)
 
 	cmd.PersistentFlags().BoolVarP(
 		&forceReset, "force", "f", false,
@@ -294,4 +297,15 @@ func resetConfigDir(configPathDir, pkiPathDir string) {
 			klog.Errorf("[reset] failed to remove file: %q [%v]\n", path, err)
 		}
 	}
+}
+
+func resetDetectCRISocket(client clientset.Interface) (string, error) {
+	// first try to connect to the cluster for the CRI socket
+	cfg, err := configutil.FetchConfigFromFileOrCluster(client, os.Stdout, "reset", "", false)
+	if err == nil {
+		return cfg.NodeRegistration.CRISocket, nil
+	}
+
+	// if this fails, try to detect it
+	return utilruntime.DetectCRISocket()
 }

cmd/kubeadm/app/phases/kubelet/flags.go
@@ -26,7 +26,6 @@ import (
 	"github.com/pkg/errors"
 	"k8s.io/klog"
 	kubeadmapi "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm"
-	kubeadmapiv1beta1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta1"
 	"k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	"k8s.io/kubernetes/cmd/kubeadm/app/images"
 	kubeadmutil "k8s.io/kubernetes/cmd/kubeadm/app/util"
@@ -74,7 +73,7 @@ func WriteKubeletDynamicEnvFile(cfg *kubeadmapi.InitConfiguration, registerTaint
 func buildKubeletArgMap(opts kubeletFlagsOpts) map[string]string {
 	kubeletFlags := map[string]string{}
 
-	if opts.nodeRegOpts.CRISocket == kubeadmapiv1beta1.DefaultCRISocket {
+	if opts.nodeRegOpts.CRISocket == constants.DefaultDockerCRISocket {
 		// These flags should only be set when running docker
 		kubeletFlags["network-plugin"] = "cni"
 		driver, err := kubeadmutil.GetCgroupDriverDocker(opts.execer)

cmd/kubeadm/app/util/runtime/runtime.go
@@ -17,6 +17,7 @@ limitations under the License.
 package util
 
 import (
+	"os"
 	"path/filepath"
 	goruntime "runtime"
 	"strings"
@@ -24,7 +25,7 @@ import (
 	"github.com/pkg/errors"
 
 	errorsutil "k8s.io/apimachinery/pkg/util/errors"
-	kubeadmapiv1beta1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta1"
+	"k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	utilsexec "k8s.io/utils/exec"
 )
 
@@ -54,7 +55,7 @@ func NewContainerRuntime(execer utilsexec.Interface, criSocket string) (Containe
 	var toolName string
 	var runtime ContainerRuntime
 
-	if criSocket != kubeadmapiv1beta1.DefaultCRISocket {
+	if criSocket != constants.DefaultDockerCRISocket {
 		toolName = "crictl"
 		// !!! temporary work around crictl warning:
 		// Using "/var/run/crio/crio.sock" as endpoint is deprecated,
@@ -179,4 +180,64 @@ func (runtime *CRIRuntime) ImageExists(image string) (bool, error) {
 func (runtime *DockerRuntime) ImageExists(image string) (bool, error) {
 	err := runtime.exec.Command("docker", "inspect", image).Run()
 	return err == nil, nil
+}
+
+// isExistingSocket checks if path exists and is domain socket
+func isExistingSocket(path string) bool {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return fileInfo.Mode()&os.ModeSocket != 0
+}
+
+// detectCRISocketImpl is separated out only for test purposes, DON'T call it directly, use DetectCRISocket instead
+func detectCRISocketImpl(isSocket func(string) bool) (string, error) {
+	const (
+		dockerSocket     = "/var/run/docker.sock" // The Docker socket is not CRI compatible
+		containerdSocket = "/run/containerd/containerd.sock"
+	)
+
+	foundCRISockets := []string{}
+	knownCRISockets := []string{
+		// Docker and containerd sockets are special cased below, hence not to be included here
+		"/var/run/crio/crio.sock",
+	}
+
+	if isSocket(dockerSocket) {
+		// the path in dockerSocket is not CRI compatible, hence we should replace it with a CRI compatible socket
+		foundCRISockets = append(foundCRISockets, constants.DefaultDockerCRISocket)
+	} else if isSocket(containerdSocket) {
+		// Docker 18.09 gets bundled together with containerd, thus having both dockerSocket and containerdSocket present.
+		// For compatibility reasons, we use the containerd socket only if Docker is not detected.
+		foundCRISockets = append(foundCRISockets, containerdSocket)
+	}
+
+	for _, socket := range knownCRISockets {
+		if isSocket(socket) {
+			foundCRISockets = append(foundCRISockets, socket)
+		}
+	}
+
+	switch len(foundCRISockets) {
+	case 0:
+		// Fall back to Docker if no CRI is detected, we can error out later on if we need it
+		return constants.DefaultDockerCRISocket, nil
+	case 1:
+		// Precisely one CRI found, use that
+		return foundCRISockets[0], nil
+	default:
+		// Multiple CRIs installed?
+		return "", errors.Errorf("Found multiple CRI sockets, please use --cri-socket to select one: %s", strings.Join(foundCRISockets, ", "))
+	}
+}
+
+// DetectCRISocket uses a list of known CRI sockets to detect one. If more than one or none is discovered, an error is returned.
+func DetectCRISocket() (string, error) {
+	if goruntime.GOOS != "linux" {
+		return constants.DefaultDockerCRISocket, nil
+	}
+
+	return detectCRISocketImpl(isExistingSocket)
 }

cmd/kubeadm/app/util/runtime/runtime_test.go
@@ -17,12 +17,16 @@ limitations under the License.
 package util
 
 import (
+	"io/ioutil"
+	"net"
+	"os"
 	"reflect"
+	"runtime"
 	"testing"
 
 	"github.com/pkg/errors"
 
-	kubeadmapiv1beta1 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta1"
+	"k8s.io/kubernetes/cmd/kubeadm/app/constants"
 	"k8s.io/utils/exec"
 	fakeexec "k8s.io/utils/exec/testing"
 )
@@ -41,7 +45,7 @@ func TestNewContainerRuntime(t *testing.T) {
 		isDocker  bool
 		isError   bool
 	}{
-		{"valid: default cri socket", execLookPathOK, kubeadmapiv1beta1.DefaultCRISocket, true, false},
+		{"valid: default cri socket", execLookPathOK, constants.DefaultDockerCRISocket, true, false},
 		{"valid: cri-o socket url", execLookPathOK, "unix:///var/run/crio/crio.sock", false, false},
 		{"valid: cri-o socket path", execLookPathOK, "/var/run/crio/crio.sock", false, false},
 		{"invalid: no crictl", execLookPathErr, "unix:///var/run/crio/crio.sock", false, true},
@@ -105,8 +109,8 @@ func TestIsRunning(t *testing.T) {
 	}{
 		{"valid: CRI-O is running", "unix:///var/run/crio/crio.sock", criExecer, false},
 		{"invalid: CRI-O is not running", "unix:///var/run/crio/crio.sock", criExecer, true},
-		{"valid: docker is running", kubeadmapiv1beta1.DefaultCRISocket, dockerExecer, false},
-		{"invalid: docker is not running", kubeadmapiv1beta1.DefaultCRISocket, dockerExecer, true},
+		{"valid: docker is running", constants.DefaultDockerCRISocket, dockerExecer, false},
+		{"invalid: docker is not running", constants.DefaultDockerCRISocket, dockerExecer, true},
 	}
 
 	for _, tc := range cases {
@@ -146,7 +150,7 @@ func TestListKubeContainers(t *testing.T) {
 	}{
 		{"valid: list containers using CRI socket url", "unix:///var/run/crio/crio.sock", false},
 		{"invalid: list containers using CRI socket url", "unix:///var/run/crio/crio.sock", true},
-		{"valid: list containers using docker", kubeadmapiv1beta1.DefaultCRISocket, false},
+		{"valid: list containers using docker", constants.DefaultDockerCRISocket, false},
 	}
 
 	for _, tc := range cases {
@@ -199,8 +203,8 @@ func TestRemoveContainers(t *testing.T) {
 		{"valid: remove containers using CRI", "unix:///var/run/crio/crio.sock", []string{"k8s_p1", "k8s_p2", "k8s_p3"}, false}, // Test case 1
 		{"invalid: CRI rmp failure", "unix:///var/run/crio/crio.sock", []string{"k8s_p1", "k8s_p2", "k8s_p3"}, true},
 		{"invalid: CRI stopp failure", "unix:///var/run/crio/crio.sock", []string{"k8s_p1", "k8s_p2", "k8s_p3"}, true},
-		{"valid: remove containers using docker", kubeadmapiv1beta1.DefaultCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, false},
-		{"invalid: remove containers using docker", kubeadmapiv1beta1.DefaultCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, true},
+		{"valid: remove containers using docker", constants.DefaultDockerCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, false},
+		{"invalid: remove containers using docker", constants.DefaultDockerCRISocket, []string{"k8s_c1", "k8s_c2", "k8s_c3"}, true},
 	}
 
 	for _, tc := range cases {
@@ -243,8 +247,8 @@ func TestPullImage(t *testing.T) {
 	}{
 		{"valid: pull image using CRI", "unix:///var/run/crio/crio.sock", "image1", false},
 		{"invalid: CRI pull error", "unix:///var/run/crio/crio.sock", "image2", true},
-		{"valid: pull image using docker", kubeadmapiv1beta1.DefaultCRISocket, "image1", false},
-		{"invalide: docer pull error", kubeadmapiv1beta1.DefaultCRISocket, "image2", true},
+		{"valid: pull image using docker", constants.DefaultDockerCRISocket, "image1", false},
+		{"invalide: docer pull error", constants.DefaultDockerCRISocket, "image2", true},
 	}
 
 	for _, tc := range cases {
@@ -287,8 +291,8 @@ func TestImageExists(t *testing.T) {
 	}{
 		{"valid: test if image exists using CRI", "unix:///var/run/crio/crio.sock", "image1", false},
 		{"invalid: CRI inspecti failure", "unix:///var/run/crio/crio.sock", "image2", true},
-		{"valid: test if image exists using docker", kubeadmapiv1beta1.DefaultCRISocket, "image1", false},
-		{"invalid: docker inspect failure", kubeadmapiv1beta1.DefaultCRISocket, "image2", true},
+		{"valid: test if image exists using docker", constants.DefaultDockerCRISocket, "image1", false},
+		{"invalid: docker inspect failure", constants.DefaultDockerCRISocket, "image2", true},
 	}
 
 	for _, tc := range cases {
@@ -301,6 +305,145 @@ func TestImageExists(t *testing.T) {
 			result, err := runtime.ImageExists(tc.image)
 			if !tc.result != result {
 				t.Errorf("unexpected ImageExists result: %t, criSocket: %s, image: %s, expected result: %t", err, tc.criSocket, tc.image, tc.result)
+			}
+		})
+	}
+}
+
+func TestIsExistingSocket(t *testing.T) {
+	// this test is not expected to work on Windows
+	if runtime.GOOS == "windows" {
+		return
+	}
+
+	const tempPrefix = "test.kubeadm.runtime.isExistingSocket."
+	tests := []struct {
+		name string
+		proc func(*testing.T)
+	}{
+		{
+			name: "Valid domain socket is detected as such",
+			proc: func(t *testing.T) {
+				tmpFile, err := ioutil.TempFile("", tempPrefix)
+				if err != nil {
+					t.Fatalf("unexpected error by TempFile: %v", err)
+				}
+				theSocket := tmpFile.Name()
+				os.Remove(theSocket)
+				tmpFile.Close()
+
+				con, err := net.Listen("unix", theSocket)
+				if err != nil {
+					t.Fatalf("unexpected error while dialing a socket: %v", err)
+				}
+				defer con.Close()
+
+				if !isExistingSocket(theSocket) {
+					t.Fatalf("isExistingSocket(%q) gave unexpected result. Should have been true, instead of false", theSocket)
+				}
+			},
+		},
+		{
+			name: "Regular file is not a domain socket",
+			proc: func(t *testing.T) {
+				tmpFile, err := ioutil.TempFile("", tempPrefix)
+				if err != nil {
+					t.Fatalf("unexpected error by TempFile: %v", err)
+				}
+				theSocket := tmpFile.Name()
+				defer os.Remove(theSocket)
+				tmpFile.Close()
+
+				if isExistingSocket(theSocket) {
+					t.Fatalf("isExistingSocket(%q) gave unexpected result. Should have been false, instead of true", theSocket)
+				}
+			},
+		},
+		{
+			name: "Non existent socket is not a domain socket",
+			proc: func(t *testing.T) {
+				const theSocket = "/non/existent/socket"
+				if isExistingSocket(theSocket) {
+					t.Fatalf("isExistingSocket(%q) gave unexpected result. Should have been false, instead of true", theSocket)
+				}
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, test.proc)
+	}
+}
+
+func TestDetectCRISocketImpl(t *testing.T) {
+	tests := []struct {
+		name            string
+		existingSockets []string
+		expectedError   bool
+		expectedSocket  string
+	}{
+		{
+			name:            "No existing sockets, use Docker",
+			existingSockets: []string{},
+			expectedError:   false,
+			expectedSocket:  constants.DefaultDockerCRISocket,
+		},
+		{
+			name:            "One valid CRI socket leads to success",
+			existingSockets: []string{"/var/run/crio/crio.sock"},
+			expectedError:   false,
+			expectedSocket:  "/var/run/crio/crio.sock",
+		},
+		{
+			name:            "Correct Docker CRI socket is returned",
+			existingSockets: []string{"/var/run/docker.sock"},
+			expectedError:   false,
+			expectedSocket:  constants.DefaultDockerCRISocket,
+		},
+		{
+			name: "CRI and Docker sockets lead to an error",
+			existingSockets: []string{
+				"/var/run/docker.sock",
+				"/var/run/crio/crio.sock",
+			},
+			expectedError: true,
+		},
+		{
+			name: "Docker and containerd lead to Docker being used",
+			existingSockets: []string{
+				"/var/run/docker.sock",
+				"/run/containerd/containerd.sock",
+			},
+			expectedError:  false,
+			expectedSocket: constants.DefaultDockerCRISocket,
+		},
+		{
+			name: "A couple of CRI sockets lead to an error",
+			existingSockets: []string{
+				"/var/run/crio/crio.sock",
+				"/run/containerd/containerd.sock",
+			},
+			expectedError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			socket, err := detectCRISocketImpl(func(path string) bool {
+				for _, existing := range test.existingSockets {
+					if path == existing {
+						return true
+					}
+				}
+
+				return false
+			})
+			if (err != nil) != test.expectedError {
+				t.Fatalf("detectCRISocketImpl returned unexpected result\n\tExpected error: %t\n\tGot error: %t", test.expectedError, err != nil)
+			}
+			if !test.expectedError && socket != test.expectedSocket {
+				t.Fatalf("detectCRISocketImpl returned unexpected CRI socket\n\tExpected socket: %s\n\tReturned socket: %s",
+					test.expectedSocket, socket)
 			}
 		})
 	}