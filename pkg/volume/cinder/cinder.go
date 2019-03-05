adisky/kubernetes

Add CSI Migration feature flags for Cinder In tree Driver

pkg/volume/cinder/cinder.go
@@ -109,7 +109,8 @@ func (plugin *cinderPlugin) CanSupport(spec *volume.Spec) bool {
 }
 
 func (plugin *cinderPlugin) IsMigratedToCSI() bool {
-	return false
+	return utilfeature.DefaultFeatureGate.Enabled(feature.CSIMigration) &&
+            utilfeature.DefaultFeatureGate.Enabled(feature.CSIMigrationOpenStack)
 }
 
 func (plugin *cinderPlugin) RequiresRemount() bool {

cymtrick/kubernetes-error-fuzzer

pkg/volume/cinder/cinder.go
@@ -109,7 +109,8 @@ func (plugin *cinderPlugin) CanSupport(spec *volume.Spec) bool {
 }
 
 func (plugin *cinderPlugin) IsMigratedToCSI() bool {
-	return false
+	return utilfeature.DefaultFeatureGate.Enabled(features.CSIMigration) &&
+		utilfeature.DefaultFeatureGate.Enabled(features.CSIMigrationOpenStack)
 }
 
 func (plugin *cinderPlugin) RequiresRemount() bool {