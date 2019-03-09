wongma7/kubernetes

Add e2e tests for multiAttach

test/e2e/storage/in_tree_volumes.go
@@ -55,6 +55,7 @@ var testSuites = []func() testsuites.TestSuite{
 	testsuites.InitVolumeModeTestSuite,
 	testsuites.InitSubPathTestSuite,
 	testsuites.InitProvisioningTestSuite,
+	testsuites.InitMultiAttachTestSuite,
 }
 
 // This executes testSuites for in-tree volumes.

cymtrick/kubernetes-error-fuzzer

test/e2e/storage/in_tree_volumes.go
@@ -55,6 +55,7 @@ var testSuites = []func() testsuites.TestSuite{
 	testsuites.InitVolumeModeTestSuite,
 	testsuites.InitSubPathTestSuite,
 	testsuites.InitProvisioningTestSuite,
+	testsuites.InitMultiVolumeTestSuite,
 }
 
 // This executes testSuites for in-tree volumes.