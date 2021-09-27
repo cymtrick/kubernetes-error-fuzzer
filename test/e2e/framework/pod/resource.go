verb/kubernetes

Create node_e2e test for ephemeral containers

test/e2e/framework/pod/resource.go
@@ -335,6 +335,23 @@ func podContainerStarted(c clientset.Interface, namespace, podName string, conta
 	}
 }
 
+func podContainerNameRunning(c clientset.Interface, namespace, podName, containerName string) wait.ConditionFunc {
+	return func() (bool, error) {
+		pod, err := c.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		for _, statuses := range [][]v1.ContainerStatus{pod.Status.ContainerStatuses, pod.Status.InitContainerStatuses, pod.Status.EphemeralContainerStatuses} {
+			for _, cs := range statuses {
+				if cs.Name == containerName {
+					return cs.State.Running != nil, nil
+				}
+			}
+		}
+		return false, nil
+	}
+}
+
 // LogPodStates logs basic info of provided pods for debugging.
 func LogPodStates(pods []v1.Pod) {
 	// Find maximum widths for pod, node, and phase strings for column printing.

test/e2e/framework/pod/wait.go
@@ -568,3 +568,8 @@ func WaitForPodContainerToFail(c clientset.Interface, namespace, podName string,
 func WaitForPodContainerStarted(c clientset.Interface, namespace, podName string, containerIndex int, timeout time.Duration) error {
 	return wait.PollImmediate(poll, timeout, podContainerStarted(c, namespace, podName, containerIndex))
 }
+
+// WaitForPodContainerNameRunning waits for the given Pod container to have a state of running
+func WaitForPodContainerNameRunning(c clientset.Interface, namespace, podName, containerName string, timeout time.Duration) error {
+	return wait.PollImmediate(poll, timeout, podContainerNameRunning(c, namespace, podName, containerName))
+}

cymtrick/kubernetes-error-fuzzer

test/e2e/framework/pod/resource.go
@@ -335,6 +335,23 @@ func podContainerStarted(c clientset.Interface, namespace, podName string, conta
 	}
 }
 
+func isContainerRunning(c clientset.Interface, namespace, podName, containerName string) wait.ConditionFunc {
+	return func() (bool, error) {
+		pod, err := c.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		for _, statuses := range [][]v1.ContainerStatus{pod.Status.ContainerStatuses, pod.Status.InitContainerStatuses, pod.Status.EphemeralContainerStatuses} {
+			for _, cs := range statuses {
+				if cs.Name == containerName {
+					return cs.State.Running != nil, nil
+				}
+			}
+		}
+		return false, nil
+	}
+}
+
 // LogPodStates logs basic info of provided pods for debugging.
 func LogPodStates(pods []v1.Pod) {
 	// Find maximum widths for pod, node, and phase strings for column printing.

test/e2e/framework/pod/wait.go
@@ -565,3 +565,8 @@ func WaitForPodFailedReason(c clientset.Interface, pod *v1.Pod, reason string, t
 	}
 	return nil
 }
+
+// WaitForContainerRunning waits for the given Pod container to have a state of running
+func WaitForContainerRunning(c clientset.Interface, namespace, podName, containerName string, timeout time.Duration) error {
+	return wait.PollImmediate(poll, timeout, isContainerRunning(c, namespace, podName, containerName))
+}