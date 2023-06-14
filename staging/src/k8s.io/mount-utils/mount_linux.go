cymtrick/kubernetes-error-fuzzer

commit 1a930da6dcae234973b1fd4ba65bea9ea25a8d24

Return unwrapped ErrNotExist when EvalSymlink returns PathError
By default filepath.EvalSymlink returns PathError. When a file is
not found, we should unwrap it and return ErrNotExist as this
is what this function expects.

Similar to the comment at:
https://github.com/kubernetes/kubernetes/pull/109217#discussion_r896272206

staging/src/k8s.io/mount-utils/mount_linux.go
@@ -732,6 +732,9 @@ func (mounter *Mounter) IsMountPoint(file string) (bool, error) {
 	// Resolve any symlinks in file, kernel would do the same and use the resolved path in /proc/mounts.
 	resolvedFile, err := filepath.EvalSymlinks(file)
 	if err != nil {
+		if errors.Is(isMntErr, fs.ErrNotExist) {
+			return false, fs.ErrNotExist
+		}
 		return false, err
 	}
 

commit f0ac2042517d590a9708b622d3aafc305ea7426c

mount-utils: IsMountPoint: fix
Commit 1a930da6dcae2 added a code to return unwrapped fs.ErrNotExist
error in case filepath.EvalSymlinks failed a wrapped one.

This never worked because of a copy/paste bug.

Fix this.

Fixes: 1a930da6dcae2
Cc: Manu Gupta <manugupt1@gmail.com>
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

staging/src/k8s.io/mount-utils/mount_linux.go
@@ -766,7 +766,7 @@ func (mounter *Mounter) IsMountPoint(file string) (bool, error) {
 	// Resolve any symlinks in file, kernel would do the same and use the resolved path in /proc/mounts.
 	resolvedFile, err := filepath.EvalSymlinks(file)
 	if err != nil {
-		if errors.Is(isMntErr, fs.ErrNotExist) {
+		if errors.Is(err, fs.ErrNotExist) {
 			return false, fs.ErrNotExist
 		}
 		return false, err