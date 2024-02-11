kubernetes/kubernetes

ktesting: add TContext
The new TContext interface combines a normal context and the testing interface,
then adds some helper methods. The context gets canceled when the test is done,
but that can also be requested earlier via Cancel.

The intended usage is to pass a single `tCtx ktesting.TContext` parameter
around in all helper functions that get called by a unit or integration test.

Logging is also more useful: Log[f] and Fatal[f] output is prefixed with
"[FATAL] ERROR: " to make it stand out more from regular log output.

If this approach turns out to be useful, it could be extended further (for
example, with a per-test timeout) and might get moved to a staging repository
to enable usage of it in other staging repositories.

To allow other implementations besides testing.T and testing.B, a custom
ktesting.TB interface gets defined with the methods expected from the
actual implementation. One such implementation can be ginkgo.GinkgoT().

test/utils/ktesting/contexthelper_test.go
@@ -1,0 +1,125 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ktesting
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCleanupErr(t *testing.T) {
+	if !errors.Is(cleanupErr, context.Canceled) {
+		t.Errorf("cleanupErr %T should be a %T", cleanupErr, context.Canceled)
+	}
+}
+
+func TestCause(t *testing.T) {
+	timeoutCause := canceledError("I timed out")
+	parentCause := errors.New("parent canceled")
+
+	t.Parallel()
+	for name, tt := range map[string]struct {
+		parentCtx              context.Context
+		timeout                time.Duration
+		sleep                  time.Duration
+		cancelCause            string
+		expectErr, expectCause error
+		expectDeadline         time.Duration
+	}{
+		"nothing": {
+			parentCtx: context.Background(),
+			timeout:   5 * time.Millisecond,
+			sleep:     time.Millisecond,
+		},
+		"timeout": {
+			parentCtx:   context.Background(),
+			timeout:     time.Millisecond,
+			sleep:       5 * time.Millisecond,
+			expectErr:   context.Canceled,
+			expectCause: canceledError(timeoutCause),
+		},
+		"parent-canceled": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithCancel(context.Background())
+				cancel()
+				return ctx
+			}(),
+			timeout:     time.Millisecond,
+			sleep:       5 * time.Millisecond,
+			expectErr:   context.Canceled,
+			expectCause: context.Canceled,
+		},
+		"parent-cause": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithCancelCause(context.Background())
+				cancel(parentCause)
+				return ctx
+			}(),
+			timeout:     time.Millisecond,
+			sleep:       5 * time.Millisecond,
+			expectErr:   context.Canceled,
+			expectCause: parentCause,
+		},
+		"deadline-no-parent": {
+			parentCtx:      context.Background(),
+			timeout:        time.Minute,
+			expectDeadline: time.Minute,
+		},
+		"deadline-parent": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+				t.Cleanup(cancel)
+				return ctx
+			}(),
+			timeout:        2 * time.Minute,
+			expectDeadline: time.Minute,
+		},
+		"deadline-child": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+				t.Cleanup(cancel)
+				return ctx
+			}(),
+			timeout:        time.Minute,
+			expectDeadline: time.Minute,
+		},
+	} {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			ctx, cancel := withTimeout(tt.parentCtx, t, tt.timeout, timeoutCause.Error())
+			if tt.cancelCause != "" {
+				cancel(tt.cancelCause)
+			}
+			if tt.expectDeadline != 0 {
+				actualDeadline, ok := ctx.Deadline()
+				if assert.True(t, ok, "should have had a deadline") {
+					assert.InDelta(t, time.Until(actualDeadline), tt.expectDeadline, float64(time.Second), "remaining time till Deadline()")
+				}
+			}
+			time.Sleep(tt.sleep)
+			actualErr := ctx.Err()
+			actualCause := context.Cause(ctx)
+			assert.Equal(t, tt.expectErr, actualErr, "ctx.Err()")
+			assert.Equal(t, tt.expectCause, actualCause, "context.Cause()")
+
+		})
+	}
+}

test/utils/ktesting/errorcontext.go
@@ -1,0 +1,153 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ktesting
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+
+	"k8s.io/klog/v2"
+)
+
+// WithError creates a context where test failures are collected and stored in
+// the provided error instance when the caller is done. Use it like this:
+//
+//	func doSomething(tCtx ktesting.TContext) (finalErr error) {
+//	     tCtx, finalize := WithError(tCtx, &finalErr)
+//	     defer finalize()
+//	     ...
+//	     tCtx.Fatal("some failure")
+//
+// Any error already stored in the variable will get overwritten by finalize if
+// there were test failures, otherwise the variable is left unchanged.
+// If there were multiple test errors, then the error will wrap all of
+// them with errors.Join.
+//
+// Test failures are not propagated to the parent context.
+func WithError(tCtx TContext, err *error) (TContext, func()) {
+	eCtx := &errorContext{
+		TContext: tCtx,
+	}
+
+	return eCtx, func() {
+		// Recover has to be called in the deferred function. When called inside
+		// a function called by a deferred function (like finalize below), it
+		// returns nil.
+		if e := recover(); e != nil {
+			if _, ok := e.(fatalWithError); !ok {
+				// Not our own panic, pass it on instead of setting the error.
+				panic(e)
+			}
+		}
+
+		eCtx.finalize(err)
+	}
+}
+
+type errorContext struct {
+	TContext
+
+	mutex  sync.Mutex
+	errors []error
+	failed bool
+}
+
+func (eCtx *errorContext) finalize(err *error) {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	if !eCtx.failed {
+		return
+	}
+
+	errs := eCtx.errors
+	if len(errs) == 0 {
+		errs = []error{failedWithNoError}
+	}
+	*err = errors.Join(errs...)
+}
+
+func (eCtx *errorContext) Error(args ...any) {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	// Gomega adds a leading newline in https://github.com/onsi/gomega/blob/f804ac6ada8d36164ecae0513295de8affce1245/internal/gomega.go#L37
+	// Let's strip that at start and end because ktesting will make errors
+	// stand out more with the "ERROR" prefix, so there's no need for additional
+	// line breaks.
+	eCtx.errors = append(eCtx.errors, errors.New(strings.TrimSpace(fmt.Sprintln(args...))))
+	eCtx.failed = true
+}
+
+func (eCtx *errorContext) Errorf(format string, args ...any) {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	eCtx.errors = append(eCtx.errors, errors.New(strings.TrimSpace(fmt.Sprintf(format, args...))))
+	eCtx.failed = true
+}
+
+func (eCtx *errorContext) Fail() {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	eCtx.failed = true
+}
+
+func (eCtx *errorContext) FailNow() {
+	eCtx.Helper()
+	eCtx.Fail()
+	panic(failed)
+}
+
+func (eCtx *errorContext) Failed() bool {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	return eCtx.failed
+}
+
+func (eCtx *errorContext) Fatal(args ...any) {
+	eCtx.Error(args...)
+	eCtx.FailNow()
+}
+
+func (eCtx *errorContext) Fatalf(format string, args ...any) {
+	eCtx.Errorf(format, args...)
+	eCtx.FailNow()
+}
+
+func (eCtx *errorContext) CleanupCtx(cb func(TContext)) {
+	eCtx.Helper()
+	cleanupCtx(eCtx, cb)
+}
+
+func (eCtx *errorContext) Logger() klog.Logger {
+	return klog.FromContext(eCtx)
+}
+
+// fatalWithError is the internal type that should never get propagated up. The
+// only case where that can happen is when the developer forgot to call
+// finalize via defer. The string explains that, in case that developers get to
+// see it.
+type fatalWithError string
+
+const failed = fatalWithError("WithError TContext encountered a fatal error, but the finalize function was not called via defer as it should have been.")
+
+var failedWithNoError = errors.New("WithError context was marked as failed without recording an error")

test/utils/ktesting/errorcontext_test.go
@@ -1,0 +1,118 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ktesting
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithError(t *testing.T) {
+	t.Run("panic", func(t *testing.T) {
+		assert.Panics(t, func() {
+			tCtx := Init(t)
+			var err error
+			tCtx, finalize := WithError(tCtx, &err)
+			defer finalize()
+
+			panic("pass me through")
+		})
+	})
+
+	normalErr := errors.New("normal error")
+
+	for name, tc := range map[string]struct {
+		cb           func(TContext)
+		expectNoFail bool
+		expectError  string
+	}{
+		"none": {
+			cb:           func(tCtx TContext) {},
+			expectNoFail: true,
+			expectError:  normalErr.Error(),
+		},
+		"Error": {
+			cb: func(tCtx TContext) {
+				tCtx.Error("some error")
+			},
+			expectError: "some error",
+		},
+		"Errorf": {
+			cb: func(tCtx TContext) {
+				tCtx.Errorf("some %s", "error")
+			},
+			expectError: "some error",
+		},
+		"Fatal": {
+			cb: func(tCtx TContext) {
+				tCtx.Fatal("some error")
+				tCtx.Error("another error")
+			},
+			expectError: "some error",
+		},
+		"Fatalf": {
+			cb: func(tCtx TContext) {
+				tCtx.Fatalf("some %s", "error")
+				tCtx.Error("another error")
+			},
+			expectError: "some error",
+		},
+		"Fail": {
+			cb: func(tCtx TContext) {
+				tCtx.Fatalf("some %s", "error")
+				tCtx.Error("another error")
+			},
+			expectError: "some error",
+		},
+		"FailNow": {
+			cb: func(tCtx TContext) {
+				tCtx.FailNow()
+				tCtx.Error("another error")
+			},
+			expectError: failedWithNoError.Error(),
+		},
+		"many": {
+			cb: func(tCtx TContext) {
+				tCtx.Error("first error")
+				tCtx.Error("second error")
+			},
+			expectError: `first error
+second error`,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			tCtx := Init(t)
+			err := normalErr
+			tCtx, finalize := WithError(tCtx, &err)
+			func() {
+				defer finalize()
+				tc.cb(tCtx)
+			}()
+
+			assert.Equal(t, !tc.expectNoFail, tCtx.Failed(), "Failed()")
+			if tc.expectError == "" {
+				assert.NoError(t, err)
+			} else {
+				if assert.NotNil(t, err) {
+					assert.Equal(t, tc.expectError, err.Error())
+				}
+			}
+		})
+	}
+}

cymtrick/kubernetes-error-fuzzer

test/utils/ktesting/contexthelper_test.go
@@ -1,0 +1,126 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ktesting
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCleanupErr(t *testing.T) {
+	actual := cleanupErr(t.Name())
+	if !errors.Is(actual, context.Canceled) {
+		t.Errorf("cleanupErr %T should be a %T", actual, context.Canceled)
+	}
+}
+
+func TestCause(t *testing.T) {
+	timeoutCause := canceledError("I timed out")
+	parentCause := errors.New("parent canceled")
+
+	t.Parallel()
+	for name, tt := range map[string]struct {
+		parentCtx              context.Context
+		timeout                time.Duration
+		sleep                  time.Duration
+		cancelCause            string
+		expectErr, expectCause error
+		expectDeadline         time.Duration
+	}{
+		"nothing": {
+			parentCtx: context.Background(),
+			timeout:   5 * time.Millisecond,
+			sleep:     time.Millisecond,
+		},
+		"timeout": {
+			parentCtx:   context.Background(),
+			timeout:     time.Millisecond,
+			sleep:       5 * time.Millisecond,
+			expectErr:   context.Canceled,
+			expectCause: canceledError(timeoutCause),
+		},
+		"parent-canceled": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithCancel(context.Background())
+				cancel()
+				return ctx
+			}(),
+			timeout:     time.Millisecond,
+			sleep:       5 * time.Millisecond,
+			expectErr:   context.Canceled,
+			expectCause: context.Canceled,
+		},
+		"parent-cause": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithCancelCause(context.Background())
+				cancel(parentCause)
+				return ctx
+			}(),
+			timeout:     time.Millisecond,
+			sleep:       5 * time.Millisecond,
+			expectErr:   context.Canceled,
+			expectCause: parentCause,
+		},
+		"deadline-no-parent": {
+			parentCtx:      context.Background(),
+			timeout:        time.Minute,
+			expectDeadline: time.Minute,
+		},
+		"deadline-parent": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+				t.Cleanup(cancel)
+				return ctx
+			}(),
+			timeout:        2 * time.Minute,
+			expectDeadline: time.Minute,
+		},
+		"deadline-child": {
+			parentCtx: func() context.Context {
+				ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+				t.Cleanup(cancel)
+				return ctx
+			}(),
+			timeout:        time.Minute,
+			expectDeadline: time.Minute,
+		},
+	} {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			ctx, cancel := withTimeout(tt.parentCtx, t, tt.timeout, timeoutCause.Error())
+			if tt.cancelCause != "" {
+				cancel(tt.cancelCause)
+			}
+			if tt.expectDeadline != 0 {
+				actualDeadline, ok := ctx.Deadline()
+				if assert.True(t, ok, "should have had a deadline") {
+					assert.InDelta(t, time.Until(actualDeadline), tt.expectDeadline, float64(time.Second), "remaining time till Deadline()")
+				}
+			}
+			time.Sleep(tt.sleep)
+			actualErr := ctx.Err()
+			actualCause := context.Cause(ctx)
+			assert.Equal(t, tt.expectErr, actualErr, "ctx.Err()")
+			assert.Equal(t, tt.expectCause, actualCause, "context.Cause()")
+
+		})
+	}
+}

test/utils/ktesting/errorcontext.go
@@ -1,0 +1,153 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ktesting
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"sync"
+
+	"k8s.io/klog/v2"
+)
+
+// WithError creates a context where test failures are collected and stored in
+// the provided error instance when the caller is done. Use it like this:
+//
+//	func doSomething(tCtx ktesting.TContext) (finalErr error) {
+//	     tCtx, finalize := WithError(tCtx, &finalErr)
+//	     defer finalize()
+//	     ...
+//	     tCtx.Fatal("some failure")
+//
+// Any error already stored in the variable will get overwritten by finalize if
+// there were test failures, otherwise the variable is left unchanged.
+// If there were multiple test errors, then the error will wrap all of
+// them with errors.Join.
+//
+// Test failures are not propagated to the parent context.
+func WithError(tCtx TContext, err *error) (TContext, func()) {
+	eCtx := &errorContext{
+		TContext: tCtx,
+	}
+
+	return eCtx, func() {
+		// Recover has to be called in the deferred function. When called inside
+		// a function called by a deferred function (like finalize below), it
+		// returns nil.
+		if e := recover(); e != nil {
+			if _, ok := e.(fatalWithError); !ok {
+				// Not our own panic, pass it on instead of setting the error.
+				panic(e)
+			}
+		}
+
+		eCtx.finalize(err)
+	}
+}
+
+type errorContext struct {
+	TContext
+
+	mutex  sync.Mutex
+	errors []error
+	failed bool
+}
+
+func (eCtx *errorContext) finalize(err *error) {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	if !eCtx.failed {
+		return
+	}
+
+	errs := eCtx.errors
+	if len(errs) == 0 {
+		errs = []error{errFailedWithNoExplanation}
+	}
+	*err = errors.Join(errs...)
+}
+
+func (eCtx *errorContext) Error(args ...any) {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	// Gomega adds a leading newline in https://github.com/onsi/gomega/blob/f804ac6ada8d36164ecae0513295de8affce1245/internal/gomega.go#L37
+	// Let's strip that at start and end because ktesting will make errors
+	// stand out more with the "ERROR" prefix, so there's no need for additional
+	// line breaks.
+	eCtx.errors = append(eCtx.errors, errors.New(strings.TrimSpace(fmt.Sprintln(args...))))
+	eCtx.failed = true
+}
+
+func (eCtx *errorContext) Errorf(format string, args ...any) {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	eCtx.errors = append(eCtx.errors, errors.New(strings.TrimSpace(fmt.Sprintf(format, args...))))
+	eCtx.failed = true
+}
+
+func (eCtx *errorContext) Fail() {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	eCtx.failed = true
+}
+
+func (eCtx *errorContext) FailNow() {
+	eCtx.Helper()
+	eCtx.Fail()
+	panic(failed)
+}
+
+func (eCtx *errorContext) Failed() bool {
+	eCtx.mutex.Lock()
+	defer eCtx.mutex.Unlock()
+
+	return eCtx.failed
+}
+
+func (eCtx *errorContext) Fatal(args ...any) {
+	eCtx.Error(args...)
+	eCtx.FailNow()
+}
+
+func (eCtx *errorContext) Fatalf(format string, args ...any) {
+	eCtx.Errorf(format, args...)
+	eCtx.FailNow()
+}
+
+func (eCtx *errorContext) CleanupCtx(cb func(TContext)) {
+	eCtx.Helper()
+	cleanupCtx(eCtx, cb)
+}
+
+func (eCtx *errorContext) Logger() klog.Logger {
+	return klog.FromContext(eCtx)
+}
+
+// fatalWithError is the internal type that should never get propagated up. The
+// only case where that can happen is when the developer forgot to call
+// finalize via defer. The string explains that, in case that developers get to
+// see it.
+type fatalWithError string
+
+const failed = fatalWithError("WithError TContext encountered a fatal error, but the finalize function was not called via defer as it should have been.")
+
+var errFailedWithNoExplanation = errors.New("WithError context was marked as failed without recording an error")

test/utils/ktesting/errorcontext_test.go
@@ -1,0 +1,116 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ktesting
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithError(t *testing.T) {
+	t.Run("panic", func(t *testing.T) {
+		assert.Panics(t, func() {
+			tCtx := Init(t)
+			var err error
+			_, finalize := WithError(tCtx, &err)
+			defer finalize()
+
+			panic("pass me through")
+		})
+	})
+
+	normalErr := errors.New("normal error")
+
+	for name, tc := range map[string]struct {
+		cb           func(TContext)
+		expectNoFail bool
+		expectError  string
+	}{
+		"none": {
+			cb:           func(tCtx TContext) {},
+			expectNoFail: true,
+			expectError:  normalErr.Error(),
+		},
+		"Error": {
+			cb: func(tCtx TContext) {
+				tCtx.Error("some error")
+			},
+			expectError: "some error",
+		},
+		"Errorf": {
+			cb: func(tCtx TContext) {
+				tCtx.Errorf("some %s", "error")
+			},
+			expectError: "some error",
+		},
+		"Fatal": {
+			cb: func(tCtx TContext) {
+				tCtx.Fatal("some error")
+				tCtx.Error("another error")
+			},
+			expectError: "some error",
+		},
+		"Fatalf": {
+			cb: func(tCtx TContext) {
+				tCtx.Fatalf("some %s", "error")
+				tCtx.Error("another error")
+			},
+			expectError: "some error",
+		},
+		"Fail": {
+			cb: func(tCtx TContext) {
+				tCtx.Fatalf("some %s", "error")
+				tCtx.Error("another error")
+			},
+			expectError: "some error",
+		},
+		"FailNow": {
+			cb: func(tCtx TContext) {
+				tCtx.FailNow()
+				tCtx.Error("another error")
+			},
+			expectError: errFailedWithNoExplanation.Error(),
+		},
+		"many": {
+			cb: func(tCtx TContext) {
+				tCtx.Error("first error")
+				tCtx.Error("second error")
+			},
+			expectError: `first error
+second error`,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			tCtx := Init(t)
+			err := normalErr
+			tCtx, finalize := WithError(tCtx, &err)
+			func() {
+				defer finalize()
+				tc.cb(tCtx)
+			}()
+
+			assert.Equal(t, !tc.expectNoFail, tCtx.Failed(), "Failed()")
+			if tc.expectError == "" {
+				assert.NoError(t, err)
+			} else if assert.NotNil(t, err) {
+				assert.Equal(t, tc.expectError, err.Error())
+			}
+		})
+	}
+}