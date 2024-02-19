AxeZhan/kubernetes

graduate PodLifecycleSleepAction to beta

pkg/kubelet/metrics/metrics.go
@@ -822,6 +822,15 @@ var (
 			StabilityLevel: metrics.ALPHA,
 		},
 	)
+
+	LifecycleHandlerSleepTerminated = metrics.NewCounter(
+		&metrics.CounterOpts{
+			Subsystem:      KubeletSubsystem,
+			Name:           "lifecycle_handler_sleep_terminated_total",
+			Help:           "The number of times lifecycle sleep handler got terminated before it finishes",
+			StabilityLevel: metrics.ALPHA,
+		},
+	)
 )
 
 var registerMetrics sync.Once
@@ -904,6 +913,10 @@ func Register(collectors ...metrics.StableCollector) {
 		}
 
 		legacyregistry.MustRegister(LifecycleHandlerHTTPFallbacks)
+
+		if utilfeature.DefaultFeatureGate.Enabled(features.PodLifecycleSleepAction) {
+			legacyregistry.MustRegister(LifecycleHandlerSleepTerminated)
+		}
 	})
 }
 

cymtrick/kubernetes-error-fuzzer

pkg/kubelet/metrics/metrics.go
@@ -859,6 +859,15 @@ var (
 		},
 		[]string{"image_size_in_bytes"},
 	)
+
+	LifecycleHandlerSleepTerminated = metrics.NewCounter(
+		&metrics.CounterOpts{
+			Subsystem:      KubeletSubsystem,
+			Name:           "sleep_action_terminated_early_total",
+			Help:           "The number of times lifecycle sleep handler got terminated before it finishes",
+			StabilityLevel: metrics.ALPHA,
+		},
+	)
 )
 
 var registerMetrics sync.Once
@@ -942,6 +951,7 @@ func Register(collectors ...metrics.StableCollector) {
 		}
 
 		legacyregistry.MustRegister(LifecycleHandlerHTTPFallbacks)
+		legacyregistry.MustRegister(LifecycleHandlerSleepTerminated)
 	})
 }
 